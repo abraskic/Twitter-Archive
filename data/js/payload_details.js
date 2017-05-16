c78bbb15968bd6a12f2eb4006fce44fe3c07f9e0
abraskic <andrew.braskic@gmail.com>
2017-05-16 12:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71486,
-	"created_at": "Tue May 16 03:14:48 +0000 2017",
+	"tweets": 71487,
+	"created_at": "Tue May 16 04:14:47 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,363 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Tue May 16 03:22:50 +0000 2017",
+		"id": 864320208143396900,
+		"id_str": "864320208143396864",
+		"text": "RT @Herreracus: https://t.co/LVNHsG5ugH",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Herreracus",
+					"name": "Robin Herrera",
+					"id": 286040224,
+					"id_str": "286040224",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 864234744451481600,
+					"id_str": "864234744451481601",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+					"url": "https://t.co/LVNHsG5ugH",
+					"display_url": "pic.twitter.com/LVNHsG5ugH",
+					"expanded_url": "https://twitter.com/Herreracus/status/864234762126241792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 864234762126241800,
+					"source_status_id_str": "864234762126241792",
+					"source_user_id": 286040224,
+					"source_user_id_str": "286040224"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 864234744451481600,
+					"id_str": "864234744451481601",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+					"url": "https://t.co/LVNHsG5ugH",
+					"display_url": "pic.twitter.com/LVNHsG5ugH",
+					"expanded_url": "https://twitter.com/Herreracus/status/864234762126241792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 281,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 864234762126241800,
+					"source_status_id_str": "864234762126241792",
+					"source_user_id": 286040224,
+					"source_user_id_str": "286040224"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Live fast, have seven children, get fat, put all your assets into a trust fund, die young, leave them nothing.",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 258,
+			"friends_count": 71,
+			"listed_count": 26,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 2918,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 71450,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon May 15 21:43:18 +0000 2017",
+			"id": 864234762126241800,
+			"id_str": "864234762126241792",
+			"text": "https://t.co/LVNHsG5ugH",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 864234744451481600,
+						"id_str": "864234744451481601",
+						"indices": [
+							0,
+							23
+						],
+						"media_url": "http://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+						"url": "https://t.co/LVNHsG5ugH",
+						"display_url": "pic.twitter.com/LVNHsG5ugH",
+						"expanded_url": "https://twitter.com/Herreracus/status/864234762126241792/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 864234744451481600,
+						"id_str": "864234744451481601",
+						"indices": [
+							0,
+							23
+						],
+						"media_url": "http://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_5g69GUwAEVWLk.jpg",
+						"url": "https://t.co/LVNHsG5ugH",
+						"display_url": "pic.twitter.com/LVNHsG5ugH",
+						"expanded_url": "https://twitter.com/Herreracus/status/864234762126241792/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 500,
+								"h": 281,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 286040224,
+				"id_str": "286040224",
+				"name": "Robin Herrera",
+				"screen_name": "Herreracus",
+				"location": "Portland, OR",
+				"description": "Kid's author (rep'd by @saraagent) and @OniPress editor (Letter 44, Invader ZIM, Space Battle Lunchtime, etc). Profile pic by @sarahgraleyart.",
+				"url": "http://t.co/69jiY9qPTL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/69jiY9qPTL",
+								"expanded_url": "http://robinherrera.com",
+								"display_url": "robinherrera.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1130,
+				"friends_count": 550,
+				"listed_count": 53,
+				"created_at": "Fri Apr 22 07:14:20 +0000 2011",
+				"favourites_count": 641,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5601,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/237425688/Zombie.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/237425688/Zombie.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/821825981748908033/7mUcknxk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/821825981748908033/7mUcknxk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/286040224/1358831334",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2523,
+			"favorite_count": 5422,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2523,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "und"
+	},
+	{
 		"created_at": "Tue May 16 03:02:41 +0000 2017",
 		"id": 864315138408538100,
 		"id_str": "864315138408538112",
