c7e8e35c25130fd062aa9648bd175203f1c31485
abraskic <andrew.braskic@gmail.com>
2017-07-21 06:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71927,
-	"created_at": "Thu Jul 20 09:14:48 +0000 2017",
+	"tweets": 71928,
+	"created_at": "Thu Jul 20 22:14:47 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,298 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Thu Jul 20 21:54:02 +0000 2017",
+		"id": 888155061393674200,
+		"id_str": "888155061393674240",
+		"text": "Fidget spinners are the gateway drug to the Japanese mafia or some shit. https://t.co/NUUtaphVPb",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NUUtaphVPb",
+					"expanded_url": "https://twitter.com/jasonmustian/status/888151385925197827",
+					"display_url": "twitter.com/jasonmustian/s…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Live fast, have seven children, get fat, put all your assets into a trust fund, die young, leave them nothing.",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 262,
+			"friends_count": 74,
+			"listed_count": 26,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 2961,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 71885,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "6c19d49ffc343fce",
+			"url": "https://api.twitter.com/1.1/geo/id/6c19d49ffc343fce.json",
+			"place_type": "neighborhood",
+			"name": "Banksia Grove",
+			"full_name": "Banksia Grove, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.79415516,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.68577296
+						],
+						[
+							115.79415516,
+							-31.68577296
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 888151385925197800,
+		"quoted_status_id_str": "888151385925197827",
+		"quoted_status": {
+			"created_at": "Thu Jul 20 21:39:26 +0000 2017",
+			"id": 888151385925197800,
+			"id_str": "888151385925197827",
+			"text": "Ya'll living in 2017 while this guy's out here living in 30...oh, wait, no he's dead. https://t.co/bP1XLBYpxT",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 888151323757101000,
+						"id_str": "888151323757101056",
+						"indices": [
+							86,
+							109
+						],
+						"media_url": "http://pbs.twimg.com/media/DFNY7F9VwAA5x_o.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DFNY7F9VwAA5x_o.jpg",
+						"url": "https://t.co/bP1XLBYpxT",
+						"display_url": "pic.twitter.com/bP1XLBYpxT",
+						"expanded_url": "https://twitter.com/jasonmustian/status/888151385925197827/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 888151323757101000,
+						"id_str": "888151323757101056",
+						"indices": [
+							86,
+							109
+						],
+						"media_url": "http://pbs.twimg.com/media/DFNY7F9VwAA5x_o.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DFNY7F9VwAA5x_o.jpg",
+						"url": "https://t.co/bP1XLBYpxT",
+						"display_url": "pic.twitter.com/bP1XLBYpxT",
+						"expanded_url": "https://twitter.com/jasonmustian/status/888151385925197827/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 623,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 105250713,
+				"id_str": "105250713",
+				"name": "Jason Mustian",
+				"screen_name": "jasonmustian",
+				"location": "Austin, TX",
+				"description": "Digital growth guy who thinks you're an idiot for hiring a full-time Digital growth guy. Formerly VP @thechive, CEO @Distractify, Managing Editor @someecards.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 19060,
+				"friends_count": 570,
+				"listed_count": 0,
+				"created_at": "Fri Jan 15 20:43:30 +0000 2010",
+				"favourites_count": 26135,
+				"utc_offset": -21600,
+				"time_zone": "Mountain Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 5251,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/299080084/pineapple_25450_lg.gif",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/299080084/pineapple_25450_lg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/753059143343230977/-kAtXohe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/753059143343230977/-kAtXohe_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/105250713/1398383345",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jul 20 08:55:19 +0000 2017",
 		"id": 887959091749855200,
 		"id_str": "887959091749855235",
