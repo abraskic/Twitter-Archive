763610041caec8060a9b724472b28aa4474e8bb0
abraskic <andrew.braskic@gmail.com>
2017-08-21 04:14:50 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72108,
-	"created_at": "Sun Aug 20 12:14:47 +0000 2017",
+	"tweets": 72109,
+	"created_at": "Sun Aug 20 20:14:48 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,350 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Sun Aug 20 20:00:15 +0000 2017",
+		"id": 899360449874935800,
+		"id_str": "899360449874935809",
+		"text": "RT @jasonmustian: Send noods. https://t.co/WZsnMlMpgS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jasonmustian",
+					"name": "Jason Mustian",
+					"id": 105250713,
+					"id_str": "105250713",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 899328349662724100,
+					"id_str": "899328349662724096",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+					"url": "https://t.co/WZsnMlMpgS",
+					"display_url": "pic.twitter.com/WZsnMlMpgS",
+					"expanded_url": "https://twitter.com/jasonmustian/status/899328363940257795/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 899328363940257800,
+					"source_status_id_str": "899328363940257795",
+					"source_user_id": 105250713,
+					"source_user_id_str": "105250713"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 899328349662724100,
+					"id_str": "899328349662724096",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+					"url": "https://t.co/WZsnMlMpgS",
+					"display_url": "pic.twitter.com/WZsnMlMpgS",
+					"expanded_url": "https://twitter.com/jasonmustian/status/899328363940257795/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 899328363940257800,
+					"source_status_id_str": "899328363940257795",
+					"source_user_id": 105250713,
+					"source_user_id_str": "105250713"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 261,
+			"friends_count": 76,
+			"listed_count": 26,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 2977,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 72065,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Aug 20 17:52:45 +0000 2017",
+			"id": 899328363940257800,
+			"id_str": "899328363940257795",
+			"text": "Send noods. https://t.co/WZsnMlMpgS",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 899328349662724100,
+						"id_str": "899328349662724096",
+						"indices": [
+							12,
+							35
+						],
+						"media_url": "http://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+						"url": "https://t.co/WZsnMlMpgS",
+						"display_url": "pic.twitter.com/WZsnMlMpgS",
+						"expanded_url": "https://twitter.com/jasonmustian/status/899328363940257795/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 899328349662724100,
+						"id_str": "899328349662724096",
+						"indices": [
+							12,
+							35
+						],
+						"media_url": "http://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHsOXoIVwAAuvzu.jpg",
+						"url": "https://t.co/WZsnMlMpgS",
+						"display_url": "pic.twitter.com/WZsnMlMpgS",
+						"expanded_url": "https://twitter.com/jasonmustian/status/899328363940257795/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 105250713,
+				"id_str": "105250713",
+				"name": "Jason Mustian",
+				"screen_name": "jasonmustian",
+				"location": "Austin, TX",
+				"description": "Lover of hating things. Formerly VP @thechive, CEO @distractify, Managing Editor @someecards.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 19012,
+				"friends_count": 570,
+				"listed_count": 1221,
+				"created_at": "Fri Jan 15 20:43:30 +0000 2010",
+				"favourites_count": 26211,
+				"utc_offset": -21600,
+				"time_zone": "Mountain Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 5277,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/299080084/pineapple_25450_lg.gif",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/299080084/pineapple_25450_lg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/753059143343230977/-kAtXohe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/753059143343230977/-kAtXohe_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/105250713/1398383345",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Aug 20 11:39:41 +0000 2017",
 		"id": 899234478899347500,
 		"id_str": "899234478899347456",
