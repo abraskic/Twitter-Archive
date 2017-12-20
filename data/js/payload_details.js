ec21c65ea2eeaecb9df668df3840744cc0970e9a
abraskic <andrew.braskic@gmail.com>
2017-12-20 15:14:48 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72846,
-	"created_at": "Wed Dec 20 03:14:47 +0000 2017",
+	"tweets": 72848,
+	"created_at": "Wed Dec 20 07:14:47 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_12.js
+++ data/js/tweets/2017_12.js
@@ -1,6 +1,582 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Wed Dec 20 06:25:40 +0000 2017",
+		"id": 943366773402775600,
+		"id_str": "943366773402775552",
+		"text": "RT @lanarepublic: #Jobs #community #jobs @DanielAndrewsMP @FedSquare https://t.co/3E8U6Ow8dP",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Jobs",
+					"indices": [
+						18,
+						23
+					]
+				},
+				{
+					"text": "community",
+					"indices": [
+						24,
+						34
+					]
+				},
+				{
+					"text": "jobs",
+					"indices": [
+						35,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lanarepublic",
+					"name": "luca lana",
+					"id": 24364148,
+					"id_str": "24364148",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "DanielAndrewsMP",
+					"name": "Daniel Andrews",
+					"id": 228535666,
+					"id_str": "228535666",
+					"indices": [
+						41,
+						57
+					]
+				},
+				{
+					"screen_name": "FedSquare",
+					"name": "Federation Square",
+					"id": 34868390,
+					"id_str": "34868390",
+					"indices": [
+						58,
+						68
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 943314027303747600,
+					"id_str": "943314027303747584",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+					"url": "https://t.co/3E8U6Ow8dP",
+					"display_url": "pic.twitter.com/3E8U6Ow8dP",
+					"expanded_url": "https://twitter.com/lanarepublic/status/943314683561308160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 853,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1066,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 943314683561308200,
+					"source_status_id_str": "943314683561308160",
+					"source_user_id": 24364148,
+					"source_user_id_str": "24364148"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 943314027303747600,
+					"id_str": "943314027303747584",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+					"url": "https://t.co/3E8U6Ow8dP",
+					"display_url": "pic.twitter.com/3E8U6Ow8dP",
+					"expanded_url": "https://twitter.com/lanarepublic/status/943314683561308160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 853,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1066,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 943314683561308200,
+					"source_status_id_str": "943314683561308160",
+					"source_user_id": 24364148,
+					"source_user_id_str": "24364148"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 75,
+			"listed_count": 27,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3023,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 72789,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Dec 20 02:58:41 +0000 2017",
+			"id": 943314683561308200,
+			"id_str": "943314683561308160",
+			"text": "#Jobs #community #jobs @DanielAndrewsMP @FedSquare https://t.co/3E8U6Ow8dP",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Jobs",
+						"indices": [
+							0,
+							5
+						]
+					},
+					{
+						"text": "community",
+						"indices": [
+							6,
+							16
+						]
+					},
+					{
+						"text": "jobs",
+						"indices": [
+							17,
+							22
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "DanielAndrewsMP",
+						"name": "Daniel Andrews",
+						"id": 228535666,
+						"id_str": "228535666",
+						"indices": [
+							23,
+							39
+						]
+					},
+					{
+						"screen_name": "FedSquare",
+						"name": "Federation Square",
+						"id": 34868390,
+						"id_str": "34868390",
+						"indices": [
+							40,
+							50
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 943314027303747600,
+						"id_str": "943314027303747584",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+						"url": "https://t.co/3E8U6Ow8dP",
+						"display_url": "pic.twitter.com/3E8U6Ow8dP",
+						"expanded_url": "https://twitter.com/lanarepublic/status/943314683561308160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 853,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 483,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1500,
+								"h": 1066,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 943314027303747600,
+						"id_str": "943314027303747584",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DRdTHCUVQAA-2EG.jpg",
+						"url": "https://t.co/3E8U6Ow8dP",
+						"display_url": "pic.twitter.com/3E8U6Ow8dP",
+						"expanded_url": "https://twitter.com/lanarepublic/status/943314683561308160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 853,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 483,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1500,
+								"h": 1066,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 24364148,
+				"id_str": "24364148",
+				"name": "luca lana",
+				"screen_name": "lanarepublic",
+				"location": "Melbourne",
+				"description": "",
+				"url": "http://t.co/71DdFNHyxF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/71DdFNHyxF",
+								"expanded_url": "http://lucalana.com",
+								"display_url": "lucalana.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 98,
+				"friends_count": 255,
+				"listed_count": 2,
+				"created_at": "Sat Mar 14 12:57:31 +0000 2009",
+				"favourites_count": 24,
+				"utc_offset": 39600,
+				"time_zone": "Melbourne",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 189,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/485743366542352384/Yqp0wlNO_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/485743366542352384/Yqp0wlNO_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/24364148/1359079422",
+				"profile_link_color": "F5D6D2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 30,
+			"favorite_count": 57,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
+		"created_at": "Wed Dec 20 06:15:26 +0000 2017",
+		"id": 943364200016908300,
+		"id_str": "943364200016908288",
+		"text": "@bernietb Indicators? What are indicators?",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bernietb",
+					"name": "Yes Clem Fandango, I can hear you",
+					"id": 15490312,
+					"id_str": "15490312",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 943360798453362700,
+		"in_reply_to_status_id_str": "943360798453362688",
+		"in_reply_to_user_id": 15490312,
+		"in_reply_to_user_id_str": "15490312",
+		"in_reply_to_screen_name": "bernietb",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 75,
+			"listed_count": 27,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3023,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 72789,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "6c19d49ffc343fce",
+			"url": "https://api.twitter.com/1.1/geo/id/6c19d49ffc343fce.json",
+			"place_type": "neighborhood",
+			"name": "Banksia Grove",
+			"full_name": "Banksia Grove, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.79415516,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.68577296
+						],
+						[
+							115.79415516,
+							-31.68577296
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Dec 20 02:17:03 +0000 2017",
 		"id": 943304206995169300,
 		"id_str": "943304206995169285",
