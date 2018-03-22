9f5e04552fda174efcaae9a5353f7e89b141bb3b
abraskic <andrew.braskic@gmail.com>
2018-03-22 23:14:50 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73387,
-	"created_at": "Thu Mar 22 09:14:49 +0000 2018",
+	"tweets": 73389,
+	"created_at": "Thu Mar 22 15:14:49 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,576 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Thu Mar 22 14:56:07 +0000 2018",
+		"id": 976834918029328400,
+		"id_str": "976834918029328384",
+		"text": "RT @adele_pascale: Perfume names are getting worse... https://t.co/um2Ik9lMND",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "adele_pascale",
+					"name": "Adele PJ",
+					"id": 164600606,
+					"id_str": "164600606",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 976391910687420400,
+					"id_str": "976391910687420416",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+					"url": "https://t.co/um2Ik9lMND",
+					"display_url": "pic.twitter.com/um2Ik9lMND",
+					"expanded_url": "https://twitter.com/adele_pascale/status/976391928878108672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 976391928878108700,
+					"source_status_id_str": "976391928878108672",
+					"source_user_id": 164600606,
+					"source_user_id_str": "164600606"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 976391910687420400,
+					"id_str": "976391910687420416",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+					"url": "https://t.co/um2Ik9lMND",
+					"display_url": "pic.twitter.com/um2Ik9lMND",
+					"expanded_url": "https://twitter.com/adele_pascale/status/976391928878108672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 976391928878108700,
+					"source_status_id_str": "976391928878108672",
+					"source_user_id": 164600606,
+					"source_user_id_str": "164600606"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 260,
+			"friends_count": 74,
+			"listed_count": 25,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3054,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 73315,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Mar 21 09:35:50 +0000 2018",
+			"id": 976391928878108700,
+			"id_str": "976391928878108672",
+			"text": "Perfume names are getting worse... https://t.co/um2Ik9lMND",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 976391910687420400,
+						"id_str": "976391910687420416",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+						"url": "https://t.co/um2Ik9lMND",
+						"display_url": "pic.twitter.com/um2Ik9lMND",
+						"expanded_url": "https://twitter.com/adele_pascale/status/976391928878108672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 976391910687420400,
+						"id_str": "976391910687420416",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYzXRQYWsAA5FB8.jpg",
+						"url": "https://t.co/um2Ik9lMND",
+						"display_url": "pic.twitter.com/um2Ik9lMND",
+						"expanded_url": "https://twitter.com/adele_pascale/status/976391928878108672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 768,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 164600606,
+				"id_str": "164600606",
+				"name": "Adele PJ",
+				"screen_name": "adele_pascale",
+				"location": "Middle of Naewhere, Highlands",
+				"description": "Britpop/Mod/Northern Soul/Ska/2Tone. Tied to the 90s. Always overdressed. Owner of 124 pairs of shoes. More entertaining than a highbrow magazine. January Club.",
+				"url": "https://t.co/o9FShLumHG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/o9FShLumHG",
+								"expanded_url": "https://ramblingsofa30somethingweb.wordpress.com",
+								"display_url": "…blingsofa30somethingweb.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1016,
+				"friends_count": 400,
+				"listed_count": 20,
+				"created_at": "Fri Jul 09 07:49:10 +0000 2010",
+				"favourites_count": 20321,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 24293,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0099B9",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/546108470/Cleo",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/546108470/Cleo",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/972193402317099008/no0wKdib_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/972193402317099008/no0wKdib_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/164600606/1515962393",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "5ED4DC",
+				"profile_sidebar_fill_color": "95E8EC",
+				"profile_text_color": "3C3940",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6185,
+			"favorite_count": 23556,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 6185,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Mar 22 14:26:05 +0000 2018",
+		"id": 976827360170291200,
+		"id_str": "976827360170291200",
+		"text": "@Epigrammist I thought it was the City of Cockbum? https://t.co/eYIrEWMMJJ",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Epigrammist",
+					"name": "Toby G",
+					"id": 31065693,
+					"id_str": "31065693",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 976827348585607200,
+					"id_str": "976827348585607168",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DY5jTE9U8AAdKsr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DY5jTE9U8AAdKsr.jpg",
+					"url": "https://t.co/eYIrEWMMJJ",
+					"display_url": "pic.twitter.com/eYIrEWMMJJ",
+					"expanded_url": "https://twitter.com/braskic/status/976827360170291200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 976827348585607200,
+					"id_str": "976827348585607168",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DY5jTE9U8AAdKsr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DY5jTE9U8AAdKsr.jpg",
+					"url": "https://t.co/eYIrEWMMJJ",
+					"display_url": "pic.twitter.com/eYIrEWMMJJ",
+					"expanded_url": "https://twitter.com/braskic/status/976827360170291200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 443,
+							"h": 332,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 976792797532405800,
+		"in_reply_to_status_id_str": "976792797532405760",
+		"in_reply_to_user_id": 31065693,
+		"in_reply_to_user_id_str": "31065693",
+		"in_reply_to_screen_name": "Epigrammist",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 260,
+			"friends_count": 74,
+			"listed_count": 25,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3054,
+			"utc_offset": 28800,
+			"time_zone": "Australia/Perth",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 73315,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "6c19d49ffc343fce",
+			"url": "https://api.twitter.com/1.1/geo/id/6c19d49ffc343fce.json",
+			"place_type": "neighborhood",
+			"name": "Banksia Grove",
+			"full_name": "Banksia Grove, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.79415516,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.71610899
+						],
+						[
+							115.81899912,
+							-31.68577296
+						],
+						[
+							115.79415516,
+							-31.68577296
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Mar 22 08:46:59 +0000 2018",
 		"id": 976742020164104200,
 		"id_str": "976742020164104193",
