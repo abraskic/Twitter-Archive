8cab66c2892389b26302fd62b5eddae213ee1aae
abraskic <andrew.braskic@gmail.com>
2018-10-19 12:21:02 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74389,
-	"created_at": "Fri Oct 19 02:14:46 +0000 2018",
+	"tweets": 74390,
+	"created_at": "Fri Oct 19 04:21:01 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,285 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Fri Oct 19 03:28:38 +0000 2018",
+		"id": 1053125745009057800,
+		"id_str": "1053125745009057792",
+		"full_text": "Gotta love the time of the year when PVC pipes are in full bloom and the services start locating themselves. Spring is in the air! https://t.co/ReiaJcuxof",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1053125718601752600,
+					"id_str": "1053125718601752577",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/Dp10QukV4AEsBHB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dp10QukV4AEsBHB.jpg",
+					"url": "https://t.co/ReiaJcuxof",
+					"display_url": "pic.twitter.com/ReiaJcuxof",
+					"expanded_url": "https://twitter.com/braskic/status/1053125745009057792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1053125718601752600,
+					"id_str": "1053125718601752577",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/Dp10QukV4AEsBHB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dp10QukV4AEsBHB.jpg",
+					"url": "https://t.co/ReiaJcuxof",
+					"display_url": "pic.twitter.com/ReiaJcuxof",
+					"expanded_url": "https://twitter.com/braskic/status/1053125745009057792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1053125719780282400,
+					"id_str": "1053125719780282369",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/Dp10Qy9U0AEB3Mx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dp10Qy9U0AEB3Mx.jpg",
+					"url": "https://t.co/ReiaJcuxof",
+					"display_url": "pic.twitter.com/ReiaJcuxof",
+					"expanded_url": "https://twitter.com/braskic/status/1053125745009057792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1053125721336373200,
+					"id_str": "1053125721336373248",
+					"indices": [
+						131,
+						154
+					],
+					"media_url": "http://pbs.twimg.com/media/Dp10Q4wU4AAzINw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dp10Q4wU4AAzINw.jpg",
+					"url": "https://t.co/ReiaJcuxof",
+					"display_url": "pic.twitter.com/ReiaJcuxof",
+					"expanded_url": "https://twitter.com/braskic/status/1053125745009057792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 266,
+			"friends_count": 68,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3083,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74230,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "17cb1043306e8f3b",
+			"url": "https://api.twitter.com/1.1/geo/id/17cb1043306e8f3b.json",
+			"place_type": "neighborhood",
+			"name": "Wanneroo",
+			"full_name": "Wanneroo, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.77373884,
+							-31.78925802
+						],
+						[
+							115.84684512,
+							-31.78925802
+						],
+						[
+							115.84684512,
+							-31.71638997
+						],
+						[
+							115.77373884,
+							-31.71638997
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Gotta love the time of the year when PVC pipes are in full bloom and the services start locating themselves. Spring is in the air! https://t.co/ReiaJcuxof"
+	},
+	{
 		"created_at": "Fri Oct 19 01:33:51 +0000 2018",
 		"id": 1053096857809768400,
 		"id_str": "1053096857809768449",
