841b23a468c8a5cf0d6b585778c2fb4a6b973674
abraskic <andrew.braskic@gmail.com>
2018-10-31 18:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74425,
-	"created_at": "Wed Oct 31 00:14:47 +0000 2018",
+	"tweets": 74426,
+	"created_at": "Wed Oct 31 10:14:48 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Tue Oct 30 14:38:59 +0000 2018",
+		"id": 1057280711030386700,
+		"id_str": "1057280711030386689",
+		"full_text": "a momma cat carries her baby to safety https://t.co/lP3udixyEy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1057280640498962400,
+					"id_str": "1057280640498962434",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/Dqw3JAzWkAI4AqA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dqw3JAzWkAI4AqA.jpg",
+					"url": "https://t.co/lP3udixyEy",
+					"display_url": "pic.twitter.com/lP3udixyEy",
+					"expanded_url": "https://twitter.com/Just_fionna/status/1057280711030386689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1057280640498962400,
+					"id_str": "1057280640498962434",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/Dqw3JAzWkAI4AqA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dqw3JAzWkAI4AqA.jpg",
+					"url": "https://t.co/lP3udixyEy",
+					"display_url": "pic.twitter.com/lP3udixyEy",
+					"expanded_url": "https://twitter.com/Just_fionna/status/1057280711030386689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2773425313,
+			"id_str": "2773425313",
+			"name": "Pumpkin Spice Fox",
+			"screen_name": "Just_fionna",
+			"location": "nowhere",
+			"description": "I am that fox that people talk about. will probably give you pets, and soft hugs. Send fox pictures, loods @fionnalewd sads at @Homu_sexual",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 623,
+			"friends_count": 743,
+			"listed_count": 17,
+			"created_at": "Wed Aug 27 14:23:13 +0000 2014",
+			"favourites_count": 26632,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51500,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/986186040917594117/k2LDiuQJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/986186040917594117/k2LDiuQJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2773425313/1504720444",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 98,
+		"favorite_count": 220,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "a momma cat carries her baby to safety https://t.co/lP3udixyEy"
+	},
+	{
 		"created_at": "Tue Oct 30 23:23:23 +0000 2018",
 		"id": 1057412679382356000,
 		"id_str": "1057412679382355968",
