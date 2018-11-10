f34f0ee32a07ec669b0e44c83a1809e4749702ac
abraskic <andrew.braskic@gmail.com>
2018-11-10 11:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74533,
-	"created_at": "Fri Nov 09 22:14:46 +0000 2018",
+	"tweets": 74534,
+	"created_at": "Sat Nov 10 03:14:46 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Wed Nov 07 22:41:02 +0000 2018",
+		"id": 1060301125906251800,
+		"id_str": "1060301125906251777",
+		"full_text": "IT’S REAL. THIS IS REAL. HASBRO REALLY PRINTED THIS. https://t.co/pfygITLbmQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1060301120315318300,
+					"id_str": "1060301120315318272",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/DrbyQE9X4AAiU2-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrbyQE9X4AAiU2-.jpg",
+					"url": "https://t.co/pfygITLbmQ",
+					"display_url": "pic.twitter.com/pfygITLbmQ",
+					"expanded_url": "https://twitter.com/suchnerve/status/1060301125906251777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1060301120315318300,
+					"id_str": "1060301120315318272",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/DrbyQE9X4AAiU2-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrbyQE9X4AAiU2-.jpg",
+					"url": "https://t.co/pfygITLbmQ",
+					"display_url": "pic.twitter.com/pfygITLbmQ",
+					"expanded_url": "https://twitter.com/suchnerve/status/1060301125906251777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16738904,
+			"id_str": "16738904",
+			"name": "Vivian",
+			"screen_name": "suchnerve",
+			"location": "",
+			"description": "social issues, politics, programming, puns, shitposting, etc… personal account, so anything goes. (you've been warned.) she/her. header: @PinkieToons. 🌹",
+			"url": "https://t.co/ckXGDCLllz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ckXGDCLllz",
+							"expanded_url": "https://www.patreon.com/suchnerve",
+							"display_url": "patreon.com/suchnerve",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12313,
+			"friends_count": 777,
+			"listed_count": 295,
+			"created_at": "Tue Oct 14 14:49:23 +0000 2008",
+			"favourites_count": 84393,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 55585,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FA5A85",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1057846360853094401/XgSQ8hHe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1057846360853094401/XgSQ8hHe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16738904/1518200545",
+			"profile_link_color": "AA00FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2941,
+		"favorite_count": 5450,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "IT’S REAL. THIS IS REAL. HASBRO REALLY PRINTED THIS. https://t.co/pfygITLbmQ"
+	},
+	{
 		"created_at": "Fri Nov 09 21:38:54 +0000 2018",
 		"id": 1061010263980224500,
 		"id_str": "1061010263980224512",
