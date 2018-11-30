76ca8570edd309ee715f11251442928ac97eec88
abraskic <andrew.braskic@gmail.com>
2018-11-30 10:14:49 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74798,
-	"created_at": "Thu Nov 29 23:14:49 +0000 2018",
+	"tweets": 74799,
+	"created_at": "Fri Nov 30 02:14:48 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,352 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Fri Nov 30 02:10:51 +0000 2018",
+		"id": 1068326462799794200,
+		"id_str": "1068326462799794177",
+		"full_text": "And 11 months and 25 days since people stopped giving a shit. https://t.co/BkxVOv89XC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BkxVOv89XC",
+					"expanded_url": "https://twitter.com/Westcycle/status/1068281209870897152",
+					"display_url": "twitter.com/Westcycle/stat…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 66,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3049,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74342,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				-31.70092472,
+				115.79560696
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				115.79560696,
+				-31.70092472
+			]
+		},
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1068281209870897200,
+		"quoted_status_id_str": "1068281209870897152",
+		"quoted_status_permalink": {
+			"url": "https://t.co/BkxVOv89XC",
+			"expanded": "https://twitter.com/Westcycle/status/1068281209870897152",
+			"display": "twitter.com/Westcycle/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 29 23:11:02 +0000 2018",
+			"id": 1068281209870897200,
+			"id_str": "1068281209870897152",
+			"full_text": "Today marks 12 months since the WA State Government introduced minimum passing distance laws. Thanks to all the drivers out there that have being giving riders more space! #MustBeaMetre #MinimumPassingDistance https://t.co/ohZmgnKQQc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				209
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MustBeaMetre",
+						"indices": [
+							172,
+							185
+						]
+					},
+					{
+						"text": "MinimumPassingDistance",
+						"indices": [
+							186,
+							209
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1068281200823791600,
+						"id_str": "1068281200823791617",
+						"indices": [
+							210,
+							233
+						],
+						"media_url": "http://pbs.twimg.com/media/DtNMF5dUcAEH3q6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DtNMF5dUcAEH3q6.jpg",
+						"url": "https://t.co/ohZmgnKQQc",
+						"display_url": "pic.twitter.com/ohZmgnKQQc",
+						"expanded_url": "https://twitter.com/Westcycle/status/1068281209870897152/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 570,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 940,
+								"h": 788,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 940,
+								"h": 788,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1068281200823791600,
+						"id_str": "1068281200823791617",
+						"indices": [
+							210,
+							233
+						],
+						"media_url": "http://pbs.twimg.com/media/DtNMF5dUcAEH3q6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DtNMF5dUcAEH3q6.jpg",
+						"url": "https://t.co/ohZmgnKQQc",
+						"display_url": "pic.twitter.com/ohZmgnKQQc",
+						"expanded_url": "https://twitter.com/Westcycle/status/1068281209870897152/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 570,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 940,
+								"h": 788,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 940,
+								"h": 788,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 377841067,
+				"id_str": "377841067",
+				"name": "WestCycle",
+				"screen_name": "Westcycle",
+				"location": "Western Australia",
+				"description": "Peak body for cycling in WA. More kids riding through to more gold medals and everything in between.",
+				"url": "https://t.co/FZGtmgumkL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FZGtmgumkL",
+								"expanded_url": "http://www.westcycle.org.au",
+								"display_url": "westcycle.org.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 712,
+				"friends_count": 811,
+				"listed_count": 16,
+				"created_at": "Thu Sep 22 06:06:34 +0000 2011",
+				"favourites_count": 1361,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 911,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1901130534/West_Cycle_Logo_Low__1781D0_1__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1901130534/West_Cycle_Logo_Low__1781D0_1__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/377841067/1458282106",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "And 11 months and 25 days since people stopped giving a shit. https://t.co/BkxVOv89XC"
+	},
+	{
 		"created_at": "Thu Nov 29 22:21:52 +0000 2018",
 		"id": 1068268834446954500,
 		"id_str": "1068268834446954496",
