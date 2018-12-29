abc1b0ba8f47a6969c39c6657397a307da027f82
abraskic <andrew.braskic@gmail.com>
2018-12-29 22:14:50 +0800

    Added 1 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -44,6 +44,96 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
+			"followers_count": 2122,
+			"friends_count": 1580,
+			"listed_count": 37,
+			"created_at": "Sat May 14 08:52:33 +0000 2011",
+			"favourites_count": 14981,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 72839,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1075703316238131200/uNd-jcfQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1075703316238131200/uNd-jcfQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/298421861/1535285002",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 121,
+		"favorite_count": 673,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Pat Cummins like most of us in the general population must get home after a hard day of cricketing and think fuck I work with some useless cunts  #AUDvIND"
+	},
+	{
+		"created_at": "Sat Dec 29 06:02:29 +0000 2018",
+		"id": 1078894001690861600,
+		"id_str": "1078894001690861569",
+		"full_text": "Pat Cummins like most of us in the general population must get home after a hard day of cricketing and think fuck I work with some useless cunts  #AUDvIND",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AUDvIND",
+					"indices": [
+						146,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 298421861,
+			"id_str": "298421861",
+			"name": "Mitch",
+			"screen_name": "M_1tch",
+			"location": "West side of Australia",
+			"description": "I tweet stuff mainly shit takes and @CollingwoodFC based content",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 2120,
 			"friends_count": 1580,
 			"listed_count": 37,
