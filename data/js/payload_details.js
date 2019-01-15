29b6a2ab32626e6ef7060fada11aae07a04a79df
abraskic <andrew.braskic@gmail.com>
2019-01-15 15:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75123,
-	"created_at": "Tue Jan 15 06:14:46 +0000 2019",
+	"tweets": 75124,
+	"created_at": "Tue Jan 15 07:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -137,6 +137,190 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 27948,
+			"friends_count": 8219,
+			"listed_count": 609,
+			"created_at": "Sun Jul 13 12:33:17 +0000 2008",
+			"favourites_count": 22416,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1885,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1056315670479159297/tUCA9ST3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1056315670479159297/tUCA9ST3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15414356/1516232371",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 27,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When your Dad organises dinner because your Mom has to work night shift as well as her day job at TSA. https://t.co/tUAQJaccgQ"
+	},
+	{
+		"created_at": "Tue Jan 15 00:10:11 +0000 2019",
+		"id": 1084965935088169000,
+		"id_str": "1084965935088168960",
+		"full_text": "When your Dad organises dinner because your Mom has to work night shift as well as her day job at TSA. https://t.co/tUAQJaccgQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1084965926464761900,
+					"id_str": "1084965926464761856",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw6SwwBVsAAwoDj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw6SwwBVsAAwoDj.jpg",
+					"url": "https://t.co/tUAQJaccgQ",
+					"display_url": "pic.twitter.com/tUAQJaccgQ",
+					"expanded_url": "https://twitter.com/danilic/status/1084965935088168960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084965926464761900,
+					"id_str": "1084965926464761856",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw6SwwBVsAAwoDj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw6SwwBVsAAwoDj.jpg",
+					"url": "https://t.co/tUAQJaccgQ",
+					"display_url": "pic.twitter.com/tUAQJaccgQ",
+					"expanded_url": "https://twitter.com/danilic/status/1084965935088168960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15414356,
+			"id_str": "15414356",
+			"name": "DAN iLIC",
+			"screen_name": "danilic",
+			"location": "Sydney, New South Wales",
+			"description": "Comedy Industrialist. Broadcaster. Director. Writer. MC. Hoity-Toity. Mostly sits on Cadigal and Biddigal land. https://t.co/zSvAKT96rD",
+			"url": "https://t.co/zSvAKT96rD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zSvAKT96rD",
+							"expanded_url": "http://danilic.com",
+							"display_url": "danilic.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/zSvAKT96rD",
+							"expanded_url": "http://danilic.com",
+							"display_url": "danilic.com",
+							"indices": [
+								112,
+								135
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
 			"followers_count": 27949,
 			"friends_count": 8219,
 			"listed_count": 609,
