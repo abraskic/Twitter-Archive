78265e69b95623075980f8f9724ca2695710acb3
abraskic <andrew.braskic@gmail.com>
2019-03-18 07:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75690,
-	"created_at": "Sat Mar 16 13:14:46 +0000 2019",
+	"tweets": 75691,
+	"created_at": "Sun Mar 17 23:14:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 17 22:23:41 +0000 2019",
+		"id": 1107329050035540000,
+		"id_str": "1107407184034332672",
+		"full_text": "https://t.co/Z9AkdXfRMh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1107329022936170500,
+					"id_str": "1107329022936170496",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1107329022936170496/pu/img/-h-91bR-vHsH9iDa.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1107329022936170496/pu/img/-h-91bR-vHsH9iDa.jpg",
+					"url": "https://t.co/Z9AkdXfRMh",
+					"display_url": "pic.twitter.com/Z9AkdXfRMh",
+					"expanded_url": "https://twitter.com/leinweber/status/1107329050035539970/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1107329022936170500,
+					"id_str": "1107329022936170496",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1107329022936170496/pu/img/-h-91bR-vHsH9iDa.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1107329022936170496/pu/img/-h-91bR-vHsH9iDa.jpg",
+					"url": "https://t.co/Z9AkdXfRMh",
+					"display_url": "pic.twitter.com/Z9AkdXfRMh",
+					"expanded_url": "https://twitter.com/leinweber/status/1107329050035539970/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							427,
+							240
+						],
+						"duration_millis": 5706,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1107329022936170496/pu/vid/854x480/ZaK8WAsRa3yMD48Z.mp4?tag=8"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1107329022936170496/pu/vid/320x180/oQgR9ZhCB9jDMikO.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1107329022936170496/pu/pl/7sqHnX4nnLokerf7.m3u8?tag=8"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1107329022936170496/pu/vid/640x360/v0EJzAPljqv_SXix.mp4?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7524442,
+			"id_str": "7524442",
+			"name": "will leinweber",
+			"screen_name": "leinweber",
+			"location": "San Francisco",
+			"description": "i dont want the world,\ni just want your half",
+			"url": "http://t.co/qQHvcsuNz7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/qQHvcsuNz7",
+							"expanded_url": "http://bitfission.com",
+							"display_url": "bitfission.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1683,
+			"friends_count": 198,
+			"listed_count": 72,
+			"created_at": "Tue Jul 17 05:04:42 +0000 2007",
+			"favourites_count": 1029,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6546,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/634436514865266688/45xPu9PH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/634436514865266688/45xPu9PH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7524442/1360527825",
+			"profile_link_color": "7F490A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "B1C18B",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4647,
+		"favorite_count": 12644,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/Z9AkdXfRMh"
+	},
+	{
 		"created_at": "Sat Mar 16 12:45:39 +0000 2019",
 		"id": 1106899325714493400,
 		"id_str": "1106899325714493440",
