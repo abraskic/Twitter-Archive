606013123a762f403f339be52c6bac35d3c22b33
abraskic <andrew.braskic@gmail.com>
2019-04-01 10:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75717,
-	"created_at": "Sun Mar 31 05:14:47 +0000 2019",
+	"tweets": 75718,
+	"created_at": "Mon Apr 01 02:14:46 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,366 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Mon Apr 01 01:59:46 +0000 2019",
+		"id": 1112534989583712300,
+		"id_str": "1112534989583712258",
+		"full_text": "I’m not a mathematician but shouldn’t they move it more than 3.22 meters to account for future continental drift? https://t.co/GHTiuhc16u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GHTiuhc16u",
+					"expanded_url": "https://twitter.com/canberradsn/status/1112511806805663744",
+					"display_url": "twitter.com/canberradsn/st…",
+					"indices": [
+						114,
+						137
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 256,
+			"friends_count": 63,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 2991,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74698,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1112511806805663700,
+		"quoted_status_id_str": "1112511806805663744",
+		"quoted_status_permalink": {
+			"url": "https://t.co/GHTiuhc16u",
+			"expanded": "https://twitter.com/canberradsn/status/1112511806805663744",
+			"display": "twitter.com/canberradsn/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 01 00:27:38 +0000 2019",
+			"id": 1112511806805663700,
+			"id_str": "1112511806805663744",
+			"full_text": "The survey works begin! 📡📏🌏\n\nIt has been determined, due to continental drift and Australia moving north at 7 centimetres per year, that after 46 years of operations, our big 70 metre dish Deep Space Station 43 #DSS43, must be moved 3.22 metres to the south.\n\nHuge task ahead! https://t.co/BXVVohpSPr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				276
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "DSS43",
+						"indices": [
+							211,
+							217
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1112511465171247100,
+						"id_str": "1112511465171247105",
+						"indices": [
+							277,
+							300
+						],
+						"media_url": "http://pbs.twimg.com/media/D3BvSIIU4AEjzAd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D3BvSIIU4AEjzAd.jpg",
+						"url": "https://t.co/BXVVohpSPr",
+						"display_url": "pic.twitter.com/BXVVohpSPr",
+						"expanded_url": "https://twitter.com/CanberraDSN/status/1112511806805663744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 1920,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1112511465171247100,
+						"id_str": "1112511465171247105",
+						"indices": [
+							277,
+							300
+						],
+						"media_url": "http://pbs.twimg.com/media/D3BvSIIU4AEjzAd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D3BvSIIU4AEjzAd.jpg",
+						"url": "https://t.co/BXVVohpSPr",
+						"display_url": "pic.twitter.com/BXVVohpSPr",
+						"expanded_url": "https://twitter.com/CanberraDSN/status/1112511806805663744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 1920,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1112511486407000000,
+						"id_str": "1112511486407000070",
+						"indices": [
+							277,
+							300
+						],
+						"media_url": "http://pbs.twimg.com/media/D3BvTXPUwAYrAar.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D3BvTXPUwAYrAar.jpg",
+						"url": "https://t.co/BXVVohpSPr",
+						"display_url": "pic.twitter.com/BXVVohpSPr",
+						"expanded_url": "https://twitter.com/CanberraDSN/status/1112511806805663744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1280,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 62740714,
+				"id_str": "62740714",
+				"name": "CanberraDSN",
+				"screen_name": "CanberraDSN",
+				"location": "Canberra, Australia",
+				"description": "Education and outreach at CanberraDSN. Part of NASA's Deep Space Network. Managed in Australia by CSIRO. Tweets are our own and not those of NASA, JPL or CSIRO.",
+				"url": "https://t.co/s786zPzIoy",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/s786zPzIoy",
+								"expanded_url": "http://www.cdscc.nasa.gov",
+								"display_url": "cdscc.nasa.gov",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 18818,
+				"friends_count": 120,
+				"listed_count": 635,
+				"created_at": "Tue Aug 04 06:43:52 +0000 2009",
+				"favourites_count": 11656,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 4769,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/831795999110733825/2nkoQTF6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/831795999110733825/2nkoQTF6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/62740714/1474437157",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 59,
+			"favorite_count": 145,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m not a mathematician but shouldn’t they move it more than 3.22 meters to account for future continental drift? https://t.co/GHTiuhc16u"
+	}
+]
