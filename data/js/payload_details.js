c37f48c3e6959368ef66a6bcc75254323166e061
abraskic <andrew.braskic@gmail.com>
2019-04-17 07:14:47 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75771,
-	"created_at": "Tue Apr 16 10:14:46 +0000 2019",
+	"tweets": 75773,
+	"created_at": "Tue Apr 16 23:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 16 22:33:28 +0000 2019",
+		"id": 1118281279512858600,
+		"id_str": "1118281279512858624",
+		"full_text": "@fictillius But I already have a job, health and education. Is there another bus for people like me?",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fictillius",
+					"name": "Evan",
+					"id": 7050962,
+					"id_str": "7050962",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1118280610588454900,
+		"in_reply_to_status_id_str": "1118280610588454912",
+		"in_reply_to_user_id": 7050962,
+		"in_reply_to_user_id_str": "7050962",
+		"in_reply_to_screen_name": "fictillius",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 256,
+			"friends_count": 63,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 2989,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74763,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@fictillius But I already have a job, health and education. Is there another bus for people like me?"
+	},
+	{
+		"created_at": "Tue Apr 16 22:32:15 +0000 2019",
+		"id": 1118141455061672000,
+		"id_str": "1118280974700142592",
+		"full_text": "Government: You owe us money. It’s called taxes.\n\nMe: How much do I owe?\n\nGov’t: You have to figure that out.\n\nMe: I just pay what I want?\n\nGov’t: Oh, no we know exactly how much you owe. But you have to guess that number too.\n\nMe: What if I get it wrong?\n\nGov’t: You go to prison",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18561929,
+			"id_str": "18561929",
+			"name": "jordan",
+			"screen_name": "jordan_stratton",
+			"location": "",
+			"description": "v v cute but barfs a lot. Check out things I write @thedad. Check out things I say @IDWGUpod.",
+			"url": "https://t.co/TrlMUbfUA8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TrlMUbfUA8",
+							"expanded_url": "https://twitter.com/search?q=from%3Ajordan_stratton%20exclude%3Areplies&s=09",
+							"display_url": "twitter.com/search?q=from%…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14863,
+			"friends_count": 1455,
+			"listed_count": 562,
+			"created_at": "Fri Jan 02 20:36:53 +0000 2009",
+			"favourites_count": 68707,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21897,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "535D8A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/948158644549496833/HADLcRkA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/948158644549496833/HADLcRkA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18561929/1513377331",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "455A6E",
+			"profile_sidebar_fill_color": "EAEDF2",
+			"profile_text_color": "788779",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24499,
+		"favorite_count": 95911,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Government: You owe us money. It’s called taxes.\n\nMe: How much do I owe?\n\nGov’t: You have to figure that out.\n\nMe: I just pay what I want?\n\nGov’t: Oh, no we know exactly how much you owe. But you have to guess that number too.\n\nMe: What if I get it wrong?\n\nGov’t: You go to prison"
+	},
+	{
 		"created_at": "Tue Apr 16 09:54:39 +0000 2019",
 		"id": 1118090319684235300,
 		"id_str": "1118090319684235265",
