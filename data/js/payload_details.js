eb2517bee9be0fd0611ea69344b4c2189777ec71
abraskic <andrew.braskic@gmail.com>
2019-05-15 05:14:49 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75862,
-	"created_at": "Tue May 14 10:14:47 +0000 2019",
+	"tweets": 75863,
+	"created_at": "Tue May 14 21:14:48 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,96 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Tue May 14 20:33:52 +0000 2019",
+		"id": 1128278964554702800,
+		"id_str": "1128398041256321024",
+		"full_text": "After watching episode 2 of Chernobyl I was thinking I am glad that I don't live in a country where bureaucrats ignore scientists.\n\nAnd then I remembered that I do. #auspol",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "auspol",
+					"indices": [
+						165,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2770511,
+			"id_str": "2770511",
+			"name": "Devar",
+			"screen_name": "Devar",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1176,
+			"friends_count": 645,
+			"listed_count": 72,
+			"created_at": "Thu Mar 29 02:31:13 +0000 2007",
+			"favourites_count": 6723,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46743,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000040",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1021707451182153729/yZw20f-a_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021707451182153729/yZw20f-a_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2770511/1500445437",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFDB77",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "After watching episode 2 of Chernobyl I was thinking I am glad that I don't live in a country where bureaucrats ignore scientists.\n\nAnd then I remembered that I do. #auspol"
+	},
+	{
 		"created_at": "Tue May 14 09:15:40 +0000 2019",
 		"id": 1128227366323359700,
 		"id_str": "1128227366323359745",
