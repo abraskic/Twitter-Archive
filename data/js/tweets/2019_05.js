32084fed61860723c6864ae0255a7c0133102079
abraskic <andrew.braskic@gmail.com>
2019-05-22 18:14:48 +0800

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 22 10:13:11 +0000 2019",
+		"id": 1130810450872918000,
+		"id_str": "1131140944558878720",
+		"full_text": "https://t.co/V43t7xJyZM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1130810445227368400,
+					"id_str": "1130810445227368448",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D7FyG9AUIAAmAiC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7FyG9AUIAAmAiC.jpg",
+					"url": "https://t.co/V43t7xJyZM",
+					"display_url": "pic.twitter.com/V43t7xJyZM",
+					"expanded_url": "https://twitter.com/JBarney1994/status/1130810450872918016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 856,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 606,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 856,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1130810445227368400,
+					"id_str": "1130810445227368448",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D7FyG9AUIAAmAiC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7FyG9AUIAAmAiC.jpg",
+					"url": "https://t.co/V43t7xJyZM",
+					"display_url": "pic.twitter.com/V43t7xJyZM",
+					"expanded_url": "https://twitter.com/JBarney1994/status/1130810450872918016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 856,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 606,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 856,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2529890340,
+			"id_str": "2529890340",
+			"name": "Cam Zurhaar fan account 🦘",
+			"screen_name": "JBarney1994",
+			"location": "Instagram: barney_94",
+			"description": "Television graphics operator @Foxfooty @aflwomens @BBL @NBL. Podcaster @NorthTalkPod. Product of the Goulburn Valley. North nuffie.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 745,
+			"friends_count": 551,
+			"listed_count": 20,
+			"created_at": "Wed May 28 12:24:22 +0000 2014",
+			"favourites_count": 25929,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11676,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1108735126987431936/Noo_LAKe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108735126987431936/Noo_LAKe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2529890340/1556411345",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/V43t7xJyZM"
+	},
+	{
 		"created_at": "Wed May 22 08:14:52 +0000 2019",
 		"id": 1131111170062991400,
 		"id_str": "1131111170062991360",
