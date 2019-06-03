0ec84376f1425f089017f13bc13078d1d9ee0772
abraskic <andrew.braskic@gmail.com>
2019-06-03 20:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75926,
-	"created_at": "Mon Jun 03 10:14:46 +0000 2019",
+	"tweets": 75927,
+	"created_at": "Mon Jun 03 12:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,390 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 11:23:48 +0000 2019",
+		"id": 1135507369394184200,
+		"id_str": "1135507369394184193",
+		"full_text": "Imagine in twenty years time having a driverless police car come to your house so you can arrest yourself. https://t.co/ReE1O5SpyD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ReE1O5SpyD",
+					"expanded_url": "https://twitter.com/victoriapolice/status/1135347733710090240",
+					"display_url": "twitter.com/victoriapolice…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 66,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3007,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74938,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1135347733710090200,
+		"quoted_status_id_str": "1135347733710090240",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ReE1O5SpyD",
+			"expanded": "https://twitter.com/victoriapolice/status/1135347733710090240",
+			"display": "twitter.com/victoriapolice…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 03 00:49:28 +0000 2019",
+			"id": 1135347733710090200,
+			"id_str": "1135347733710090240",
+			"full_text": "From today, our Road Policing Command will utilise the all-electric @Tesla Model X in highway patrol operational duties, a first for an Australian police organisation.\n\n🔗https://t.co/POIrKEqEgZ https://t.co/ZeUHuiliRc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				193
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Tesla",
+						"name": "Tesla",
+						"id": 13298072,
+						"id_str": "13298072",
+						"indices": [
+							68,
+							74
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/POIrKEqEgZ",
+						"expanded_url": "https://bit.ly/2Z0W5G3",
+						"display_url": "bit.ly/2Z0W5G3",
+						"indices": [
+							170,
+							193
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1135345807694024700,
+						"id_str": "1135345807694024704",
+						"indices": [
+							194,
+							217
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1135345807694024704/pu/img/BVpHZ2re-SftPtHh.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1135345807694024704/pu/img/BVpHZ2re-SftPtHh.jpg",
+						"url": "https://t.co/ZeUHuiliRc",
+						"display_url": "pic.twitter.com/ZeUHuiliRc",
+						"expanded_url": "https://twitter.com/VictoriaPolice/status/1135347733710090240/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1135345807694024700,
+						"id_str": "1135345807694024704",
+						"indices": [
+							194,
+							217
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1135345807694024704/pu/img/BVpHZ2re-SftPtHh.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1135345807694024704/pu/img/BVpHZ2re-SftPtHh.jpg",
+						"url": "https://t.co/ZeUHuiliRc",
+						"display_url": "pic.twitter.com/ZeUHuiliRc",
+						"expanded_url": "https://twitter.com/VictoriaPolice/status/1135347733710090240/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 140000,
+							"variants": [
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1135345807694024704/pu/vid/720x720/HRkw-6xB-OQLRMEe.mp4?tag=9"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1135345807694024704/pu/pl/5B8Wfj3U8G4FLyhD.m3u8?tag=9"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1135345807694024704/pu/vid/480x480/l9g1BocyMwJP19oX.mp4?tag=9"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1135345807694024704/pu/vid/320x320/PNL9rlhI6kg2U4ma.mp4?tag=9"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "Victoria Police launch first all-electric operational vehicle",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16638310,
+				"id_str": "16638310",
+				"name": "Victoria Police",
+				"screen_name": "VictoriaPolice",
+				"location": "Australia",
+				"description": "Do not use Twitter to request emergency assistance. Call Triple Zero (000) for Police, Fire or Ambulance. Terms of Use: https://t.co/skZa75AmF9",
+				"url": "http://t.co/n7Cnw9mo6j",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/n7Cnw9mo6j",
+								"expanded_url": "http://www.vicpolicenews.com.au",
+								"display_url": "vicpolicenews.com.au",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/skZa75AmF9",
+								"expanded_url": "http://bit.ly/2d7O2Vl",
+								"display_url": "bit.ly/2d7O2Vl",
+								"indices": [
+									120,
+									143
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 238514,
+				"friends_count": 324,
+				"listed_count": 1546,
+				"created_at": "Tue Oct 07 22:31:14 +0000 2008",
+				"favourites_count": 1310,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 44199,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E8E8E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/413133049069449217/8LJ8-3NW_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/413133049069449217/8LJ8-3NW_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16638310/1549575925",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EDEDED",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 478,
+			"favorite_count": 1384,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Imagine in twenty years time having a driverless police car come to your house so you can arrest yourself. https://t.co/ReE1O5SpyD"
+	},
+	{
 		"created_at": "Mon Jun 03 10:09:07 +0000 2019",
 		"id": 1135488573891723300,
 		"id_str": "1135488573891723264",
