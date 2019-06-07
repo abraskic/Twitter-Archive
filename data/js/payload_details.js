5703f77b93722b52ef42fcc8ad6b16c5a8d8c3ab
abraskic <andrew.braskic@gmail.com>
2019-06-07 12:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75945,
-	"created_at": "Thu Jun 06 13:14:46 +0000 2019",
+	"tweets": 75946,
+	"created_at": "Fri Jun 07 04:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,366 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 07 04:14:08 +0000 2019",
+		"id": 1136848790961381400,
+		"id_str": "1136848790961381376",
+		"full_text": "Yes we actually drive like this. https://t.co/h0xdZTdvDb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/h0xdZTdvDb",
+					"expanded_url": "https://twitter.com/dougalwallace/status/1136095382327373824",
+					"display_url": "twitter.com/dougalwallace/…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 66,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3013,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74961,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1136095382327373800,
+		"quoted_status_id_str": "1136095382327373824",
+		"quoted_status_permalink": {
+			"url": "https://t.co/h0xdZTdvDb",
+			"expanded": "https://twitter.com/dougalwallace/status/1136095382327373824",
+			"display": "twitter.com/dougalwallace/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 05 02:20:21 +0000 2019",
+			"id": 1136095382327373800,
+			"id_str": "1136095382327373824",
+			"full_text": "Peak Perth merging at 5 in the morning... #waitforit #perthtraffic https://t.co/SUj8ewXtK8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				66
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "waitforit",
+						"indices": [
+							42,
+							52
+						]
+					},
+					{
+						"text": "perthtraffic",
+						"indices": [
+							53,
+							66
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1136095319198801900,
+						"id_str": "1136095319198801921",
+						"indices": [
+							67,
+							90
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136095319198801921/pu/img/iaX6Me8imDtW3XBA.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136095319198801921/pu/img/iaX6Me8imDtW3XBA.jpg",
+						"url": "https://t.co/SUj8ewXtK8",
+						"display_url": "pic.twitter.com/SUj8ewXtK8",
+						"expanded_url": "https://twitter.com/DougalWallace/status/1136095382327373824/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1136095319198801900,
+						"id_str": "1136095319198801921",
+						"indices": [
+							67,
+							90
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1136095319198801921/pu/img/iaX6Me8imDtW3XBA.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1136095319198801921/pu/img/iaX6Me8imDtW3XBA.jpg",
+						"url": "https://t.co/SUj8ewXtK8",
+						"display_url": "pic.twitter.com/SUj8ewXtK8",
+						"expanded_url": "https://twitter.com/DougalWallace/status/1136095382327373824/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 352,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								20,
+								11
+							],
+							"duration_millis": 24100,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1136095319198801921/pu/vid/490x270/H-vGlDia3oVmEue_.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1136095319198801921/pu/vid/640x352/TlWFPLRWmyWcWX4K.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1136095319198801921/pu/pl/65iLP-7MS5iuPeuL.m3u8?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 34167540,
+				"id_str": "34167540",
+				"name": "Dougal Wallace📺",
+				"screen_name": "DougalWallace",
+				"location": "Perth, Western Australia",
+				"description": "Once reporter, Chief of Staff, now Executive Producer: Ten Eyewitness News, Perth @10NewsFirstPER  Send news tips and dog pics to perthnews@networkten.com.au",
+				"url": "https://t.co/IK64qKt4O0",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IK64qKt4O0",
+								"expanded_url": "http://tenplay.com.au/news/perth",
+								"display_url": "tenplay.com.au/news/perth",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1252,
+				"friends_count": 839,
+				"listed_count": 28,
+				"created_at": "Wed Apr 22 03:31:20 +0000 2009",
+				"favourites_count": 143,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3195,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1114039537024376834/4uTMoSQl_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1114039537024376834/4uTMoSQl_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/34167540/1558593718",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 39,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yes we actually drive like this. https://t.co/h0xdZTdvDb"
+	},
+	{
 		"created_at": "Thu Jun 06 13:12:13 +0000 2019",
 		"id": 1135916035687587800,
 		"id_str": "1136621817571885056",
