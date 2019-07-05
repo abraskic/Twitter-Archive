42215324c0f33d4e1dc82b2ace7772e0598b6370
abraskic <andrew.braskic@gmail.com>
2019-07-05 09:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76026,
-	"created_at": "Thu Jul 04 23:14:45 +0000 2019",
+	"tweets": 76027,
+	"created_at": "Fri Jul 05 01:14:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Fri Jul 05 00:59:59 +0000 2019",
+		"id": 1146623763712098300,
+		"id_str": "1146946793340452864",
+		"full_text": "Never forget. https://t.co/J4qi9o1PDR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146623753968689200,
+					"id_str": "1146623753968689159",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/D-mgOqpUIAcvC1B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-mgOqpUIAcvC1B.jpg",
+					"url": "https://t.co/J4qi9o1PDR",
+					"display_url": "pic.twitter.com/J4qi9o1PDR",
+					"expanded_url": "https://twitter.com/pixelatedboat/status/1146623763712098304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 851,
+							"h": 479,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 851,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146623753968689200,
+					"id_str": "1146623753968689159",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/D-mgOqpUIAcvC1B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-mgOqpUIAcvC1B.jpg",
+					"url": "https://t.co/J4qi9o1PDR",
+					"display_url": "pic.twitter.com/J4qi9o1PDR",
+					"expanded_url": "https://twitter.com/pixelatedboat/status/1146623763712098304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 851,
+							"h": 479,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 851,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2999158160,
+			"id_str": "2999158160",
+			"name": "pixelatedboat aka “mr tweets”",
+			"screen_name": "pixelatedboat",
+			"location": "Tiny Train World",
+			"description": "Prince of lies. Read my comics on IG: https://t.co/FB75MYaFo4",
+			"url": "https://t.co/Zkk0nai3v2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Zkk0nai3v2",
+							"expanded_url": "http://onegianthand.com",
+							"display_url": "onegianthand.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/FB75MYaFo4",
+							"expanded_url": "http://instagram.com/onegianthand",
+							"display_url": "instagram.com/onegianthand",
+							"indices": [
+								38,
+								61
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 286238,
+			"friends_count": 847,
+			"listed_count": 2270,
+			"created_at": "Wed Jan 28 08:27:10 +0000 2015",
+			"favourites_count": 220581,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36730,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875489341291675649/hc8K1aT0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875489341291675649/hc8K1aT0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2999158160/1554694863",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1277,
+		"favorite_count": 7212,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Never forget. https://t.co/J4qi9o1PDR"
+	},
+	{
 		"created_at": "Thu Jul 04 22:59:11 +0000 2019",
 		"id": 1146916393884901400,
 		"id_str": "1146916393884901376",
