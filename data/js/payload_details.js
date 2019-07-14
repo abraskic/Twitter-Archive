78f4b2bf19a14f05397a18e606dc8f90766b4c24
abraskic <andrew.braskic@gmail.com>
2019-07-14 20:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76053,
-	"created_at": "Sat Jul 13 09:14:47 +0000 2019",
+	"tweets": 76054,
+	"created_at": "Sun Jul 14 12:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 14 11:53:42 +0000 2019",
+		"id": 1150372798084206600,
+		"id_str": "1150372798084206592",
+		"full_text": "I probably couldn’t even win a point against Robin Williams let alone Serena Williams. https://t.co/kHInYYgYXT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kHInYYgYXT",
+					"expanded_url": "https://twitter.com/yougov/status/1149699495002853376",
+					"display_url": "twitter.com/yougov/status/…",
+					"indices": [
+						87,
+						110
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 256,
+			"friends_count": 67,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3036,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75087,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1149699495002853400,
+		"quoted_status_id_str": "1149699495002853376",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kHInYYgYXT",
+			"expanded": "https://twitter.com/yougov/status/1149699495002853376",
+			"display": "twitter.com/yougov/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jul 12 15:18:14 +0000 2019",
+			"id": 1149699495002853400,
+			"id_str": "1149699495002853376",
+			"full_text": "One in eight men (12%) say they could win a point in a game of tennis against 23 time grand slam winner Serena Williams https://t.co/q0eNBjn7Vu https://t.co/3InBOWdYwh",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				143
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/q0eNBjn7Vu",
+						"expanded_url": "https://yougov.co.uk/opi/surveys/results?utm_source=twitter&utm_medium=daily_questions&utm_campaign=question_1#/survey/344ce84b-a48d-11e9-8e40-79d1f09423a3/question/4d73bd62-a48f-11e9-aee6-6742cfe83f15/gender",
+						"display_url": "yougov.co.uk/opi/surveys/re…",
+						"indices": [
+							120,
+							143
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1149691124677849100,
+						"id_str": "1149691124677849088",
+						"indices": [
+							144,
+							167
+						],
+						"media_url": "http://pbs.twimg.com/media/D_SF_I_XsAABV3r.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D_SF_I_XsAABV3r.jpg",
+						"url": "https://t.co/3InBOWdYwh",
+						"display_url": "pic.twitter.com/3InBOWdYwh",
+						"expanded_url": "https://twitter.com/YouGov/status/1149699495002853376/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 412,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 996,
+								"h": 604,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 996,
+								"h": 604,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1149691124677849100,
+						"id_str": "1149691124677849088",
+						"indices": [
+							144,
+							167
+						],
+						"media_url": "http://pbs.twimg.com/media/D_SF_I_XsAABV3r.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D_SF_I_XsAABV3r.jpg",
+						"url": "https://t.co/3InBOWdYwh",
+						"display_url": "pic.twitter.com/3InBOWdYwh",
+						"expanded_url": "https://twitter.com/YouGov/status/1149699495002853376/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 412,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 996,
+								"h": 604,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 996,
+								"h": 604,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14810076,
+				"id_str": "14810076",
+				"name": "YouGov",
+				"screen_name": "YouGov",
+				"location": "Earth",
+				"description": "What the world thinks",
+				"url": "http://t.co/aAxsytYPqd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/aAxsytYPqd",
+								"expanded_url": "http://www.YouGov.com",
+								"display_url": "YouGov.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 171245,
+				"friends_count": 414,
+				"listed_count": 2846,
+				"created_at": "Sat May 17 11:50:07 +0000 2008",
+				"favourites_count": 14,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 13871,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1059895702493507586/028J07rS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1059895702493507586/028J07rS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14810076/1541589653",
+				"profile_link_color": "644AAB",
+				"profile_sidebar_border_color": "F2E195",
+				"profile_sidebar_fill_color": "FFF7CC",
+				"profile_text_color": "31373A",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3511,
+			"favorite_count": 11320,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I probably couldn’t even win a point against Robin Williams let alone Serena Williams. https://t.co/kHInYYgYXT"
+	},
+	{
 		"created_at": "Sat Jul 13 09:11:01 +0000 2019",
 		"id": 1149969468212174800,
 		"id_str": "1149969468212174848",
