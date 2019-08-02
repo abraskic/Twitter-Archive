5d9d92a565d31ed426e7a171cee99d3b091b3cda
abraskic <andrew.braskic@gmail.com>
2019-08-02 08:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76105,
-	"created_at": "Thu Aug 01 09:14:46 +0000 2019",
+	"tweets": 76106,
+	"created_at": "Fri Aug 02 00:14:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 01 23:19:04 +0000 2019",
+		"id": 1156595571211276300,
+		"id_str": "1157068258131890176",
+		"full_text": "my parents don’t have weighing scales so they weigh our gas canister with the wii fit https://t.co/2yFhxbm8Zm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1156595557852418000,
+					"id_str": "1156595557852418049",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/EA0NiC6WkAE-mHL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA0NiC6WkAE-mHL.jpg",
+					"url": "https://t.co/2yFhxbm8Zm",
+					"display_url": "pic.twitter.com/2yFhxbm8Zm",
+					"expanded_url": "https://twitter.com/thewhippasnappa/status/1156595571211276288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1156595557852418000,
+					"id_str": "1156595557852418049",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/EA0NiC6WkAE-mHL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA0NiC6WkAE-mHL.jpg",
+					"url": "https://t.co/2yFhxbm8Zm",
+					"display_url": "pic.twitter.com/2yFhxbm8Zm",
+					"expanded_url": "https://twitter.com/thewhippasnappa/status/1156595571211276288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1156595557885972500,
+					"id_str": "1156595557885972483",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/EA0NiDCWkAMeIJG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA0NiDCWkAMeIJG.jpg",
+					"url": "https://t.co/2yFhxbm8Zm",
+					"display_url": "pic.twitter.com/2yFhxbm8Zm",
+					"expanded_url": "https://twitter.com/thewhippasnappa/status/1156595571211276288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1156595557885980700,
+					"id_str": "1156595557885980678",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/EA0NiDCWsAYrnG8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA0NiDCWsAYrnG8.jpg",
+					"url": "https://t.co/2yFhxbm8Zm",
+					"display_url": "pic.twitter.com/2yFhxbm8Zm",
+					"expanded_url": "https://twitter.com/thewhippasnappa/status/1156595571211276288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1156595557890232300,
+					"id_str": "1156595557890232320",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/EA0NiDDXkAAUaMN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA0NiDDXkAAUaMN.jpg",
+					"url": "https://t.co/2yFhxbm8Zm",
+					"display_url": "pic.twitter.com/2yFhxbm8Zm",
+					"expanded_url": "https://twitter.com/thewhippasnappa/status/1156595571211276288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 236460454,
+			"id_str": "236460454",
+			"name": "josh",
+			"screen_name": "thewhippasnappa",
+			"location": "london, england",
+			"description": "22 year old aspiring events/media somebody, socialist, tv & film lover. (he/him)",
+			"url": "https://t.co/ERCxArTTEn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ERCxArTTEn",
+							"expanded_url": "http://instagram.com/whippasnappa",
+							"display_url": "instagram.com/whippasnappa",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4189,
+			"friends_count": 1725,
+			"listed_count": 73,
+			"created_at": "Mon Jan 10 16:58:06 +0000 2011",
+			"favourites_count": 44082,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 72933,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1151971584623730689/JKsKvHVO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1151971584623730689/JKsKvHVO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/236460454/1563486207",
+			"profile_link_color": "00CCFF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15819,
+		"favorite_count": 86716,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "my parents don’t have weighing scales so they weigh our gas canister with the wii fit https://t.co/2yFhxbm8Zm"
+	},
+	{
 		"created_at": "Thu Aug 01 08:50:48 +0000 2019",
 		"id": 1156849750055911400,
 		"id_str": "1156849750055911424",
