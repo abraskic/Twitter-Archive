3efdeb115df092d01fef973f49066d5090435ddb
abraskic <andrew.braskic@gmail.com>
2019-08-09 04:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76127,
-	"created_at": "Thu Aug 08 12:14:45 +0000 2019",
+	"tweets": 76128,
+	"created_at": "Thu Aug 08 20:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 08 20:07:08 +0000 2019",
+		"id": 1159556671942742000,
+		"id_str": "1159556671942742016",
+		"full_text": "@GiveMeSpace3 @theeyecollector Our national sport is essentially a bastardised version of netball/basketball. https://t.co/4rWDTCdVQC",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GiveMeSpace3",
+					"name": "High Peak Scourger",
+					"id": 913390078025719800,
+					"id_str": "913390078025719808",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "theeyecollector",
+					"name": "Hannah // absolute cyclepath \\\\",
+					"id": 1004698538704556000,
+					"id_str": "1004698538704556032",
+					"indices": [
+						14,
+						30
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4rWDTCdVQC",
+					"expanded_url": "https://youtu.be/XMZYZcoAcU0",
+					"display_url": "youtu.be/XMZYZcoAcU0",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1159490702918787000,
+		"in_reply_to_status_id_str": "1159490702918787073",
+		"in_reply_to_user_id": 913390078025719800,
+		"in_reply_to_user_id_str": "913390078025719808",
+		"in_reply_to_screen_name": "GiveMeSpace3",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": "https://t.co/bH9IVISfAv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVISfAv",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 68,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3047,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75170,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@GiveMeSpace3 @theeyecollector Our national sport is essentially a bastardised version of netball/basketball. https://t.co/4rWDTCdVQC"
+	},
+	{
 		"created_at": "Thu Aug 08 11:43:59 +0000 2019",
 		"id": 1159430047930863600,
 		"id_str": "1159430047930863616",
