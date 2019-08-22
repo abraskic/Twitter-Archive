024ede110fdbd93d68a1ca3cf6f79582457b6b89
abraskic <andrew.braskic@gmail.com>
2019-08-22 14:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76157,
-	"created_at": "Wed Aug 21 02:14:46 +0000 2019",
+	"tweets": 76158,
+	"created_at": "Thu Aug 22 06:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 22 05:59:48 +0000 2019",
+		"id": 1163877036345180200,
+		"id_str": "1164416862769180672",
+		"full_text": "A time lapse of the night sky with a twist. The milky way is fixed, while the earth rotates. https://t.co/5iMxw8bN43",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1163876991080312800,
+					"id_str": "1163876991080312832",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1163876991080312832/pu/img/EDuFGHYCz5cEY60-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1163876991080312832/pu/img/EDuFGHYCz5cEY60-.jpg",
+					"url": "https://t.co/5iMxw8bN43",
+					"display_url": "pic.twitter.com/5iMxw8bN43",
+					"expanded_url": "https://twitter.com/skepteis/status/1163877036345180165/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1163876991080312800,
+					"id_str": "1163876991080312832",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1163876991080312832/pu/img/EDuFGHYCz5cEY60-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1163876991080312832/pu/img/EDuFGHYCz5cEY60-.jpg",
+					"url": "https://t.co/5iMxw8bN43",
+					"display_url": "pic.twitter.com/5iMxw8bN43",
+					"expanded_url": "https://twitter.com/skepteis/status/1163877036345180165/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							3,
+							2
+						],
+						"duration_millis": 19937,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1163876991080312832/pu/vid/960x640/BZyFgl3_P6hxhiBz.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1163876991080312832/pu/pl/nCAI8ENnkwMtArY4.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1163876991080312832/pu/vid/540x360/7tg4gR7O4x4uRe5N.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1163876991080312832/pu/vid/404x270/uJSFQ7HDVN-sXEej.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 824525636719022100,
+			"id_str": "824525636719022080",
+			"name": "Daniel Probst",
+			"screen_name": "skepteis",
+			"location": "Switzerland",
+			"description": "Computer scientist doing a PhD in cheminformatics. Interested in all things scientific. Skeptic. Big fan of cats, Punk Rock, and Hockey.",
+			"url": "https://t.co/8dVsoeJ1Sc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8dVsoeJ1Sc",
+							"expanded_url": "https://danielprobst.net/aboutme/",
+							"display_url": "danielprobst.net/aboutme/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 944,
+			"friends_count": 1094,
+			"listed_count": 14,
+			"created_at": "Thu Jan 26 07:53:25 +0000 2017",
+			"favourites_count": 8410,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7551,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1150060100792606720/p01KWVll_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1150060100792606720/p01KWVll_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/824525636719022080/1535617205",
+			"profile_link_color": "3E9DD3",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4006,
+		"favorite_count": 13698,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A time lapse of the night sky with a twist. The milky way is fixed, while the earth rotates. https://t.co/5iMxw8bN43"
+	},
+	{
 		"created_at": "Wed Aug 21 02:02:44 +0000 2019",
 		"id": 1163994813261213700,
 		"id_str": "1163994813261213696",
