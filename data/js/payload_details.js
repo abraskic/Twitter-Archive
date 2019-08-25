dd15cf9cb457ae94bb176543c9b703eebf455b79
abraskic <andrew.braskic@gmail.com>
2019-08-26 06:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76165,
-	"created_at": "Sun Aug 25 10:14:46 +0000 2019",
+	"tweets": 76166,
+	"created_at": "Sun Aug 25 22:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 25 21:31:26 +0000 2019",
+		"id": 1165651710397702100,
+		"id_str": "1165738479898071040",
+		"full_text": "Surprising move from the referee. https://t.co/t2Mwhpj5ij",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1165651661743775700,
+					"id_str": "1165651661743775746",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1165651661743775746/pu/img/s6HZWkdt54-LQq0U.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1165651661743775746/pu/img/s6HZWkdt54-LQq0U.jpg",
+					"url": "https://t.co/t2Mwhpj5ij",
+					"display_url": "pic.twitter.com/t2Mwhpj5ij",
+					"expanded_url": "https://twitter.com/meowlibrary/status/1165651710397702144/video/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 716,
+							"h": 402,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 716,
+							"h": 402,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1165651661743775700,
+					"id_str": "1165651661743775746",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1165651661743775746/pu/img/s6HZWkdt54-LQq0U.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1165651661743775746/pu/img/s6HZWkdt54-LQq0U.jpg",
+					"url": "https://t.co/t2Mwhpj5ij",
+					"display_url": "pic.twitter.com/t2Mwhpj5ij",
+					"expanded_url": "https://twitter.com/meowlibrary/status/1165651710397702144/video/1",
+					"type": "video",
+					"sizes": {
+						"medium": {
+							"w": 716,
+							"h": 402,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 716,
+							"h": 402,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							358,
+							201
+						],
+						"duration_millis": 10010,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1165651661743775746/pu/vid/480x270/m6mo-tnAhKZu2iim.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1165651661743775746/pu/pl/BYOg7jL6UH7p6tMl.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1165651661743775746/pu/vid/716x402/WxzSCsTsjPvWCMrD.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://autosharebot.com\" rel=\"nofollow\">AutoShareBot</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1073081825159917600,
+			"id_str": "1073081825159917568",
+			"name": "Meow Library",
+			"screen_name": "meowlibrary",
+			"location": "",
+			"description": "THE BEST parody/commentary account for cats. Sometimes we tweet non-cat things. We don't own any media. DM us for DMCA takedown/credit. DM us your pet videos!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 29720,
+			"friends_count": 506,
+			"listed_count": 195,
+			"created_at": "Thu Dec 13 05:06:58 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 47,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1073482109480665088/POaTr_MD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1073482109480665088/POaTr_MD_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 982,
+		"favorite_count": 2821,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Surprising move from the referee. https://t.co/t2Mwhpj5ij"
+	},
+	{
 		"created_at": "Sun Aug 25 09:45:09 +0000 2019",
 		"id": 1165477934125080600,
 		"id_str": "1165560735289622528",
