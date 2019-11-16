a772b28eb8f4d56464aeae32778b28f94a01e30e
abraskic <andrew.braskic@gmail.com>
2019-11-16 17:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76335,
-	"created_at": "Sat Nov 16 00:14:46 +0000 2019",
+	"tweets": 76336,
+	"created_at": "Sat Nov 16 09:14:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 16 08:56:09 +0000 2019",
+		"id": 1195451506125918200,
+		"id_str": "1195626599980232707",
+		"full_text": "Me: one Big Mac without strawberries, please\n\nMcDonald’s employee: strawberries?\n\nMe: no, thanks",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266255881,
+			"id_str": "266255881",
+			"name": "Julicorn 🦄",
+			"screen_name": "ChicksRule",
+			"location": "",
+			"description": "Humble genius and tweeter extraordinaire. Extremely normal (don’t check).  Avi by @kwalps https://t.co/sk2mN8KfoB",
+			"url": "https://t.co/XbKoF6UfPj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XbKoF6UfPj",
+							"expanded_url": "https://twitter.com/search?q=from:@ChicksRule/exclude:replies",
+							"display_url": "twitter.com/search?q=from:…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/sk2mN8KfoB",
+							"expanded_url": "https://twitter.com/search?q=from%3AChicksRule%20min_faves%3A550&s=09",
+							"display_url": "twitter.com/search?q=from%…",
+							"indices": [
+								90,
+								113
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 5655,
+			"friends_count": 1376,
+			"listed_count": 41,
+			"created_at": "Mon Mar 14 23:14:02 +0000 2011",
+			"favourites_count": 44641,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9938,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192801665193250817/ebhOQGf-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192801665193250817/ebhOQGf-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266255881/1573221322",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3976,
+		"favorite_count": 30284,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Me: one Big Mac without strawberries, please\n\nMcDonald’s employee: strawberries?\n\nMe: no, thanks"
+	},
+	{
 		"created_at": "Fri Nov 15 23:56:45 +0000 2019",
 		"id": 1195490855068397600,
 		"id_str": "1195490855068397568",
