9dc00ebe889e5af5c9b9919d1632da1757fb1bbd
abraskic <andrew.braskic@gmail.com>
2019-11-22 13:14:47 +0800

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 05:00:46 +0000 2019",
+		"id": 1197659438347239400,
+		"id_str": "1197741688501850113",
+		"full_text": "Ultimately, this had to happen (Brayden H) https://t.co/HxLQlWFn6R",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1197659395603091500,
+					"id_str": "1197659395603091457",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1197659395603091457/pu/img/5kWeaH0Wnydw4gCO.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1197659395603091457/pu/img/5kWeaH0Wnydw4gCO.jpg",
+					"url": "https://t.co/HxLQlWFn6R",
+					"display_url": "pic.twitter.com/HxLQlWFn6R",
+					"expanded_url": "https://twitter.com/BellTowerTimes/status/1197659438347239424/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 1264,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 344,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 608,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197659395603091500,
+					"id_str": "1197659395603091457",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1197659395603091457/pu/img/5kWeaH0Wnydw4gCO.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1197659395603091457/pu/img/5kWeaH0Wnydw4gCO.jpg",
+					"url": "https://t.co/HxLQlWFn6R",
+					"display_url": "pic.twitter.com/HxLQlWFn6R",
+					"expanded_url": "https://twitter.com/BellTowerTimes/status/1197659438347239424/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 1264,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 344,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 608,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							40,
+							79
+						],
+						"duration_millis": 5400,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1197659395603091457/pu/pl/_Ee107K7TCAJ63Vd.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1197659395603091457/pu/vid/640x1264/R774XMROZ1guehgt.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1197659395603091457/pu/vid/320x632/vLalIFWXQUBnQbHN.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1197659395603091457/pu/vid/360x710/FhWfs5TcVm4L4r8p.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3004178958,
+			"id_str": "3004178958",
+			"name": "The Bell Tower Times",
+			"screen_name": "BellTowerTimes",
+			"location": "Perth",
+			"description": "I write The Bell Tower Times. Embrace the Human Zoo. Keep it zesty x",
+			"url": "https://t.co/TWoqaCpo72",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TWoqaCpo72",
+							"expanded_url": "http://www.thebelltowertimes.com",
+							"display_url": "thebelltowertimes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8498,
+			"friends_count": 1256,
+			"listed_count": 31,
+			"created_at": "Sat Jan 31 02:59:21 +0000 2015",
+			"favourites_count": 8895,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12232,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/767500445514215424/fwD1Lbl2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/767500445514215424/fwD1Lbl2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3004178958/1548118974",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 76,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ultimately, this had to happen (Brayden H) https://t.co/HxLQlWFn6R"
+	},
+	{
 		"created_at": "Thu Nov 21 03:48:07 +0000 2019",
 		"id": 1197361020450164700,
 		"id_str": "1197361020450164736",
