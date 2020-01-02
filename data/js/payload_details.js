5280dfe6ebed99f11e4175ccfa2b7259caaa01ad
abraskic <andrew.braskic@gmail.com>
2020-01-02 08:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76433,
-	"created_at": "Wed Jan 01 06:14:46 +0000 2020",
+	"tweets": 76434,
+	"created_at": "Thu Jan 02 00:14:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Wed Jan 01 23:52:01 +0000 2020",
+		"id": 1212493247697707000,
+		"id_str": "1212521893728673792",
+		"full_text": "Magpie been hearing so many fire trucks that he's started to copy the sirens...\n#AustraliaBurns #AustralianBushfires #BushfireEmergency https://t.co/MZEzoq2Lxk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			135
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AustraliaBurns",
+					"indices": [
+						80,
+						95
+					]
+				},
+				{
+					"text": "AustralianBushfires",
+					"indices": [
+						96,
+						116
+					]
+				},
+				{
+					"text": "BushfireEmergency",
+					"indices": [
+						117,
+						135
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212493200897609700,
+					"id_str": "1212493200897609728",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212493200897609728/pu/img/02m-NXq2tis0bSgF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212493200897609728/pu/img/02m-NXq2tis0bSgF.jpg",
+					"url": "https://t.co/MZEzoq2Lxk",
+					"display_url": "pic.twitter.com/MZEzoq2Lxk",
+					"expanded_url": "https://twitter.com/gobbles21/status/1212493247697707008/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212493200897609700,
+					"id_str": "1212493200897609728",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212493200897609728/pu/img/02m-NXq2tis0bSgF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212493200897609728/pu/img/02m-NXq2tis0bSgF.jpg",
+					"url": "https://t.co/MZEzoq2Lxk",
+					"display_url": "pic.twitter.com/MZEzoq2Lxk",
+					"expanded_url": "https://twitter.com/gobbles21/status/1212493247697707008/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 224,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							25,
+							14
+						],
+						"duration_millis": 22600,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212493200897609728/pu/vid/400x224/HA6A9SVuA_Nr4xdk.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1212493200897609728/pu/pl/S5G7dl4aYp3mCVyh.m3u8?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 44826108,
+			"id_str": "44826108",
+			"name": "Josh",
+			"screen_name": "gobbles21",
+			"location": "Wodonga, Victoria",
+			"description": ".",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2303,
+			"friends_count": 659,
+			"listed_count": 6,
+			"created_at": "Fri Jun 05 05:04:17 +0000 2009",
+			"favourites_count": 7382,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21001,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1202782745509806081/y1BLuMGL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202782745509806081/y1BLuMGL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/44826108/1561040190",
+			"profile_link_color": "00B306",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 122,
+		"favorite_count": 160,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Magpie been hearing so many fire trucks that he's started to copy the sirens...\n#AustraliaBurns #AustralianBushfires #BushfireEmergency https://t.co/MZEzoq2Lxk"
+	},
+	{
 		"created_at": "Wed Jan 01 05:18:45 +0000 2020",
 		"id": 1212241728226259000,
 		"id_str": "1212241728226258946",
