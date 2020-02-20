aa512c16b8c9d8105dd9524a99ed5c446d64ac87
abraskic <andrew.braskic@gmail.com>
2020-02-20 15:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76529,
-	"created_at": "Wed Feb 19 10:14:45 +0000 2020",
+	"tweets": 76530,
+	"created_at": "Thu Feb 20 07:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 20 07:04:13 +0000 2020",
+		"id": 1230220956238569500,
+		"id_str": "1230387664513196032",
+		"full_text": "On train to Brugge. One of the lads let his mate print his train ticket. He printed it on an industrial machine. It scanned - from a distance. https://t.co/0UuSSVgPUC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1230220948407750700,
+					"id_str": "1230220948407750656",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ERKfb0EWkAAOBEI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERKfb0EWkAAOBEI.jpg",
+					"url": "https://t.co/0UuSSVgPUC",
+					"display_url": "pic.twitter.com/0UuSSVgPUC",
+					"expanded_url": "https://twitter.com/AndyMitten/status/1230220956238569472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1230220948407750700,
+					"id_str": "1230220948407750656",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ERKfb0EWkAAOBEI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERKfb0EWkAAOBEI.jpg",
+					"url": "https://t.co/0UuSSVgPUC",
+					"display_url": "pic.twitter.com/0UuSSVgPUC",
+					"expanded_url": "https://twitter.com/AndyMitten/status/1230220956238569472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 202519434,
+			"id_str": "202519434",
+			"name": "Andy Mitten",
+			"screen_name": "AndyMitten",
+			"location": "Barcelona/Manchester. ",
+			"description": "United We Stand editor since '89. Editor at large - FourFourTwo mag. Write for The Athletic, The National, GQ, Sunday Times, BBC, SCMP et al. Author of 11 books",
+			"url": "https://t.co/swAtOZ2wak",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/swAtOZ2wak",
+							"expanded_url": "http://www.uwsonline.com",
+							"display_url": "uwsonline.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 170816,
+			"friends_count": 1044,
+			"listed_count": 1839,
+			"created_at": "Thu Oct 14 06:31:50 +0000 2010",
+			"favourites_count": 14509,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 22287,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/903175009258139649/DFNuIDg__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/903175009258139649/DFNuIDg__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/202519434/1410432781",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 428,
+		"favorite_count": 3578,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On train to Brugge. One of the lads let his mate print his train ticket. He printed it on an industrial machine. It scanned - from a distance. https://t.co/0UuSSVgPUC"
+	},
+	{
 		"created_at": "Wed Feb 19 09:49:20 +0000 2020",
 		"id": 1230066831789973500,
 		"id_str": "1230066831789973504",
