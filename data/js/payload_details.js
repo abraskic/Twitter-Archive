c2c964cfeab569e3df573cfb607b7966e09b0a27
abraskic <andrew.braskic@gmail.com>
2020-03-22 07:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76595,
-	"created_at": "Sat Mar 21 11:14:46 +0000 2020",
+	"tweets": 76596,
+	"created_at": "Sat Mar 21 23:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,117 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sat Mar 21 22:50:40 +0000 2020",
+		"id": 1241497483022184400,
+		"id_str": "1241497483022184449",
+		"full_text": "@kristiandwyer @Transperth I watched my lost Smartrider go from Currambine to Maddington everyday for a week. Disabled it when it was in Maddington. #checkmate",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "checkmate",
+					"indices": [
+						149,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kristiandwyer",
+					"name": "Kristian Dwyer",
+					"id": 41042986,
+					"id_str": "41042986",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "Transperth",
+					"name": "Transperth",
+					"id": 15743952,
+					"id_str": "15743952",
+					"indices": [
+						15,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1241408046732992500,
+		"in_reply_to_status_id_str": "1241408046732992514",
+		"in_reply_to_user_id": 41042986,
+		"in_reply_to_user_id_str": "41042986",
+		"in_reply_to_screen_name": "kristiandwyer",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 249,
+			"friends_count": 65,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@kristiandwyer @Transperth I watched my lost Smartrider go from Currambine to Maddington everyday for a week. Disabled it when it was in Maddington. #checkmate"
+	},
+	{
 		"created_at": "Sat Mar 21 11:10:39 +0000 2020",
 		"id": 1241321317699219500,
 		"id_str": "1241321317699219458",
