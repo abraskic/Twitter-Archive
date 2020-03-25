7909f4b905e0b7015f6dbd58b1705e936171042b
abraskic <andrew.braskic@gmail.com>
2020-03-25 14:14:47 +0800

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,389 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 25 05:36:07 +0000 2020",
+		"id": 1242686680219308000,
+		"id_str": "1242686680219308032",
+		"full_text": "Not long now until the Chinese are eating weird shit again and we have our second wild source COVID-19. https://t.co/FASffrzlyH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FASffrzlyH",
+					"expanded_url": "https://twitter.com/quicktake/status/1242338060811079682",
+					"display_url": "twitter.com/quicktake/stat…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 248,
+			"friends_count": 64,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3052,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75713,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1242338060811079700,
+		"quoted_status_id_str": "1242338060811079682",
+		"quoted_status_permalink": {
+			"url": "https://t.co/FASffrzlyH",
+			"expanded": "https://twitter.com/quicktake/status/1242338060811079682",
+			"display": "twitter.com/quicktake/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Mar 24 06:30:49 +0000 2020",
+			"id": 1242338060811079700,
+			"id_str": "1242338060811079682",
+			"full_text": "China will end the lockdown of #Wuhan on April 8.\n\nThe #coronavirus restrictions in most of Hubei province will end at midnight local time. More @business: https://t.co/U2mIAj1qnW https://t.co/tjUBpQ2kGg",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				179
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Wuhan",
+						"indices": [
+							31,
+							37
+						]
+					},
+					{
+						"text": "coronavirus",
+						"indices": [
+							55,
+							67
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "business",
+						"name": "Bloomberg",
+						"id": 34713362,
+						"id_str": "34713362",
+						"indices": [
+							145,
+							154
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/U2mIAj1qnW",
+						"expanded_url": "https://trib.al/WUQciCQ",
+						"display_url": "trib.al/WUQciCQ",
+						"indices": [
+							156,
+							179
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1242337790416818200,
+						"id_str": "1242337790416818176",
+						"indices": [
+							180,
+							203
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/1242337790416818176/img/YLKX4Tf0yoVdGuUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1242337790416818176/img/YLKX4Tf0yoVdGuUa.jpg",
+						"url": "https://t.co/tjUBpQ2kGg",
+						"display_url": "pic.twitter.com/tjUBpQ2kGg",
+						"expanded_url": "https://twitter.com/QuickTake/status/1242338060811079682/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1242337790416818200,
+						"id_str": "1242337790416818176",
+						"indices": [
+							180,
+							203
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/1242337790416818176/img/YLKX4Tf0yoVdGuUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1242337790416818176/img/YLKX4Tf0yoVdGuUa.jpg",
+						"url": "https://t.co/tjUBpQ2kGg",
+						"display_url": "pic.twitter.com/tjUBpQ2kGg",
+						"expanded_url": "https://twitter.com/QuickTake/status/1242338060811079682/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 37538,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1242337790416818176/pl/hCYyG5SItudn5Af2.m3u8?tag=13"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1242337790416818176/vid/480x480/8Y8zaduEQM_MDlE-.mp4?tag=13"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1242337790416818176/vid/320x320/YtRQyH4L_XZdyRVF.mp4?tag=13"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1242337790416818176/vid/720x720/XU3rfcA1o_msf4Kv.mp4?tag=13"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 252751061,
+				"id_str": "252751061",
+				"name": "QuickTake by Bloomberg",
+				"screen_name": "QuickTake",
+				"location": "",
+				"description": "Global news the world needs today. Subscribe to our daily newsletter 👉https://t.co/Y2IWiqb6dT",
+				"url": "https://t.co/xVmBSOUvWg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/xVmBSOUvWg",
+								"expanded_url": "https://bloom.bg/31N0XzX",
+								"display_url": "bloom.bg/31N0XzX",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Y2IWiqb6dT",
+								"expanded_url": "http://bloom.bg/31N0XzX",
+								"display_url": "bloom.bg/31N0XzX",
+								"indices": [
+									70,
+									93
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 998151,
+				"friends_count": 1332,
+				"listed_count": 7334,
+				"created_at": "Tue Feb 15 20:50:58 +0000 2011",
+				"favourites_count": 1017,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 176525,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1201727640828289024/5qNzuVlU_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201727640828289024/5qNzuVlU_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/252751061/1575349172",
+				"profile_link_color": "7FDBB6",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1159,
+			"favorite_count": 2492,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Not long now until the Chinese are eating weird shit again and we have our second wild source COVID-19. https://t.co/FASffrzlyH"
+	},
+	{
 		"created_at": "Wed Mar 25 05:13:03 +0000 2020",
 		"id": 1242680875768135700,
 		"id_str": "1242680875768135683",
