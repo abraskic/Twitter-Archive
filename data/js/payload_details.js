281c6945a75ece32d8f0e0b03cc09c23513b96fd
abraskic <andrew.braskic@gmail.com>
2020-04-02 21:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76643,
-	"created_at": "Thu Apr 02 12:14:45 +0000 2020",
+	"tweets": 76644,
+	"created_at": "Thu Apr 02 13:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 12:18:10 +0000 2020",
+		"id": 1245024380561682400,
+		"id_str": "1245686961559334912",
+		"full_text": "Custom zoom background is all about the flex.\n\nLet my grandkids know their grandpa lived large. https://t.co/RckbyS8iEZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245024245660258300,
+					"id_str": "1245024245660258304",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/EUc29HtUEAAGYSA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUc29HtUEAAGYSA.jpg",
+					"url": "https://t.co/RckbyS8iEZ",
+					"display_url": "pic.twitter.com/RckbyS8iEZ",
+					"expanded_url": "https://twitter.com/Alex_Washburne/status/1245024380561682432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 683,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 387,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245024245660258300,
+					"id_str": "1245024245660258304",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/EUc29HtUEAAGYSA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUc29HtUEAAGYSA.jpg",
+					"url": "https://t.co/RckbyS8iEZ",
+					"display_url": "pic.twitter.com/RckbyS8iEZ",
+					"expanded_url": "https://twitter.com/Alex_Washburne/status/1245024380561682432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 683,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 387,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1010534825436909600,
+			"id_str": "1010534825436909568",
+			"name": "Alex Washburne",
+			"screen_name": "Alex_Washburne",
+			"location": "Bozeman, MT",
+			"description": "Biology, math, finance, sociology & more.\n\nOne man's interdisciplinary archipelago is another man's continent of expertise.\n\nScience!!!",
+			"url": "https://t.co/vsekuO9yqA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vsekuO9yqA",
+							"expanded_url": "https://www.alexwashburne.com/",
+							"display_url": "alexwashburne.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 732,
+			"friends_count": 653,
+			"listed_count": 6,
+			"created_at": "Sat Jun 23 14:47:32 +0000 2018",
+			"favourites_count": 3492,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1870,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072565924434198528/nYNcxZF1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072565924434198528/nYNcxZF1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1010534825436909568/1543512481",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 323,
+		"favorite_count": 3281,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Custom zoom background is all about the flex.\n\nLet my grandkids know their grandpa lived large. https://t.co/RckbyS8iEZ"
+	},
+	{
 		"created_at": "Thu Apr 02 12:12:15 +0000 2020",
 		"id": 1245685475731374000,
 		"id_str": "1245685475731374080",
