75ba9a5ee3f61b97d4fa9d92f1774efd779e6390
abraskic <andrew.braskic@gmail.com>
2020-04-03 17:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76644,
-	"created_at": "Thu Apr 02 13:14:46 +0000 2020",
+	"tweets": 76645,
+	"created_at": "Fri Apr 03 09:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 03 08:34:19 +0000 2020",
+		"id": 1245993019263705000,
+		"id_str": "1245993019263705090",
+		"full_text": "@Epigrammist Why not a whole case of beers? https://t.co/psfoWC2BPt",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Epigrammist",
+					"name": "Toby Wash Your Hands Keep 2m Away",
+					"id": 31065693,
+					"id_str": "31065693",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245992963575963600,
+					"id_str": "1245992963575963648",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245992963575963648/pu/img/zpnpGUySrC7QKwa-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245992963575963648/pu/img/zpnpGUySrC7QKwa-.jpg",
+					"url": "https://t.co/psfoWC2BPt",
+					"display_url": "pic.twitter.com/psfoWC2BPt",
+					"expanded_url": "https://twitter.com/braskic/status/1245993019263705090/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245992963575963600,
+					"id_str": "1245992963575963648",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245992963575963648/pu/img/zpnpGUySrC7QKwa-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245992963575963648/pu/img/zpnpGUySrC7QKwa-.jpg",
+					"url": "https://t.co/psfoWC2BPt",
+					"display_url": "pic.twitter.com/psfoWC2BPt",
+					"expanded_url": "https://twitter.com/braskic/status/1245993019263705090/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 324,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							160,
+							81
+						],
+						"duration_millis": 16420,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245992963575963648/pu/vid/532x270/1-7MkERtDQliVfcI.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245992963575963648/pu/vid/640x324/8hJrnxGr1yShraPr.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1245992963575963648/pu/pl/2yosxeGDVwi5vQ5J.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1245984443979853800,
+		"in_reply_to_status_id_str": "1245984443979853824",
+		"in_reply_to_user_id": 31065693,
+		"in_reply_to_user_id_str": "31065693",
+		"in_reply_to_screen_name": "Epigrammist",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Too cheap for the good shit, too good for the cheap shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 247,
+			"friends_count": 63,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75756,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Epigrammist Why not a whole case of beers? https://t.co/psfoWC2BPt"
+	},
+	{
 		"created_at": "Thu Apr 02 12:18:10 +0000 2020",
 		"id": 1245024380561682400,
 		"id_str": "1245686961559334912",
