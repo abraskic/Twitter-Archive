463f7fcaf053bb30200073667910a214c9b17dcf
abraskic <andrew.braskic@gmail.com>
2020-06-01 19:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76798,
-	"created_at": "Sun May 31 13:14:47 +0000 2020",
+	"tweets": 76799,
+	"created_at": "Mon Jun 01 11:14:46 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,432 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 10:41:34 +0000 2020",
+		"id": 1267405926396387300,
+		"id_str": "1267405926396387328",
+		"full_text": "Trump’s 2020 re-election campaign is weird. https://t.co/qkvbsIycxq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qkvbsIycxq",
+					"expanded_url": "https://twitter.com/breaking911/status/1267320993032388608",
+					"display_url": "twitter.com/breaking911/st…",
+					"indices": [
+						44,
+						67
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Sleep is for the week(nights)",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 244,
+			"friends_count": 64,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3103,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 75942,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1267320993032388600,
+		"quoted_status_id_str": "1267320993032388608",
+		"quoted_status_permalink": {
+			"url": "https://t.co/qkvbsIycxq",
+			"expanded": "https://twitter.com/breaking911/status/1267320993032388608",
+			"display": "twitter.com/breaking911/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 01 05:04:05 +0000 2020",
+			"id": 1267320993032388600,
+			"id_str": "1267320993032388608",
+			"full_text": "Videos continue to surface showing protesters stumbling upon pallets of bricks or pavers in areas with no construction taking place \n\n https://t.co/qOOYMsZO6n",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				158
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1266952681115050000,
+						"id_str": "1266952681115049984",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1266952681115049984/pu/img/YnmROX7jhDIFMq8Z.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1266952681115049984/pu/img/YnmROX7jhDIFMq8Z.jpg",
+						"url": "https://t.co/qOOYMsZO6n",
+						"display_url": "pic.twitter.com/qOOYMsZO6n",
+						"expanded_url": "https://twitter.com/KRHogan_NTD/status/1266952860299857921/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1266952860299858000,
+						"source_status_id_str": "1266952860299857921",
+						"source_user_id": 1212764034388250600,
+						"source_user_id_str": "1212764034388250626"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1266952681115050000,
+						"id_str": "1266952681115049984",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1266952681115049984/pu/img/YnmROX7jhDIFMq8Z.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1266952681115049984/pu/img/YnmROX7jhDIFMq8Z.jpg",
+						"url": "https://t.co/qOOYMsZO6n",
+						"display_url": "pic.twitter.com/qOOYMsZO6n",
+						"expanded_url": "https://twitter.com/KRHogan_NTD/status/1266952860299857921/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1266952860299858000,
+						"source_status_id_str": "1266952860299857921",
+						"source_user_id": 1212764034388250600,
+						"source_user_id_str": "1212764034388250626",
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 27102,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1266952681115049984/pu/pl/N-ZidJj2bbGH946H.m3u8?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1266952681115049984/pu/vid/1280x720/Ws1VQr0IpYSoePEq.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1266952681115049984/pu/vid/480x270/2HdeiI5JsjaIjCJZ.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1266952681115049984/pu/vid/640x360/ZCn3QrYcI-kYtrvq.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false,
+							"source_user": {
+								"id": 1212764034388250600,
+								"id_str": "1212764034388250626",
+								"name": "Kevin R Hogan",
+								"screen_name": "KRHogan_NTD",
+								"location": "New York, USA",
+								"description": "Reporter @News_NTD. Engaging interviews🎙Breaking news🚨Visual storytelling. My goal isn't for you to agree with me. My goal is to inform you. MS @NorthwesternU",
+								"url": "https://t.co/ekYmWvdAx2",
+								"entities": {
+									"url": {
+										"urls": [
+											{
+												"url": "https://t.co/ekYmWvdAx2",
+												"expanded_url": "https://mb.ntd.com/author/kevin-hogan",
+												"display_url": "mb.ntd.com/author/kevin-h…",
+												"indices": [
+													0,
+													23
+												]
+											}
+										]
+									},
+									"description": {
+										"urls": []
+									}
+								},
+								"protected": false,
+								"followers_count": 474,
+								"friends_count": 221,
+								"listed_count": 3,
+								"created_at": "Thu Jan 02 15:54:17 +0000 2020",
+								"favourites_count": 64,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": true,
+								"verified": false,
+								"statuses_count": 167,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": false,
+								"profile_background_color": "F5F8FA",
+								"profile_background_image_url": null,
+								"profile_background_image_url_https": null,
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/1228779642385051649/SZ4KHT7B_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/1228779642385051649/SZ4KHT7B_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/1212764034388250626/1585849736",
+								"profile_link_color": "1DA1F2",
+								"profile_sidebar_border_color": "C0DEED",
+								"profile_sidebar_fill_color": "DDEEF6",
+								"profile_text_color": "333333",
+								"profile_use_background_image": true,
+								"has_extended_profile": true,
+								"default_profile": true,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 375721095,
+				"id_str": "375721095",
+				"name": "Breaking911",
+				"screen_name": "Breaking911",
+				"location": "Nashville",
+				"description": "Breaking news in real time. Turn on notifications & visit https://t.co/RWBI1f2gDw to stay updated | Follow us on Facebook & Instagram",
+				"url": "https://t.co/RWBI1f2gDw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/RWBI1f2gDw",
+								"expanded_url": "http://Breaking911.com",
+								"display_url": "Breaking911.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/RWBI1f2gDw",
+								"expanded_url": "http://Breaking911.com",
+								"display_url": "Breaking911.com",
+								"indices": [
+									58,
+									81
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 671392,
+				"friends_count": 0,
+				"listed_count": 4673,
+				"created_at": "Sun Sep 18 16:36:06 +0000 2011",
+				"favourites_count": 2,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 360,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/619546088995979264/KuG27bBK_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/619546088995979264/KuG27bBK_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/375721095/1496844300",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8049,
+			"favorite_count": 12326,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Trump’s 2020 re-election campaign is weird. https://t.co/qkvbsIycxq"
+	}
+]
