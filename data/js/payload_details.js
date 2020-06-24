7f5fc242b291767566b4f0633a0c531f6649b591
abraskic <andrew.braskic@gmail.com>
2020-06-24 20:14:48 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76850,
-	"created_at": "Wed Jun 24 02:14:46 +0000 2020",
+	"tweets": 76851,
+	"created_at": "Wed Jun 24 12:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 24 11:27:26 +0000 2020",
+		"id": 1275710581820076000,
+		"id_str": "1275752387148308481",
+		"full_text": "is this good? https://t.co/XnIbDIJduW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1275710572013719600,
+					"id_str": "1275710572013719552",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/EbQ8AZkWkAAQw6I.png",
+					"media_url_https": "https://pbs.twimg.com/media/EbQ8AZkWkAAQw6I.png",
+					"url": "https://t.co/XnIbDIJduW",
+					"display_url": "pic.twitter.com/XnIbDIJduW",
+					"expanded_url": "https://twitter.com/dannolan/status/1275710581820076032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1275710572013719600,
+					"id_str": "1275710572013719552",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/EbQ8AZkWkAAQw6I.png",
+					"media_url_https": "https://pbs.twimg.com/media/EbQ8AZkWkAAQw6I.png",
+					"url": "https://t.co/XnIbDIJduW",
+					"display_url": "pic.twitter.com/XnIbDIJduW",
+					"expanded_url": "https://twitter.com/dannolan/status/1275710581820076032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 478,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 971761,
+			"id_str": "971761",
+			"name": "dan nolan",
+			"screen_name": "dannolan",
+			"location": "Sydney",
+			"description": "fresh fish simply cooked",
+			"url": "https://t.co/5jQiwwyzX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5jQiwwyzX6",
+							"expanded_url": "http://instagram.com/dannolan",
+							"display_url": "instagram.com/dannolan",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21110,
+			"friends_count": 2325,
+			"listed_count": 343,
+			"created_at": "Mon Mar 12 01:14:36 +0000 2007",
+			"favourites_count": 93071,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1155413896972234754/RyEHEpC6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155413896972234754/RyEHEpC6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/971761/1574758018",
+			"profile_link_color": "EE22B0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 69,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "is this good? https://t.co/XnIbDIJduW"
+	},
+	{
 		"created_at": "Wed Jun 24 01:58:51 +0000 2020",
 		"id": 1275609298140192800,
 		"id_str": "1275609298140192768",
