b1c850d862d965c827652c2f065dc1f39d42b8cf
abraskic <andrew.braskic@gmail.com>
2020-09-09 18:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76970,
-	"created_at": "Sun Sep 06 11:14:46 +0000 2020",
+	"tweets": 76971,
+	"created_at": "Wed Sep 09 10:14:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Wed Sep 09 09:55:23 +0000 2020",
+		"id": 1303633087721386000,
+		"id_str": "1303633087721385986",
+		"full_text": "This year's AFL Grand Final is going to be lit. https://t.co/NhmniY1Aoj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NhmniY1Aoj",
+					"expanded_url": "https://twitter.com/AnnastaciaMP/status/1303478409285128192",
+					"display_url": "twitter.com/AnnastaciaMP/s…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Sleep is for the week(nights and also weekends)",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 240,
+			"friends_count": 65,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3151,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76123,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1303478409285128200,
+		"quoted_status_id_str": "1303478409285128192",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NhmniY1Aoj",
+			"expanded": "https://twitter.com/AnnastaciaMP/status/1303478409285128192",
+			"display": "twitter.com/AnnastaciaMP/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 08 23:40:44 +0000 2020",
+			"id": 1303478409285128200,
+			"id_str": "1303478409285128192",
+			"full_text": "Wednesday, 9 September – coronavirus cases in Queensland:\n\n• 8 new confirmed cases\n• 29 active cases\n• 1,143 total confirmed cases\n• 990,944 tests conducted\n\nSadly, six Queenslanders with COVID-19 have died. 1,091 patients have recovered.\n\n#covid19au https://t.co/U3r37Ud59U",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				250
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "covid19au",
+						"indices": [
+							240,
+							250
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1303477160003289000,
+						"id_str": "1303477160003289089",
+						"indices": [
+							251,
+							274
+						],
+						"media_url": "http://pbs.twimg.com/media/EhbhkibUwAEdq6e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EhbhkibUwAEdq6e.jpg",
+						"url": "https://t.co/U3r37Ud59U",
+						"display_url": "pic.twitter.com/U3r37Ud59U",
+						"expanded_url": "https://twitter.com/AnnastaciaMP/status/1303478409285128192/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1303477160003289000,
+						"id_str": "1303477160003289089",
+						"indices": [
+							251,
+							274
+						],
+						"media_url": "http://pbs.twimg.com/media/EhbhkibUwAEdq6e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EhbhkibUwAEdq6e.jpg",
+						"url": "https://t.co/U3r37Ud59U",
+						"display_url": "pic.twitter.com/U3r37Ud59U",
+						"expanded_url": "https://twitter.com/AnnastaciaMP/status/1303478409285128192/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 256833444,
+				"id_str": "256833444",
+				"name": "Annastacia Palaszczuk",
+				"screen_name": "AnnastaciaMP",
+				"location": "Queensland",
+				"description": "Premier of Queensland, Minister for Trade and Member for Inala",
+				"url": "https://t.co/IAWQCpMphk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IAWQCpMphk",
+								"expanded_url": "http://www.thepremier.qld.gov.au",
+								"display_url": "thepremier.qld.gov.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 143426,
+				"friends_count": 1230,
+				"listed_count": 533,
+				"created_at": "Thu Feb 24 04:47:26 +0000 2011",
+				"favourites_count": 2804,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 10682,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5A4A4",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1280780894513422336/xAmyw1eU_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1280780894513422336/xAmyw1eU_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/256833444/1594196936",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 55,
+			"favorite_count": 125,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This year's AFL Grand Final is going to be lit. https://t.co/NhmniY1Aoj"
+	},
+	{
 		"created_at": "Sun Sep 06 10:21:19 +0000 2020",
 		"id": 1302552450742644700,
 		"id_str": "1302552450742644739",
