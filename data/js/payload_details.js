bcddb694fe81f672aa55660405eb5be532fc6794
abraskic <andrew.braskic@gmail.com>
2020-09-14 09:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76977,
-	"created_at": "Sun Sep 13 10:14:45 +0000 2020",
+	"tweets": 76978,
+	"created_at": "Mon Sep 14 01:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 14 00:33:18 +0000 2020",
+		"id": 1305276711236329500,
+		"id_str": "1305303575405293568",
+		"full_text": "This is insane, a buck fifty a kilo? https://t.co/ylYXsP4jxE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1305276669704298500,
+					"id_str": "1305276669704298497",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/Eh1GNydUYAEJ2i0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eh1GNydUYAEJ2i0.jpg",
+					"url": "https://t.co/ylYXsP4jxE",
+					"display_url": "pic.twitter.com/ylYXsP4jxE",
+					"expanded_url": "https://twitter.com/GordyPls/status/1305276711236329472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1305276669704298500,
+					"id_str": "1305276669704298497",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/Eh1GNydUYAEJ2i0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eh1GNydUYAEJ2i0.jpg",
+					"url": "https://t.co/ylYXsP4jxE",
+					"display_url": "pic.twitter.com/ylYXsP4jxE",
+					"expanded_url": "https://twitter.com/GordyPls/status/1305276711236329472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16676087,
+			"id_str": "16676087",
+			"name": "Cherished Dad",
+			"screen_name": "GordyPls",
+			"location": "The Sydney Cinematic Universe",
+			"description": "Digital technology, basketball, cooking and eating, tinkerer, papist, pretengineer.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7530,
+			"friends_count": 1888,
+			"listed_count": 275,
+			"created_at": "Thu Oct 09 23:41:08 +0000 2008",
+			"favourites_count": 14999,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 199754,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1302195242058174464/wXJVMawG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1302195242058174464/wXJVMawG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16676087/1599302515",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is insane, a buck fifty a kilo? https://t.co/ylYXsP4jxE"
+	},
+	{
 		"created_at": "Sun Sep 13 09:25:10 +0000 2020",
 		"id": 1305075034709348400,
 		"id_str": "1305075034709348352",
