3ed291448dd57d7eed2483d6d95c40c1b003fe13
abraskic <andrew.braskic@gmail.com>
2020-09-29 18:14:49 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77000,
-	"created_at": "Tue Sep 29 04:14:47 +0000 2020",
+	"tweets": 77001,
+	"created_at": "Tue Sep 29 10:14:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 29 09:34:30 +0000 2020",
+		"id": 1310875589377626000,
+		"id_str": "1310875589377626114",
+		"full_text": "@Glaven1994 @fictillius Still closed. All Victorian arrivals had to go into mandatory hotel quarantine. This change just means you can serve your quarantine at a WA residential address.",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			185
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Glaven1994",
+					"name": "Hotel Quarantine Crab 12/14 🇦🇺 🦠",
+					"id": 969828390306902000,
+					"id_str": "969828390306902022",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "fictillius",
+					"name": "🦠 Evan",
+					"id": 7050962,
+					"id_str": "7050962",
+					"indices": [
+						12,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1310865015046463500,
+		"in_reply_to_status_id_str": "1310865015046463491",
+		"in_reply_to_user_id": 969828390306902000,
+		"in_reply_to_user_id_str": "969828390306902022",
+		"in_reply_to_screen_name": "Glaven1994",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Sleep is for the week(nights and also weekends)",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 240,
+			"friends_count": 65,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76157,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Glaven1994 @fictillius Still closed. All Victorian arrivals had to go into mandatory hotel quarantine. This change just means you can serve your quarantine at a WA residential address."
+	},
+	{
 		"created_at": "Tue Sep 29 03:57:57 +0000 2020",
 		"id": 1310790894660890600,
 		"id_str": "1310790894660890625",
