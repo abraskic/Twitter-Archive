9237da69b654319687cd89d81914730303e916e9
abraskic <andrew.braskic@gmail.com>
2020-11-01 17:14:47 +0800

    Added 1 tweets

---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,244 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 01 08:43:18 +0000 2020",
+		"id": 1322298104255815700,
+		"id_str": "1322821506263445506",
+		"full_text": "This is what Election Day prep looks like in downtown D.C. #Election2020 https://t.co/EbKSjtLsFi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Election2020",
+					"indices": [
+						59,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1322298097670697000,
+					"id_str": "1322298097670696965",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/Elm_HS7WMAUTSPg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Elm_HS7WMAUTSPg.jpg",
+					"url": "https://t.co/EbKSjtLsFi",
+					"display_url": "pic.twitter.com/EbKSjtLsFi",
+					"expanded_url": "https://twitter.com/dberrygannett/status/1322298104255815681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1322298097670697000,
+					"id_str": "1322298097670696965",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/Elm_HS7WMAUTSPg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Elm_HS7WMAUTSPg.jpg",
+					"url": "https://t.co/EbKSjtLsFi",
+					"display_url": "pic.twitter.com/EbKSjtLsFi",
+					"expanded_url": "https://twitter.com/dberrygannett/status/1322298104255815681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1322298098715078700,
+					"id_str": "1322298098715078663",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/Elm_HW0WMAc89od.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Elm_HW0WMAc89od.jpg",
+					"url": "https://t.co/EbKSjtLsFi",
+					"display_url": "pic.twitter.com/EbKSjtLsFi",
+					"expanded_url": "https://twitter.com/dberrygannett/status/1322298104255815681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1322298099725992000,
+					"id_str": "1322298099725991936",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/Elm_HalXgAAuUjf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Elm_HalXgAAuUjf.jpg",
+					"url": "https://t.co/EbKSjtLsFi",
+					"display_url": "pic.twitter.com/EbKSjtLsFi",
+					"expanded_url": "https://twitter.com/dberrygannett/status/1322298104255815681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 239974061,
+			"id_str": "239974061",
+			"name": "Deborah Berry",
+			"screen_name": "dberrygannett",
+			"location": "Washington, D.C.",
+			"description": "National correspondent for USA TODAY covering civil rights, voting rights. Used to cover Deep South. Girl Scouts leader, @UofMaryland grad, @washpressclub sec",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4615,
+			"friends_count": 706,
+			"listed_count": 210,
+			"created_at": "Tue Jan 18 21:38:55 +0000 2011",
+			"favourites_count": 6315,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11607,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3641203642/b827cecccb17084fd3615b49f4054c87_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3641203642/b827cecccb17084fd3615b49f4054c87_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/239974061/1438101531",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 256,
+		"favorite_count": 587,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is what Election Day prep looks like in downtown D.C. #Election2020 https://t.co/EbKSjtLsFi"
+	}
+]
