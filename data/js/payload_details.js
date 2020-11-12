3ce7ea97a47b8d661ce86c1231f525f846c8b925
abraskic <andrew.braskic@gmail.com>
2020-11-12 19:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77065,
-	"created_at": "Wed Nov 11 09:14:46 +0000 2020",
+	"tweets": 77066,
+	"created_at": "Thu Nov 12 11:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 12 10:20:06 +0000 2020",
+		"id": 1326832132174487600,
+		"id_str": "1326832132174487553",
+		"full_text": "@BeauGiles Careful with those donuts otherwise you will end up like us sandgropers. https://t.co/WyNgw4vQeW",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BeauGiles",
+					"name": "Beau 🏳️‍🌈",
+					"id": 7636802,
+					"id_str": "7636802",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1326832109374201900,
+					"id_str": "1326832109374201858",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/EmnaxgvU4AIPbKq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EmnaxgvU4AIPbKq.jpg",
+					"url": "https://t.co/WyNgw4vQeW",
+					"display_url": "pic.twitter.com/WyNgw4vQeW",
+					"expanded_url": "https://twitter.com/braskic/status/1326832132174487553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1326832109374201900,
+					"id_str": "1326832109374201858",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/EmnaxgvU4AIPbKq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EmnaxgvU4AIPbKq.jpg",
+					"url": "https://t.co/WyNgw4vQeW",
+					"display_url": "pic.twitter.com/WyNgw4vQeW",
+					"expanded_url": "https://twitter.com/braskic/status/1326832132174487553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1326706500476137500,
+		"in_reply_to_status_id_str": "1326706500476137473",
+		"in_reply_to_user_id": 7636802,
+		"in_reply_to_user_id_str": "7636802",
+		"in_reply_to_screen_name": "BeauGiles",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Sleep is for the week(nights and also weekends)",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 237,
+			"friends_count": 64,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3186,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76233,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@BeauGiles Careful with those donuts otherwise you will end up like us sandgropers. https://t.co/WyNgw4vQeW"
+	},
+	{
 		"created_at": "Wed Nov 11 09:05:40 +0000 2020",
 		"id": 1326451014291976200,
 		"id_str": "1326451014291976192",
