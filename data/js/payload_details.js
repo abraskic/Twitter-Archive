3665b0320b81532828991654af1c6ca2471a2ac5
abraskic <andrew.braskic@gmail.com>
2020-11-14 14:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77068,
-	"created_at": "Sat Nov 14 04:14:45 +0000 2020",
+	"tweets": 77069,
+	"created_at": "Sat Nov 14 06:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 14 05:22:48 +0000 2020",
+		"id": 1327470027029639200,
+		"id_str": "1327482092200988675",
+		"full_text": "The best way to stop a bad guy with a COVID is a good guy with a COVID",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 765786,
+			"id_str": "765786",
+			"name": "Christopher Owen 🦪",
+			"screen_name": "oysta",
+			"location": "Sydney, Australia",
+			"description": "Software architect, manager, and self flagellating @NRLKnights member. Neque Interfectorem Omnibus Impletionem #Nintendo4Lyfe 🦪",
+			"url": "https://t.co/tCFPyCKBf9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tCFPyCKBf9",
+							"expanded_url": "https://christopherowen.id.au/",
+							"display_url": "christopherowen.id.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 613,
+			"friends_count": 346,
+			"listed_count": 41,
+			"created_at": "Sun Feb 11 22:52:53 +0000 2007",
+			"favourites_count": 53266,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 85748,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1322142968124305414/qj5Z6nQd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1322142968124305414/qj5Z6nQd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/765786/1602022200",
+			"profile_link_color": "876A87",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The best way to stop a bad guy with a COVID is a good guy with a COVID"
+	},
+	{
 		"created_at": "Sat Nov 14 03:33:42 +0000 2020",
 		"id": 1327454636551204900,
 		"id_str": "1327454636551204864",
