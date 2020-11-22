c88a72992e2f10d14069d9bfbef6fc980334dab5
abraskic <andrew.braskic@gmail.com>
2020-11-22 18:14:47 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77085,
-	"created_at": "Sun Nov 22 01:14:46 +0000 2020",
+	"tweets": 77087,
+	"created_at": "Sun Nov 22 10:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sun Nov 22 09:58:02 +0000 2020",
+		"id": 1330450459170136000,
+		"id_str": "1330450459170136064",
+		"full_text": "@rosanne_89 No chance. McGowan is now saying VIC could have even remain off limits later this week despite their 23 days of no cases.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rosanne_89",
+					"name": "Rosanne",
+					"id": 170572517,
+					"id_str": "170572517",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1330395980664107000,
+		"in_reply_to_status_id_str": "1330395980664107009",
+		"in_reply_to_user_id": 170572517,
+		"in_reply_to_user_id_str": "170572517",
+		"in_reply_to_screen_name": "rosanne_89",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Banksia Grove, Australia",
+			"description": "Sleep is for the week(nights and also weekends)",
+			"url": "https://t.co/bH9IVIAEbV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bH9IVIAEbV",
+							"expanded_url": "http://brask.cc/",
+							"display_url": "brask.cc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 236,
+			"friends_count": 65,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3185,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76260,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@rosanne_89 No chance. McGowan is now saying VIC could have even remain off limits later this week despite their 23 days of no cases."
+	},
+	{
+		"created_at": "Sun Nov 22 09:50:43 +0000 2020",
+		"id": 1330309254801219600,
+		"id_str": "1330448617421893635",
+		"full_text": "Just to reiterate:\n\nInside Bunnings = mask\nBunnings carpark if you can keep 1.5m apart = no mask\nIn the Bunnings sausage sizzle line = mask\nOnions = on top of the sausage\n\nI can't be clearer than that.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			201
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 228535666,
+			"id_str": "228535666",
+			"name": "Dan Andrews",
+			"screen_name": "DanielAndrewsMP",
+			"location": "Melbourne, Australia",
+			"description": "Premier of Victoria.",
+			"url": "https://t.co/HhLhQASoqi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HhLhQASoqi",
+							"expanded_url": "http://www.danandrews.com.au",
+							"display_url": "danandrews.com.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 295714,
+			"friends_count": 3103,
+			"listed_count": 793,
+			"created_at": "Mon Dec 20 00:56:03 +0000 2010",
+			"favourites_count": 2054,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6863,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C2082D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1296611128504545280/Z2Ijn7V-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1296611128504545280/Z2Ijn7V-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/228535666/1542511812",
+			"profile_link_color": "BF2D1A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3809,
+		"favorite_count": 30552,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Just to reiterate:\n\nInside Bunnings = mask\nBunnings carpark if you can keep 1.5m apart = no mask\nIn the Bunnings sausage sizzle line = mask\nOnions = on top of the sausage\n\nI can't be clearer than that."
+	},
+	{
 		"created_at": "Sun Nov 22 00:35:26 +0000 2020",
 		"id": 1330308875648704500,
 		"id_str": "1330308875648704518",
