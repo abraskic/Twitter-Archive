374a0545e52d65d6e1c59d01b6487196f2eea987
abraskic <andrew.braskic@gmail.com>
2020-12-12 21:14:47 +0800

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sat Dec 12 12:36:50 +0000 2020",
+		"id": 1337515711359557600,
+		"id_str": "1337738178195681280",
+		"full_text": "This is the best meme that’s ever been crafted, and whoever made it is genius https://t.co/9cXHu6cMjy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1337515708662550500,
+					"id_str": "1337515708662550528",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo_Pc1hWEAAcE8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo_Pc1hWEAAcE8w.jpg",
+					"url": "https://t.co/9cXHu6cMjy",
+					"display_url": "pic.twitter.com/9cXHu6cMjy",
+					"expanded_url": "https://twitter.com/js_thrill/status/1337515711359557633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 897,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 897,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 546,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1337515708662550500,
+					"id_str": "1337515708662550528",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo_Pc1hWEAAcE8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo_Pc1hWEAAcE8w.jpg",
+					"url": "https://t.co/9cXHu6cMjy",
+					"display_url": "pic.twitter.com/9cXHu6cMjy",
+					"expanded_url": "https://twitter.com/js_thrill/status/1337515711359557633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 897,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 897,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 546,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9026112,
+			"id_str": "9026112",
+			"name": "john stuart yggdrasil 🌳",
+			"screen_name": "js_thrill",
+			"location": "Buffalo, NY",
+			"description": "associate professor of philosophy, superlatively online, i mostly make jokes about early modern philosophers, he/him",
+			"url": "https://t.co/QjzVtcHFPq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QjzVtcHFPq",
+							"expanded_url": "https://understanding-humans.com/",
+							"display_url": "understanding-humans.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3151,
+			"friends_count": 923,
+			"listed_count": 90,
+			"created_at": "Sat Sep 22 03:05:47 +0000 2007",
+			"favourites_count": 11134,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41485,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1176503655643648002/90nRSNMW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1176503655643648002/90nRSNMW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9026112/1601128308",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 557,
+		"favorite_count": 3417,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is the best meme that’s ever been crafted, and whoever made it is genius https://t.co/9cXHu6cMjy"
+	},
+	{
 		"created_at": "Sat Dec 12 01:31:43 +0000 2020",
 		"id": 1337570795887812600,
 		"id_str": "1337570795887812611",
