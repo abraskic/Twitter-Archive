30594019cc8f4031b9e098b5138f576f07940c00
abraskic <andrew.braskic@gmail.com>
2020-12-21 16:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77169,
-	"created_at": "Mon Dec 21 03:14:46 +0000 2020",
+	"tweets": 77170,
+	"created_at": "Mon Dec 21 08:14:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 21 08:13:16 +0000 2020",
+		"id": 1340367451737886700,
+		"id_str": "1340933340015161344",
+		"full_text": "I put on some happy hardcore for the bird https://t.co/ohL3ThY0sm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1340367267284963300,
+					"id_str": "1340367267284963328",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1340367267284963328/pu/img/FMY-tYY75KeWvEh6.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1340367267284963328/pu/img/FMY-tYY75KeWvEh6.jpg",
+					"url": "https://t.co/ohL3ThY0sm",
+					"display_url": "pic.twitter.com/ohL3ThY0sm",
+					"expanded_url": "https://twitter.com/xxllthxx/status/1340367451737886720/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1340367267284963300,
+					"id_str": "1340367267284963328",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1340367267284963328/pu/img/FMY-tYY75KeWvEh6.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1340367267284963328/pu/img/FMY-tYY75KeWvEh6.jpg",
+					"url": "https://t.co/ohL3ThY0sm",
+					"display_url": "pic.twitter.com/ohL3ThY0sm",
+					"expanded_url": "https://twitter.com/xxllthxx/status/1340367451737886720/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 22356,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340367267284963328/pu/vid/480x270/twKWekcR86AFYQVZ.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1340367267284963328/pu/pl/6fHnXoaYJ1gOVqP-.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340367267284963328/pu/vid/1280x720/_yRy8mdtWuaSD5Iw.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340367267284963328/pu/vid/640x360/m3kQuOeaW2O4UgX1.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 348022122,
+			"id_str": "348022122",
+			"name": "LLTH | 3⚧️3 | 🙂",
+			"screen_name": "xxllthxx",
+			"location": "she/her",
+			"description": "Felix RS | I design synthesizers and make gabber | instagram: @/xllthx",
+			"url": "https://t.co/iG9KZgiEFv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iG9KZgiEFv",
+							"expanded_url": "https://soundcloud.com/xllthx",
+							"display_url": "soundcloud.com/xllthx",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 179,
+			"listed_count": 7,
+			"created_at": "Wed Aug 03 19:06:43 +0000 2011",
+			"favourites_count": 57499,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4024,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1313978462693187585/Oiho0aDK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1313978462693187585/Oiho0aDK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/348022122/1576428493",
+			"profile_link_color": "555555",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7893,
+		"favorite_count": 27589,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I put on some happy hardcore for the bird https://t.co/ohL3ThY0sm"
+	},
+	{
 		"created_at": "Mon Dec 21 02:49:36 +0000 2020",
 		"id": 1340851887831052300,
 		"id_str": "1340851887831052288",
