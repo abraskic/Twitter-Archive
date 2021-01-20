8d7d1c5efdd432faf0f8ea5366d4fb5e115e27c0
abraskic <andrew.braskic@gmail.com>
2021-01-20 13:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77243,
-	"created_at": "Wed Jan 20 00:14:46 +0000 2021",
+	"tweets": 77244,
+	"created_at": "Wed Jan 20 05:14:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 20 04:34:42 +0000 2021",
+		"id": 1351682031478927400,
+		"id_str": "1351749971410710530",
+		"full_text": "mfw when a pr person calls me to follow up on the pitch they emailed to me two times https://t.co/VCz26uxroD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1351681498315780000,
+					"id_str": "1351681498315780097",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EsIjKUSUcAEwMDK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EsIjKUSUcAEwMDK.jpg",
+					"url": "https://t.co/VCz26uxroD",
+					"display_url": "pic.twitter.com/VCz26uxroD",
+					"expanded_url": "https://twitter.com/cameronwilson/status/1351682031478927360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1022,
+							"h": 574,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1022,
+							"h": 574,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1351681498315780000,
+					"id_str": "1351681498315780097",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EsIjKUSUcAEwMDK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EsIjKUSUcAEwMDK.jpg",
+					"url": "https://t.co/VCz26uxroD",
+					"display_url": "pic.twitter.com/VCz26uxroD",
+					"expanded_url": "https://twitter.com/cameronwilson/status/1351682031478927360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1022,
+							"h": 574,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1022,
+							"h": 574,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15055120,
+			"id_str": "15055120",
+			"name": "CAMERONWILSON",
+			"screen_name": "cameronwilson",
+			"location": "",
+			"description": "internet reporter @BIAUS/@GizmodoAU via @BuzzFeedNews & @ABCNews. E: cmwlsn@protonmail.com he/him",
+			"url": "https://t.co/Z8ofFGEmRX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z8ofFGEmRX",
+							"expanded_url": "https://linktr.ee/camwilson",
+							"display_url": "linktr.ee/camwilson",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11529,
+			"friends_count": 4126,
+			"listed_count": 114,
+			"created_at": "Mon Jun 09 07:20:27 +0000 2008",
+			"favourites_count": 43527,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15002,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1268382907690713088/-WeRtir5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1268382907690713088/-WeRtir5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15055120/1510137440",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "mfw when a pr person calls me to follow up on the pitch they emailed to me two times https://t.co/VCz26uxroD"
+	},
+	{
 		"created_at": "Tue Jan 19 23:43:35 +0000 2021",
 		"id": 1351676712396886000,
 		"id_str": "1351676712396886016",
