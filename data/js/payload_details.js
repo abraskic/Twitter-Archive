1ecd5bc8f90c3e14bd3737479f21ebc5f178e17a
abraskic <andrew.braskic@gmail.com>
2021-06-25 12:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77483,
-	"created_at": "Fri Jun 25 01:14:45 +0000 2021",
+	"tweets": 77484,
+	"created_at": "Fri Jun 25 04:14:51 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 25 03:53:19 +0000 2021",
+		"id": 1408272065384247300,
+		"id_str": "1408272065384247297",
+		"full_text": "My sources say there was a Perth Uber Driver at the Sydney super-spreading party. Grab your toilet paper while you still can. 👀 https://t.co/9oRfci0ySo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9oRfci0ySo",
+					"expanded_url": "https://twitter.com/whattimemark/status/1408270334608826370",
+					"display_url": "twitter.com/whattimemark/s…",
+					"indices": [
+						128,
+						151
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perth, Western Australia",
+			"description": "I am not who you think I am; I am not who I think I am; I am who I think you think I am.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 238,
+			"friends_count": 70,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3340,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1408270334608826400,
+		"quoted_status_id_str": "1408270334608826370",
+		"quoted_status_permalink": {
+			"url": "https://t.co/9oRfci0ySo",
+			"expanded": "https://twitter.com/whattimemark/status/1408270334608826370",
+			"display": "twitter.com/whattimemark/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 25 03:46:26 +0000 2021",
+			"id": 1408270334608826400,
+			"id_str": "1408270334608826370",
+			"full_text": "The Premier is scheduled to hold a press conference at midday. #perthnews",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				73
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "perthnews",
+						"indices": [
+							63,
+							73
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1342609787226259500,
+				"id_str": "1342609787226259457",
+				"name": "What time is Mark McGowan’s press conference?",
+				"screen_name": "WhatTimeMark",
+				"location": "Perth, Western Australia",
+				"description": "Just a citizen giving you the *scheduled time of the WA Premier’s daily press conference. Times in AWST. *Mark McGowan’s punctuality not guaranteed.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1671,
+				"friends_count": 585,
+				"listed_count": 7,
+				"created_at": "Fri Dec 25 23:15:05 +0000 2020",
+				"favourites_count": 3,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 56,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1357309407013445632/D1-rTdY4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1357309407013445632/D1-rTdY4_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My sources say there was a Perth Uber Driver at the Sydney super-spreading party. Grab your toilet paper while you still can. 👀 https://t.co/9oRfci0ySo"
+	},
+	{
 		"created_at": "Fri Jun 25 00:53:12 +0000 2021",
 		"id": 1407894445463281700,
 		"id_str": "1408226739394813953",
@@ -103,10 +326,10 @@ Grailbird.data.tweets_2021_06 =
 		"user": {
 			"id": 833064800,
 			"id_str": "833064800",
-			"name": "Pat Murray 🏳️‍🌈",
+			"name": "Pat Murray 🏳️🌈",
 			"screen_name": "_patmurray",
 			"location": "a sydney boy in cupertino",
-			"description": "🦩🦆🦔📷🎹🎬⛷🏍🚲✈️ — iCloud at Apple — he/him 🏳️‍🌈",
+			"description": "🦩🦆🦔📷🎹🎬⛷🏍🚲✈️ — iCloud at Apple — he/him 🏳️🌈",
 			"url": "https://t.co/fukEmj3jiW",
 			"entities": {
 				"url": {
