a9924ac0ae3a86dab1178e4f1ca3ba2a59c532c0
abraskic <andrew.braskic@gmail.com>
2021-06-25 09:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77482,
-	"created_at": "Wed Jun 23 13:26:29 +0000 2021",
+	"tweets": 77483,
+	"created_at": "Fri Jun 25 01:14:45 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 25 00:53:12 +0000 2021",
+		"id": 1407894445463281700,
+		"id_str": "1408226739394813953",
+		"full_text": "Reach Your Peak 🌽 https://t.co/xWjBG8BCrk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1407894441541603300,
+					"id_str": "1407894441541603329",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/E4nYiQVVkAEewNZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4nYiQVVkAEewNZ.jpg",
+					"url": "https://t.co/xWjBG8BCrk",
+					"display_url": "pic.twitter.com/xWjBG8BCrk",
+					"expanded_url": "https://twitter.com/_patmurray/status/1407894445463281665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407894441541603300,
+					"id_str": "1407894441541603329",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/E4nYiQVVkAEewNZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4nYiQVVkAEewNZ.jpg",
+					"url": "https://t.co/xWjBG8BCrk",
+					"display_url": "pic.twitter.com/xWjBG8BCrk",
+					"expanded_url": "https://twitter.com/_patmurray/status/1407894445463281665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 833064800,
+			"id_str": "833064800",
+			"name": "Pat Murray 🏳️‍🌈",
+			"screen_name": "_patmurray",
+			"location": "a sydney boy in cupertino",
+			"description": "🦩🦆🦔📷🎹🎬⛷🏍🚲✈️ — iCloud at Apple — he/him 🏳️‍🌈",
+			"url": "https://t.co/fukEmj3jiW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fukEmj3jiW",
+							"expanded_url": "https://patmurray.co",
+							"display_url": "patmurray.co",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3267,
+			"friends_count": 354,
+			"listed_count": 129,
+			"created_at": "Wed Sep 19 10:52:12 +0000 2012",
+			"favourites_count": 108355,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 45201,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "7E8E9E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278768331399417856/5zsUMEFc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278768331399417856/5zsUMEFc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/833064800/1620334943",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "09B8A6",
+			"profile_text_color": "088CA6",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 30,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Reach Your Peak 🌽 https://t.co/xWjBG8BCrk"
+	},
+	{
 		"created_at": "Wed Jun 23 13:16:07 +0000 2021",
 		"id": 1407475736068432000,
 		"id_str": "1407688921111089152",
