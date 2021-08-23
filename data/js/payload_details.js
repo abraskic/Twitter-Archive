fd49848b42758ffe527513a6c9dce9e93b7e57ea
abraskic <andrew.braskic@gmail.com>
2021-08-23 19:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77546,
-	"created_at": "Sun Aug 22 05:14:45 +0000 2021",
+	"tweets": 77547,
+	"created_at": "Mon Aug 23 11:14:45 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Mon Aug 23 10:41:49 +0000 2021",
+		"id": 1429755752286068700,
+		"id_str": "1429755752286068745",
+		"full_text": "All animals are equal but some are more equal than others. https://t.co/KHgWyfFCNq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KHgWyfFCNq",
+					"expanded_url": "https://twitter.com/michaelkoziol/status/1429731470629761027",
+					"display_url": "twitter.com/michaelkoziol/…",
+					"indices": [
+						59,
+						82
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perth, Western Australia",
+			"description": "I am not who you think I am; I am not who I think I am; I am who I think you think I am.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 239,
+			"friends_count": 67,
+			"listed_count": 0,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3401,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76784,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1429731470629761000,
+		"quoted_status_id_str": "1429731470629761027",
+		"quoted_status_permalink": {
+			"url": "https://t.co/KHgWyfFCNq",
+			"expanded": "https://twitter.com/michaelkoziol/status/1429731470629761027",
+			"display": "twitter.com/michaelkoziol/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Aug 23 09:05:20 +0000 2021",
+			"id": 1429731470629761000,
+			"id_str": "1429731470629761027",
+			"full_text": "Gladys Berejiklian tells @tracygrimshaw there will be “at least one thing” fully vaccinated people can do when NSW reaches 6 million jabs that they can’t do now",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				160
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "tracygrimshaw",
+						"name": "tracy grimshaw",
+						"id": 141826134,
+						"id_str": "141826134",
+						"indices": [
+							25,
+							39
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 97548523,
+				"id_str": "97548523",
+				"name": "Michael Koziol",
+				"screen_name": "michaelkoziol",
+				"location": "Sydney",
+				"description": "Deputy editor of the SMH's Sunday edition, The Sun-Herald | Redleaf Beach bureau chief | Tips, ideas, feedback: michael.koziol@smh.com.au",
+				"url": "https://t.co/sMz6eU2RLb",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/sMz6eU2RLb",
+								"expanded_url": "http://www.smh.com.au",
+								"display_url": "smh.com.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 19936,
+				"friends_count": 2480,
+				"listed_count": 305,
+				"created_at": "Thu Dec 17 23:20:39 +0000 2009",
+				"favourites_count": 4820,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 24792,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1326127790719004672/k57DsFMN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326127790719004672/k57DsFMN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/97548523/1602836081",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15,
+			"favorite_count": 206,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "All animals are equal but some are more equal than others. https://t.co/KHgWyfFCNq"
+	},
+	{
 		"created_at": "Sun Aug 22 05:14:17 +0000 2021",
 		"id": 1429310936809021400,
 		"id_str": "1429310936809021440",
