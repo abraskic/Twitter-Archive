5102df5f3757536bae55dae6b9044b15e422fc83
abraskic <andrew.braskic@gmail.com>
2021-08-24 18:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77549,
-	"created_at": "Tue Aug 24 03:14:45 +0000 2021",
+	"tweets": 77550,
+	"created_at": "Tue Aug 24 10:14:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 24 09:38:59 +0000 2021",
+		"id": 1430049713038839800,
+		"id_str": "1430102328560873480",
+		"full_text": "i want the six million jab treat and anyone who says they are too grown up for it, i will have their treat too",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21744712,
+			"id_str": "21744712",
+			"name": "Bridie Jabour",
+			"screen_name": "bkjabour",
+			"location": "Sydney",
+			"description": "opinion editor at Guardian Australia. author of Trivial Grievances (the book, not the whingy posts). you’re doing amazing sweetie. she/her",
+			"url": "https://t.co/QJCzF9lxK9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QJCzF9lxK9",
+							"expanded_url": "https://www.booktopia.com.au/trivial-grievances-bridie-jabour/book/9781460759493.html",
+							"display_url": "booktopia.com.au/trivial-grieva…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 31910,
+			"friends_count": 3167,
+			"listed_count": 432,
+			"created_at": "Tue Feb 24 11:00:38 +0000 2009",
+			"favourites_count": 3587,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10318,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000A0F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366164572969725952/q0vfGJH-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366164572969725952/q0vfGJH-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21744712/1437135969",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 181,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "i want the six million jab treat and anyone who says they are too grown up for it, i will have their treat too"
+	},
+	{
 		"created_at": "Tue Aug 24 02:51:07 +0000 2021",
 		"id": 1429999684219674600,
 		"id_str": "1429999684219674625",
