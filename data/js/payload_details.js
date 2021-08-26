936ba069d70377a239317bd3645749c7037d8841
abraskic <andrew.braskic@gmail.com>
2021-08-26 16:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77552,
-	"created_at": "Wed Aug 25 01:14:45 +0000 2021",
+	"tweets": 77553,
+	"created_at": "Thu Aug 26 08:14:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,314 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 26 08:14:05 +0000 2021",
+		"id": 1430805736133595100,
+		"id_str": "1430805736133595136",
+		"full_text": "I’m in this tweet and I don’t like it. https://t.co/RkhfP3PYcj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RkhfP3PYcj",
+					"expanded_url": "https://twitter.com/zhaovan8/status/1430592057849040899",
+					"display_url": "twitter.com/zhaovan8/statu…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perth, Western Australia",
+			"description": "I am not who you think I am; I am not who I think I am; I am who I think you think I am.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 239,
+			"friends_count": 67,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3408,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1430592057849041000,
+		"quoted_status_id_str": "1430592057849040899",
+		"quoted_status_permalink": {
+			"url": "https://t.co/RkhfP3PYcj",
+			"expanded": "https://twitter.com/zhaovan8/status/1430592057849040899",
+			"display": "twitter.com/zhaovan8/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Aug 25 18:05:00 +0000 2021",
+			"id": 1430592057849041000,
+			"id_str": "1430592057849040899",
+			"full_text": "oh sorry i cant im busy that day https://t.co/fCqn5qDjZW",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				32
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1430396442363928600,
+						"id_str": "1430396442363928576",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/E9nJ_FsVoAAZDcs.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E9nJ_FsVoAAZDcs.jpg",
+						"url": "https://t.co/fCqn5qDjZW",
+						"display_url": "pic.twitter.com/fCqn5qDjZW",
+						"expanded_url": "https://twitter.com/zhaovan8/status/1430592057849040899/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 719,
+								"h": 1556,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 554,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 314,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1430396442363928600,
+						"id_str": "1430396442363928576",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/E9nJ_FsVoAAZDcs.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E9nJ_FsVoAAZDcs.jpg",
+						"url": "https://t.co/fCqn5qDjZW",
+						"display_url": "pic.twitter.com/fCqn5qDjZW",
+						"expanded_url": "https://twitter.com/zhaovan8/status/1430592057849040899/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 719,
+								"h": 1556,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 554,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 314,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 748303207,
+				"id_str": "748303207",
+				"name": "ivan zhao",
+				"screen_name": "zhaovan8",
+				"location": "Seattle, WA",
+				"description": "dreaming of pastries • thinking about corgis, cities, and cognition • making trouble @dormroomfund @reboot_hq • prev @blendlabsinc @askiggyapp • 🏳️‍🌈 he/him",
+				"url": "https://t.co/4j2yarA3La",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4j2yarA3La",
+								"expanded_url": "https://ivanzhao.me",
+								"display_url": "ivanzhao.me",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1104,
+				"friends_count": 969,
+				"listed_count": 13,
+				"created_at": "Fri Aug 10 01:10:11 +0000 2012",
+				"favourites_count": 15464,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1463,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1417514676561203201/ROfk19cM_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417514676561203201/ROfk19cM_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/748303207/1626801452",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4816,
+			"favorite_count": 19467,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "I’m in this tweet and I don’t like it. https://t.co/RkhfP3PYcj"
+	},
+	{
 		"created_at": "Wed Aug 25 00:57:49 +0000 2021",
 		"id": 1430333558862717000,
 		"id_str": "1430333558862716934",
