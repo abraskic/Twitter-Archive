329a8b829961d29d72d2f3e887fcf3ece8ad3a44
abraskic <andrew.braskic@gmail.com>
2021-09-28 08:14:48 +0800

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Mon Sep 27 23:59:28 +0000 2021",
+		"id": 1442640060420034600,
+		"id_str": "1442640060420034569",
+		"full_text": "@FueledbyLOLZ Me: Such a lazy weekend, only ran 19km and swam 2k.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FueledbyLOLZ",
+					"name": "Hollie S",
+					"id": 216194230,
+					"id_str": "216194230",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1442284024341225500,
+		"in_reply_to_status_id_str": "1442284024341225477",
+		"in_reply_to_user_id": 216194230,
+		"in_reply_to_user_id_str": "216194230",
+		"in_reply_to_screen_name": "FueledbyLOLZ",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perth, Western Australia",
+			"description": "I am not who you think I am; I am not who I think I am; I am who I think you think I am.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 238,
+			"friends_count": 68,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3429,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76841,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@FueledbyLOLZ Me: Such a lazy weekend, only ran 19km and swam 2k."
+	},
+	{
 		"created_at": "Mon Sep 27 12:33:25 +0000 2021",
 		"id": 1442467412033474600,
 		"id_str": "1442467412033474568",
@@ -154,7 +281,7 @@ Grailbird.data.tweets_2021_09 =
 				"name": "Scotty McDonald",
 				"screen_name": "Scotty_McDonald",
 				"location": "Brisbane, Qld",
-				"description": "Massive Brisbane fan boy. 👨‍🍼On paternity leave, but usually Social Media Producer & PM Chief of Staff @10newsfirstQLD.",
+				"description": "Massive Brisbane fan boy. 👨🍼On paternity leave, but usually Social Media Producer & PM Chief of Staff @10newsfirstQLD.",
 				"url": "https://t.co/MhdOjGzwTq",
 				"entities": {
 					"url": {
