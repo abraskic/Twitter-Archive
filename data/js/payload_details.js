bc9baefef956f4c9b7d7da68ddbf208c7f6a65f6
abraskic <andrew.braskic@gmail.com>
2021-09-30 15:26:30 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77609,
-	"created_at": "Thu Sep 30 04:26:29 +0000 2021",
+	"tweets": 77610,
+	"created_at": "Thu Sep 30 07:26:30 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 30 07:19:53 +0000 2021",
+		"id": 1443435527999459300,
+		"id_str": "1443475674484785162",
+		"full_text": "https://t.co/mVL2Bmv8LS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1443435526019768300,
+					"id_str": "1443435526019768322",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FAgc9piWYAIBVeF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAgc9piWYAIBVeF.jpg",
+					"url": "https://t.co/mVL2Bmv8LS",
+					"display_url": "pic.twitter.com/mVL2Bmv8LS",
+					"expanded_url": "https://twitter.com/SallyMayweather/status/1443435527999459332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 818,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 672,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1443435526019768300,
+					"id_str": "1443435526019768322",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FAgc9piWYAIBVeF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAgc9piWYAIBVeF.jpg",
+					"url": "https://t.co/mVL2Bmv8LS",
+					"display_url": "pic.twitter.com/mVL2Bmv8LS",
+					"expanded_url": "https://twitter.com/SallyMayweather/status/1443435527999459332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 818,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 672,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 114060003,
+			"id_str": "114060003",
+			"name": "Sal the Agorist",
+			"screen_name": "SallyMayweather",
+			"location": "",
+			"description": "Agorist. Cypherpunk. Disintermediator. Crypto-Anarchist. Rothbardian. pod :: The Agora. blog :: New Libertarian. #logic #revisionism #countereconomics",
+			"url": "https://t.co/g2l94BRCAi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/g2l94BRCAi",
+							"expanded_url": "http://saltheagorist.com",
+							"display_url": "saltheagorist.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105666,
+			"friends_count": 16268,
+			"listed_count": 601,
+			"created_at": "Sun Feb 14 00:21:41 +0000 2010",
+			"favourites_count": 58717,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 70896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/672473329089974272/tgcqYXDU_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/672473329089974272/tgcqYXDU_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/114060003/1557633900",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 161,
+		"favorite_count": 975,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/mVL2Bmv8LS"
+	},
+	{
 		"created_at": "Thu Sep 30 04:22:20 +0000 2021",
 		"id": 1443430990353887200,
 		"id_str": "1443430990353887233",
