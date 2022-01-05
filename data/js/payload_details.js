30fa104eb57ada32fe3fdb624f82c01f66b11189
abraskic <andrew.braskic@gmail.com>
2022-01-05 13:14:45 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77687,
-	"created_at": "Wed Jan 05 00:14:45 +0000 2022",
+	"tweets": 77688,
+	"created_at": "Wed Jan 05 05:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 05 05:12:40 +0000 2022",
+		"id": 1478595279477948400,
+		"id_str": "1478595279477948421",
+		"full_text": "This vaccine certificate is so fucking stupid. So I’m meant to carry this thing everywhere I go from now on? https://t.co/DJcM6Rfe9l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1478595265422454800,
+					"id_str": "1478595265422454786",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FIUGj1oUcAIXMk9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIUGj1oUcAIXMk9.jpg",
+					"url": "https://t.co/DJcM6Rfe9l",
+					"display_url": "pic.twitter.com/DJcM6Rfe9l",
+					"expanded_url": "https://twitter.com/braskic/status/1478595279477948421/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1478595265422454800,
+					"id_str": "1478595265422454786",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FIUGj1oUcAIXMk9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIUGj1oUcAIXMk9.jpg",
+					"url": "https://t.co/DJcM6Rfe9l",
+					"display_url": "pic.twitter.com/DJcM6Rfe9l",
+					"expanded_url": "https://twitter.com/braskic/status/1478595279477948421/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "French backpacker",
+			"screen_name": "braskic",
+			"location": "Joondalup, Perth (WA)",
+			"description": "I am not who you think I am; I am not who I think I am; I am who I think you think I am.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 236,
+			"friends_count": 71,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76927,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000135220043/6076b99273969a8223906fa65a5a01dd_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0fc2962f5a547000",
+			"url": "https://api.twitter.com/1.1/geo/id/0fc2962f5a547000.json",
+			"place_type": "poi",
+			"name": "Balmoral Hotel",
+			"full_name": "Balmoral Hotel",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.90465783314058,
+							-31.987122643488114
+						],
+						[
+							115.90465783314058,
+							-31.987122643488114
+						],
+						[
+							115.90465783314058,
+							-31.987122643488114
+						],
+						[
+							115.90465783314058,
+							-31.987122643488114
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "This vaccine certificate is so fucking stupid. So I’m meant to carry this thing everywhere I go from now on? https://t.co/DJcM6Rfe9l"
+	},
+	{
 		"created_at": "Tue Jan 04 23:58:09 +0000 2022",
 		"id": 1478516128812912600,
 		"id_str": "1478516128812912641",
