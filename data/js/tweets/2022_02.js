9559652f3b93c35bad2ed218fbbba8957a4767dc
abraskic <andrew.braskic@gmail.com>
2022-02-28 17:14:46 +0800

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 28 08:29:41 +0000 2022",
+		"id": 1498141912082964500,
+		"id_str": "1498213806002290688",
+		"full_text": "What’s the price of petrol in #Lismore? @9NewsSyd @nbnnews https://t.co/Dz1KvvjwA9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Lismore",
+					"indices": [
+						30,
+						38
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "9NewsSyd",
+					"name": "9News Sydney",
+					"id": 171802941,
+					"id_str": "171802941",
+					"indices": [
+						40,
+						49
+					]
+				},
+				{
+					"screen_name": "nbnnews",
+					"name": "NBN News",
+					"id": 17332980,
+					"id_str": "17332980",
+					"indices": [
+						50,
+						58
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1498141906190041000,
+					"id_str": "1498141906190041088",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FMp4IE2VgAA2-N7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMp4IE2VgAA2-N7.jpg",
+					"url": "https://t.co/Dz1KvvjwA9",
+					"display_url": "pic.twitter.com/Dz1KvvjwA9",
+					"expanded_url": "https://twitter.com/livgracecurran/status/1498141912082964484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 576,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 576,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498141906190041000,
+					"id_str": "1498141906190041088",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FMp4IE2VgAA2-N7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMp4IE2VgAA2-N7.jpg",
+					"url": "https://t.co/Dz1KvvjwA9",
+					"display_url": "pic.twitter.com/Dz1KvvjwA9",
+					"expanded_url": "https://twitter.com/livgracecurran/status/1498141912082964484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 576,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 576,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1498141906206802000,
+					"id_str": "1498141906206801922",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FMp4IE6VQAI9Zkc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMp4IE6VQAI9Zkc.jpg",
+					"url": "https://t.co/Dz1KvvjwA9",
+					"display_url": "pic.twitter.com/Dz1KvvjwA9",
+					"expanded_url": "https://twitter.com/livgracecurran/status/1498141912082964484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 936,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3229440529,
+			"id_str": "3229440529",
+			"name": "Olivia Grace-Curran",
+			"screen_name": "livgracecurran",
+			"location": "",
+			"description": "Chief of Staff & News Editor @9newsaus @nbnnews Central Coast, Newy & Hunter Valley, Mid Coast, Mid North Coast, North West, North Coast, Gold Coast",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4682,
+			"friends_count": 6011,
+			"listed_count": 39,
+			"created_at": "Fri May 29 04:16:24 +0000 2015",
+			"favourites_count": 3209,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2454,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1494197113550299137/n4wfu0t0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1494197113550299137/n4wfu0t0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3229440529/1645079895",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 48,
+		"favorite_count": 110,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What’s the price of petrol in #Lismore? @9NewsSyd @nbnnews https://t.co/Dz1KvvjwA9"
+	},
+	{
 		"created_at": "Mon Feb 28 08:24:40 +0000 2022",
 		"id": 1498212542854103000,
 		"id_str": "1498212542854103040",
