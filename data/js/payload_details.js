d52720b52de53666e83b5076edab336262ce3420
abraskic <andrew.braskic@gmail.com>
2022-03-01 16:26:31 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77782,
-	"created_at": "Mon Feb 28 22:14:45 +0000 2022",
+	"tweets": 77783,
+	"created_at": "Tue Mar 01 08:26:29 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,165 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Tue Mar 01 08:19:09 +0000 2022",
+		"id": 1498306277302096000,
+		"id_str": "1498573541163560961",
+		"full_text": "North of Brisbane! 🤦🏻‍♀️ https://t.co/WAyRNGkraf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1498306272797491200,
+					"id_str": "1498306272797491200",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/FMsNneLVgAA8aI_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMsNneLVgAA8aI_.jpg",
+					"url": "https://t.co/WAyRNGkraf",
+					"display_url": "pic.twitter.com/WAyRNGkraf",
+					"expanded_url": "https://twitter.com/CaraMia200/status/1498306277302095873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498306272797491200,
+					"id_str": "1498306272797491200",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/FMsNneLVgAA8aI_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMsNneLVgAA8aI_.jpg",
+					"url": "https://t.co/WAyRNGkraf",
+					"display_url": "pic.twitter.com/WAyRNGkraf",
+					"expanded_url": "https://twitter.com/CaraMia200/status/1498306277302095873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1573800344,
+			"id_str": "1573800344",
+			"name": "Cara Mia",
+			"screen_name": "CaraMia200",
+			"location": "",
+			"description": "“sick of all the stiffs , sick of all the dips. i’m bored.” James Newell Osterberg, Jr.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6673,
+			"friends_count": 4214,
+			"listed_count": 27,
+			"created_at": "Sat Jul 06 22:38:51 +0000 2013",
+			"favourites_count": 190216,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 224723,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113370497218097153/zH4f5Vtn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113370497218097153/zH4f5Vtn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1573800344/1567814748",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 351,
+		"favorite_count": 1324,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "North of Brisbane! 🤦🏻‍♀️ https://t.co/WAyRNGkraf"
+	}
+]
