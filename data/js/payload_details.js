b355e785f3aa49cb9b4188b87306bf0a282fe5b0
abraskic <andrew.braskic@gmail.com>
2022-03-15 07:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77808,
-	"created_at": "Sun Mar 13 16:14:45 +0000 2022",
+	"tweets": 77809,
+	"created_at": "Mon Mar 14 23:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 14 23:05:47 +0000 2022",
+		"id": 1503131005389099000,
+		"id_str": "1503507713070080000",
+		"full_text": "Instead of telling you when it's safe to cross the street, the walk signs in Crystal City, VA are just repeating \"CHANGE PASSWORD\".\n\nSomething's gone terribly wrong here. https://t.co/W5h8OjBXUu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1503130963219566600,
+					"id_str": "1503130963219566594",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1503130963219566594/pu/img/i4xN-BhoZEFdQP7s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1503130963219566594/pu/img/i4xN-BhoZEFdQP7s.jpg",
+					"url": "https://t.co/W5h8OjBXUu",
+					"display_url": "pic.twitter.com/W5h8OjBXUu",
+					"expanded_url": "https://twitter.com/JosephPolitano/status/1503131005389099011/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503130963219566600,
+					"id_str": "1503130963219566594",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1503130963219566594/pu/img/i4xN-BhoZEFdQP7s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1503130963219566594/pu/img/i4xN-BhoZEFdQP7s.jpg",
+					"url": "https://t.co/W5h8OjBXUu",
+					"display_url": "pic.twitter.com/W5h8OjBXUu",
+					"expanded_url": "https://twitter.com/JosephPolitano/status/1503131005389099011/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 5676,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1503130963219566594/pu/pl/B3xgu0kv_1S3RGop.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1503130963219566594/pu/vid/720x1280/qZLkfuFQOhMHUgQf.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1503130963219566594/pu/vid/320x568/Q2zWyOD5w1vAcIk9.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1503130963219566594/pu/vid/480x852/8d3LZ40pTU1068_z.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309908860,
+			"id_str": "3309908860",
+			"name": "Joey Politano",
+			"screen_name": "JosephPolitano",
+			"location": "Washington, DC",
+			"description": "he/him\n\nBlogger and aspiring economist @ https://t.co/4VBMBXh9nL\n\nThe views here are my own and are not representative of the U.S. Government or the BLS",
+			"url": "https://t.co/Ktd2dUX4dc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ktd2dUX4dc",
+							"expanded_url": "https://apricitas.substack.com/",
+							"display_url": "apricitas.substack.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/4VBMBXh9nL",
+							"expanded_url": "http://apricitas.substack.com",
+							"display_url": "apricitas.substack.com",
+							"indices": [
+								41,
+								64
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 11752,
+			"friends_count": 1207,
+			"listed_count": 161,
+			"created_at": "Fri Jun 05 22:00:15 +0000 2015",
+			"favourites_count": 28642,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5628,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402657976616427521/BgypCCjy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402657976616427521/BgypCCjy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309908860/1642438644",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 393,
+		"favorite_count": 1860,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Instead of telling you when it's safe to cross the street, the walk signs in Crystal City, VA are just repeating \"CHANGE PASSWORD\".\n\nSomething's gone terribly wrong here. https://t.co/W5h8OjBXUu"
+	},
+	{
 		"created_at": "Sun Mar 13 15:59:29 +0000 2022",
 		"id": 1503038042747338800,
 		"id_str": "1503038042747338752",
