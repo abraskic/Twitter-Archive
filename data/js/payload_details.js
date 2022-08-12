f5006513bad36df5cb5d93bb43989cec295b127e
abraskic <andrew.braskic@gmail.com>
2022-08-12 16:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77965,
-	"created_at": "Tue Aug 09 17:14:45 +0000 2022",
+	"tweets": 77966,
+	"created_at": "Fri Aug 12 08:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 12 08:04:25 +0000 2022",
+		"id": 1558001443495641000,
+		"id_str": "1558001443495641090",
+		"full_text": "https://t.co/uolXBNAgVv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1558001437334212600,
+					"id_str": "1558001437334212608",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ8iDXfakAAayiD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ8iDXfakAAayiD.jpg",
+					"url": "https://t.co/uolXBNAgVv",
+					"display_url": "pic.twitter.com/uolXBNAgVv",
+					"expanded_url": "https://twitter.com/braskic/status/1558001443495641090/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1558001437334212600,
+					"id_str": "1558001437334212608",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ8iDXfakAAayiD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ8iDXfakAAayiD.jpg",
+					"url": "https://t.co/uolXBNAgVv",
+					"display_url": "pic.twitter.com/uolXBNAgVv",
+					"expanded_url": "https://twitter.com/braskic/status/1558001443495641090/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 629,
+							"h": 505,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perthyongyang, WA",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 230,
+			"friends_count": 69,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3382,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77195,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "zxx",
+		"text": "https://t.co/uolXBNAgVv"
+	},
+	{
 		"created_at": "Tue Aug 09 16:34:25 +0000 2022",
 		"id": 1557042626620432400,
 		"id_str": "1557042626620432384",
