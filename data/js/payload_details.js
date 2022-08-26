7e85b0b52f7efa93ff67a26f8c8974a070ffb73a
abraskic <andrew.braskic@gmail.com>
2022-08-26 11:26:30 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77973,
-	"created_at": "Thu Aug 25 08:14:45 +0000 2022",
+	"tweets": 77974,
+	"created_at": "Fri Aug 26 03:26:30 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 26 03:22:28 +0000 2022",
+		"id": 1562969559589482500,
+		"id_str": "1563003921576370176",
+		"full_text": "oof, bad luck compagno https://t.co/4qIwCCqg4F https://t.co/MpLoRY7OvZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4qIwCCqg4F",
+					"expanded_url": "https://www.gizmodo.com.au/2022/08/italian-man-with-fever-learns-he-has-monkeypox-covid-19-and-hiv-all-at-once/",
+					"display_url": "gizmodo.com.au/2022/08/italia…",
+					"indices": [
+						23,
+						46
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1562969362599841800,
+					"id_str": "1562969362599841793",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/FbDIWrSVEAETZA2.png",
+					"media_url_https": "https://pbs.twimg.com/media/FbDIWrSVEAETZA2.png",
+					"url": "https://t.co/MpLoRY7OvZ",
+					"display_url": "pic.twitter.com/MpLoRY7OvZ",
+					"expanded_url": "https://twitter.com/decryption/status/1562969559589482499/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 709,
+							"h": 269,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 709,
+							"h": 269,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1562969362599841800,
+					"id_str": "1562969362599841793",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/FbDIWrSVEAETZA2.png",
+					"media_url_https": "https://pbs.twimg.com/media/FbDIWrSVEAETZA2.png",
+					"url": "https://t.co/MpLoRY7OvZ",
+					"display_url": "pic.twitter.com/MpLoRY7OvZ",
+					"expanded_url": "https://twitter.com/decryption/status/1562969559589482499/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 709,
+							"h": 269,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 709,
+							"h": 269,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 718713,
+			"id_str": "718713",
+			"name": "decryption",
+			"screen_name": "decryption",
+			"location": "Victoria, Australia",
+			"description": "Author of The Sizzle, a daily tech email newsletter. North Melbourne Kangaroos supporter. Recreational systems administrator.",
+			"url": "https://t.co/Z2iUMEdeZV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z2iUMEdeZV",
+							"expanded_url": "http://thesizzle.com.au",
+							"display_url": "thesizzle.com.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4302,
+			"friends_count": 359,
+			"listed_count": 259,
+			"created_at": "Sun Jan 28 08:28:45 +0000 2007",
+			"favourites_count": 24119,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1031,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1429294966283505666/s_O3OxhP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1429294966283505666/s_O3OxhP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/718713/1618787812",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "oof, bad luck compagno https://t.co/4qIwCCqg4F https://t.co/MpLoRY7OvZ"
+	},
+	{
 		"created_at": "Thu Aug 25 07:28:13 +0000 2022",
 		"id": 1562703376621912000,
 		"id_str": "1562703376621912070",
