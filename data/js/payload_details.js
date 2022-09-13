5289e1784f26d9979ea28610b847650f192cbac7
abraskic <andrew.braskic@gmail.com>
2022-09-13 18:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77997,
-	"created_at": "Tue Sep 13 02:14:45 +0000 2022",
+	"tweets": 77998,
+	"created_at": "Tue Sep 13 10:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 13 10:12:58 +0000 2022",
+		"id": 1569630205727363000,
+		"id_str": "1569630205727363072",
+		"full_text": "Close the borders. Short sharp lockdown just in case. https://t.co/NkCab6zzt2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NkCab6zzt2",
+					"expanded_url": "https://twitter.com/nswhealth/status/1569549011035770881",
+					"display_url": "twitter.com/nswhealth/stat…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Perthyongyang, WA",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 230,
+			"friends_count": 68,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77231,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1569549011035771000,
+		"quoted_status_id_str": "1569549011035770881",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NkCab6zzt2",
+			"expanded": "https://twitter.com/nswhealth/status/1569549011035770881",
+			"display": "twitter.com/nswhealth/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 13 04:50:19 +0000 2022",
+			"id": 1569549011035771000,
+			"id_str": "1569549011035770881",
+			"full_text": "NSW Health has confirmed the first case of measles in NSW since February 2020.\n\nThe case, a person in their 50s, acquired their infection while travelling in Asia last month. The case developed symptoms after returning to Sydney is now isolated in hospital. https://t.co/vQ8m2Cyt4j",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				257
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1569548248460951600,
+						"id_str": "1569548248460951553",
+						"indices": [
+							258,
+							281
+						],
+						"media_url": "http://pbs.twimg.com/media/Fcgn0O9aAAEI_cA.png",
+						"media_url_https": "https://pbs.twimg.com/media/Fcgn0O9aAAEI_cA.png",
+						"url": "https://t.co/vQ8m2Cyt4j",
+						"display_url": "pic.twitter.com/vQ8m2Cyt4j",
+						"expanded_url": "https://twitter.com/NSWHealth/status/1569549011035770881/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1569548248460951600,
+						"id_str": "1569548248460951553",
+						"indices": [
+							258,
+							281
+						],
+						"media_url": "http://pbs.twimg.com/media/Fcgn0O9aAAEI_cA.png",
+						"media_url_https": "https://pbs.twimg.com/media/Fcgn0O9aAAEI_cA.png",
+						"url": "https://t.co/vQ8m2Cyt4j",
+						"display_url": "pic.twitter.com/vQ8m2Cyt4j",
+						"expanded_url": "https://twitter.com/NSWHealth/status/1569549011035770881/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 40778270,
+				"id_str": "40778270",
+				"name": "NSW Health",
+				"screen_name": "NSWHealth",
+				"location": "NSW Australia",
+				"description": "Healthy People - now and in the future. \n\nRead our community guidelines before posting: https://t.co/RaFyZdVFv9…",
+				"url": "https://t.co/bl66OW8q2d",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bl66OW8q2d",
+								"expanded_url": "https://www.health.nsw.gov.au/",
+								"display_url": "health.nsw.gov.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/RaFyZdVFv9",
+								"expanded_url": "http://health.nsw.gov.au/social/Pages/c",
+								"display_url": "health.nsw.gov.au/social/Pages/c",
+								"indices": [
+									88,
+									111
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 338559,
+				"friends_count": 332,
+				"listed_count": 1248,
+				"created_at": "Mon May 18 01:09:11 +0000 2009",
+				"favourites_count": 1544,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 19267,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1519160877462683648/TXoTWo9U_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1519160877462683648/TXoTWo9U_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/40778270/1652675155",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 80,
+			"favorite_count": 101,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "Close the borders. Short sharp lockdown just in case. https://t.co/NkCab6zzt2"
+	},
+	{
 		"created_at": "Tue Sep 13 02:11:09 +0000 2022",
 		"id": 1569508955017322500,
 		"id_str": "1569508955017322497",
