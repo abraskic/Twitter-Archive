f8b87bca9066ece679a1359d6a0360b355f4afab
abraskic <andrew.braskic@gmail.com>
2022-09-28 16:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78009,
-	"created_at": "Tue Sep 27 11:14:45 +0000 2022",
+	"tweets": 78010,
+	"created_at": "Wed Sep 28 08:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 28 07:53:29 +0000 2022",
+		"id": 1575030925385035800,
+		"id_str": "1575030925385035776",
+		"full_text": "Hey @Optus, I’d very much like a free passport on account of me being a customer of yours 12 years ago. Please and thank you.\n https://t.co/pfg2hDgQIN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Optus",
+					"name": "Optus",
+					"id": 48201591,
+					"id_str": "48201591",
+					"indices": [
+						4,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/pfg2hDgQIN",
+					"expanded_url": "https://www.smh.com.au/technology/federal-government-examining-new-medicare-numbers-for-people-affected-by-optus-breach-20220928-p5bliq.html",
+					"display_url": "smh.com.au/technology/fed…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Kalgoorlie - Boulder, Western Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 229,
+			"friends_count": 68,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3402,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77244,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3e3a41c10697bf90",
+			"url": "https://api.twitter.com/1.1/geo/id/3e3a41c10697bf90.json",
+			"place_type": "neighborhood",
+			"name": "Mount Lawley",
+			"full_name": "Mount Lawley, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.86248388,
+							-31.94456004
+						],
+						[
+							115.88880096,
+							-31.94456004
+						],
+						[
+							115.88880096,
+							-31.91702796
+						],
+						[
+							115.86248388,
+							-31.91702796
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "Hey @Optus, I’d very much like a free passport on account of me being a customer of yours 12 years ago. Please and thank you.\n https://t.co/pfg2hDgQIN"
+	},
+	{
 		"created_at": "Tue Sep 27 11:10:07 +0000 2022",
 		"id": 1574718021108924400,
 		"id_str": "1574718021108924420",
