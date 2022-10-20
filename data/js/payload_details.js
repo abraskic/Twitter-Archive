341abcede6c7b0cf29ff0d24cef770f43212dd06
abraskic <andrew.braskic@gmail.com>
2022-10-21 06:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78035,
-	"created_at": "Thu Oct 20 16:14:46 +0000 2022",
+	"tweets": 78036,
+	"created_at": "Thu Oct 20 22:14:46 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Thu Oct 20 21:44:17 +0000 2022",
+		"id": 1583179210792706000,
+		"id_str": "1583212533418430464",
+		"full_text": "R.I.P. Queen Elizabeth. A girl boss slain by the patriarchy. We won’t forget. https://t.co/GmPdWSHtoM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1583178941644255200,
+					"id_str": "1583178941644255233",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/FfiU3ACVsAE2M6a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfiU3ACVsAE2M6a.jpg",
+					"url": "https://t.co/GmPdWSHtoM",
+					"display_url": "pic.twitter.com/GmPdWSHtoM",
+					"expanded_url": "https://twitter.com/AdenGlaven1994/status/1583179210792706049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1583178941644255200,
+					"id_str": "1583178941644255233",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/FfiU3ACVsAE2M6a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfiU3ACVsAE2M6a.jpg",
+					"url": "https://t.co/GmPdWSHtoM",
+					"display_url": "pic.twitter.com/GmPdWSHtoM",
+					"expanded_url": "https://twitter.com/AdenGlaven1994/status/1583179210792706049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 969828390306902000,
+			"id_str": "969828390306902022",
+			"name": "Aden/Crabzilla",
+			"screen_name": "AdenGlaven1994",
+			"location": "Land of the Yugambeh",
+			"description": "Southern Gold Coast 💚 Local Campaign Coordinator. Data Nerd. Here to Keep the Wheels Turning. Master’s Pub Health. North Melb & Brighton fan. Local Politics.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2579,
+			"friends_count": 1549,
+			"listed_count": 21,
+			"created_at": "Sat Mar 03 06:54:41 +0000 2018",
+			"favourites_count": 376181,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 121332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1583032401147482113/u8A666xV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1583032401147482113/u8A666xV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/969828390306902022/1655868928",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "R.I.P. Queen Elizabeth. A girl boss slain by the patriarchy. We won’t forget. https://t.co/GmPdWSHtoM"
+	},
+	{
 		"created_at": "Thu Oct 20 15:41:53 +0000 2022",
 		"id": 1583121332702949400,
 		"id_str": "1583121332702949376",
