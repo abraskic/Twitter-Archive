0f4941be62a586a93f93830330751ba8327b54c2
abraskic <andrew.braskic@gmail.com>
2022-10-25 12:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78042,
-	"created_at": "Mon Oct 24 10:14:45 +0000 2022",
+	"tweets": 78043,
+	"created_at": "Tue Oct 25 04:14:45 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Tue Oct 25 03:55:18 +0000 2022",
+		"id": 1584735711923953700,
+		"id_str": "1584755456857493504",
+		"full_text": "The year is 2052. The polar caps have melted. Half the world is flooded. The rest is desert.\nThe AI on my Apple flying car connects to my music library.\nU2’s Songs of Innocence begins to play. I curse Bono and try once again to delete it",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16218084,
+			"id_str": "16218084",
+			"name": "Tim Burrowes - Unmade",
+			"screen_name": "timburrowes",
+			"location": "Sisters Beach TAS",
+			"description": "Proprietor of email newsletter Unmade. Author of book Media Unmade. Founder of Mumbrella, back in the day. English-Australian. Or maybe Australian-English",
+			"url": "https://t.co/lm6TG77MDP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lm6TG77MDP",
+							"expanded_url": "http://unmade.media",
+							"display_url": "unmade.media",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 158167,
+			"friends_count": 35383,
+			"listed_count": 1707,
+			"created_at": "Wed Sep 10 07:45:52 +0000 2008",
+			"favourites_count": 5828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 18466,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1429974474959462409/exTbrj15_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1429974474959462409/exTbrj15_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16218084/1629767557",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "DCE5D7",
+			"profile_sidebar_fill_color": "F4EEEB",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0073b76548e5984f",
+			"url": "https://api.twitter.com/1.1/geo/id/0073b76548e5984f.json",
+			"place_type": "city",
+			"name": "Sydney",
+			"full_name": "Sydney, New South Wales",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							150.520928608,
+							-34.1183470085
+						],
+						[
+							151.343020992,
+							-34.1183470085
+						],
+						[
+							151.343020992,
+							-33.578140996
+						],
+						[
+							150.520928608,
+							-33.578140996
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The year is 2052. The polar caps have melted. Half the world is flooded. The rest is desert.\nThe AI on my Apple flying car connects to my music library.\nU2’s Songs of Innocence begins to play. I curse Bono and try once again to delete it"
+	},
+	{
 		"created_at": "Mon Oct 24 10:06:02 +0000 2022",
 		"id": 1584486367316570000,
 		"id_str": "1584486367316570113",
