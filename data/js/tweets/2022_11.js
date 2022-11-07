ac76cf9c2c51ad113a7f229b1831057cbd112bc9
abraskic <andrew.braskic@gmail.com>
2022-11-07 19:14:46 +0800

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 07 11:03:08 +0000 2022",
+		"id": 1589574164117328000,
+		"id_str": "1589574164117327872",
+		"full_text": "@lilssssa https://t.co/Xdb35dZYGq",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lilssssa",
+					"name": "churchie lè femme",
+					"id": 326759429,
+					"id_str": "326759429",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1589574158585041000,
+					"id_str": "1589574158585040896",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg9NRnqaEAACK4d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg9NRnqaEAACK4d.jpg",
+					"url": "https://t.co/Xdb35dZYGq",
+					"display_url": "pic.twitter.com/Xdb35dZYGq",
+					"expanded_url": "https://twitter.com/braskic/status/1589574164117327872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1589574158585041000,
+					"id_str": "1589574158585040896",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg9NRnqaEAACK4d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg9NRnqaEAACK4d.jpg",
+					"url": "https://t.co/Xdb35dZYGq",
+					"display_url": "pic.twitter.com/Xdb35dZYGq",
+					"expanded_url": "https://twitter.com/braskic/status/1589574164117327872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 360,
+							"h": 263,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1589566287336067000,
+		"in_reply_to_status_id_str": "1589566287336067073",
+		"in_reply_to_user_id": 326759429,
+		"in_reply_to_user_id_str": "326759429",
+		"in_reply_to_screen_name": "lilssssa",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 231,
+			"friends_count": 69,
+			"listed_count": 24,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3409,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77293,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3e3a41c10697bf90",
+			"url": "https://api.twitter.com/1.1/geo/id/3e3a41c10697bf90.json",
+			"place_type": "neighborhood",
+			"name": "Mount Lawley",
+			"full_name": "Mount Lawley, Perth (WA)",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.86248388,
+							-31.94456004
+						],
+						[
+							115.88880096,
+							-31.94456004
+						],
+						[
+							115.88880096,
+							-31.91702796
+						],
+						[
+							115.86248388,
+							-31.91702796
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "qme",
+		"text": "@lilssssa https://t.co/Xdb35dZYGq"
+	},
+	{
 		"created_at": "Mon Nov 07 09:24:50 +0000 2022",
 		"id": 1589549428276617200,
 		"id_str": "1589549428276617222",
