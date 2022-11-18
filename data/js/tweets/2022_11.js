2c3b5fbe764f7b1e934cce1f95844772b50b1f75
abraskic <andrew.braskic@gmail.com>
2022-11-18 11:14:47 +0800

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 18 02:39:44 +0000 2022",
+		"id": 1593364591509405700,
+		"id_str": "1593433745432145921",
+		"full_text": "Mastodon is a train wreck. Someone needs to build a twitter clone… nothing fancy, just like twitter and allow you to grab your twitter handle. I know python, I'll see what I can whip up this weekend. According to Musk it's not that hard and doesn't take a lot of people.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27531390,
+			"id_str": "27531390",
+			"name": "Ron Gilbert",
+			"screen_name": "grumpygamer",
+			"location": "Monkey Island",
+			"description": "I am the designer/creator of Monkey Island, The Cave, Pajama Sam and the designer/co-creator of Maniac Mansion, DeathSpank and Thimbleweed Park.",
+			"url": "https://t.co/u8810P3WtH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/u8810P3WtH",
+							"expanded_url": "https://grumpygamer.com",
+							"display_url": "grumpygamer.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 135948,
+			"friends_count": 1,
+			"listed_count": 1880,
+			"created_at": "Sun Mar 29 23:30:48 +0000 2009",
+			"favourites_count": 4615,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15676,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1548038791046107138/hoyeU59W_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1548038791046107138/hoyeU59W_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27531390/1361642804",
+			"profile_link_color": "7D0002",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FF9B54",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 104,
+		"favorite_count": 1311,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Mastodon is a train wreck. Someone needs to build a twitter clone… nothing fancy, just like twitter and allow you to grab your twitter handle. I know python, I'll see what I can whip up this weekend. According to Musk it's not that hard and doesn't take a lot of people."
+	},
+	{
 		"created_at": "Tue Nov 15 10:10:06 +0000 2022",
 		"id": 1592459921945415700,
 		"id_str": "1592459921945415682",
