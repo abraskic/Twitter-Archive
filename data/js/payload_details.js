d470d01a67b8dcdd2793e36f6e3c53c5d6f4e557
abraskic <andrew.braskic@gmail.com>
2022-12-02 14:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78094,
-	"created_at": "Tue Nov 29 20:14:45 +0000 2022",
+	"tweets": 78095,
+	"created_at": "Fri Dec 02 06:14:46 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,165 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Fri Dec 02 05:37:16 +0000 2022",
+		"id": 1598551855621345300,
+		"id_str": "1598551855621345282",
+		"full_text": "Train for FREE in December. No Strings^ No catches* No commitment*^\n\nTerms and conditions apply. https://t.co/b2kBROnPP7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1598551835216076800,
+					"id_str": "1598551835216076800",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi8ybLUaYAAi2cY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi8ybLUaYAAi2cY.jpg",
+					"url": "https://t.co/b2kBROnPP7",
+					"display_url": "pic.twitter.com/b2kBROnPP7",
+					"expanded_url": "https://twitter.com/braskic/status/1598551855621345282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 689,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 390,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1169,
+							"h": 2036,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598551835216076800,
+					"id_str": "1598551835216076800",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi8ybLUaYAAi2cY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi8ybLUaYAAi2cY.jpg",
+					"url": "https://t.co/b2kBROnPP7",
+					"display_url": "pic.twitter.com/b2kBROnPP7",
+					"expanded_url": "https://twitter.com/braskic/status/1598551855621345282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 689,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 390,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1169,
+							"h": 2036,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 228,
+			"friends_count": 70,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3416,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "Train for FREE in December. No Strings^ No catches* No commitment*^\n\nTerms and conditions apply. https://t.co/b2kBROnPP7"
+	}
+]
