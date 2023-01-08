54919aaa05985d9c5234ff5bfdd7ce0f2656dfeb
abraskic <andrew.braskic@gmail.com>
2023-01-08 19:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78149,
-	"created_at": "Sun Jan 08 04:14:45 +0000 2023",
+	"tweets": 78150,
+	"created_at": "Sun Jan 08 11:14:45 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Sun Jan 08 10:35:43 +0000 2023",
+		"id": 1612035311286251500,
+		"id_str": "1612035311286251520",
+		"full_text": "@AdenGlaven1994 How else would we get gold like this if the cricket didn’t get washed out? https://t.co/tUw6aQjvrs",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AdenGlaven1994",
+					"name": "Teal Katter Gaming Crab",
+					"id": 969828390306902000,
+					"id_str": "969828390306902022",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tUw6aQjvrs",
+					"expanded_url": "https://vt.tiktok.com/ZS8romhTQ/",
+					"display_url": "vt.tiktok.com/ZS8romhTQ/",
+					"indices": [
+						91,
+						114
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1611956682472689700,
+		"in_reply_to_status_id_str": "1611956682472689664",
+		"in_reply_to_user_id": 969828390306902000,
+		"in_reply_to_user_id_str": "969828390306902022",
+		"in_reply_to_screen_name": "AdenGlaven1994",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 228,
+			"friends_count": 69,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3416,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "@AdenGlaven1994 How else would we get gold like this if the cricket didn’t get washed out? https://t.co/tUw6aQjvrs"
+	},
+	{
 		"created_at": "Sun Jan 08 03:29:00 +0000 2023",
 		"id": 1611927928509726700,
 		"id_str": "1611927928509726726",
