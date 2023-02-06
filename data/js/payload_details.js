307e95bdfbd5443b2152bdbfa6590a9d9c2edc14
abraskic <andrew.braskic@gmail.com>
2023-02-06 08:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78169,
-	"created_at": "Fri Feb 03 08:26:29 +0000 2023",
+	"tweets": 78170,
+	"created_at": "Mon Feb 06 00:14:45 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,314 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Sun Feb 05 23:49:27 +0000 2023",
+		"id": 1622381922935783400,
+		"id_str": "1622381922935783425",
+		"full_text": "❤️🚕🥵😬😅 https://t.co/D1jQR9bWhN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			6
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/D1jQR9bWhN",
+					"expanded_url": "https://twitter.com/settcandy/status/1619370280870359041",
+					"display_url": "twitter.com/settcandy/stat…",
+					"indices": [
+						7,
+						30
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 227,
+			"friends_count": 69,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3350,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77380,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1619370280870359000,
+		"quoted_status_id_str": "1619370280870359041",
+		"quoted_status_permalink": {
+			"url": "https://t.co/D1jQR9bWhN",
+			"expanded": "https://twitter.com/settcandy/status/1619370280870359041",
+			"display": "twitter.com/settcandy/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jan 28 16:22:16 +0000 2023",
+			"id": 1619370280870359000,
+			"id_str": "1619370280870359041",
+			"full_text": "3rd row will be your February month https://t.co/PnS2H52RHL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				35
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1619370277393125400,
+						"id_str": "1619370277393125378",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/media/Fnkor__X0AIo5oq.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fnkor__X0AIo5oq.jpg",
+						"url": "https://t.co/PnS2H52RHL",
+						"display_url": "pic.twitter.com/PnS2H52RHL",
+						"expanded_url": "https://twitter.com/SettCandy/status/1619370280870359041/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1619370277393125400,
+						"id_str": "1619370277393125378",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/media/Fnkor__X0AIo5oq.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fnkor__X0AIo5oq.jpg",
+						"url": "https://t.co/PnS2H52RHL",
+						"display_url": "pic.twitter.com/PnS2H52RHL",
+						"expanded_url": "https://twitter.com/SettCandy/status/1619370280870359041/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 622,
+								"h": 615,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1225059312701034500,
+				"id_str": "1225059312701034496",
+				"name": "𝒮𝑒𝓉𝓉 🍎🔥",
+				"screen_name": "SettCandy",
+				"location": " promo dm 📩",
+				"description": "J'adore Vivienne, habillez-moi Gucci, Fendi, et Prada. Valentino, Armani too. Merde I love them Jimmy Choo 💋🛍️ fan not affiliated with anyone",
+				"url": "https://t.co/WYQQT3j6qW",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WYQQT3j6qW",
+								"expanded_url": "http://www.tiktok.com/@settcandy",
+								"display_url": "tiktok.com/@settcandy",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 112787,
+				"friends_count": 5876,
+				"listed_count": 172,
+				"created_at": "Wed Feb 05 14:11:23 +0000 2020",
+				"favourites_count": 29722,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 11644,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1618269764698112000/oe3hCFJB_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1618269764698112000/oe3hCFJB_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1225059312701034496/1674830142",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 153,
+			"favorite_count": 1416,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": true,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "art",
+		"text": "❤️🚕🥵😬😅 https://t.co/D1jQR9bWhN"
+	},
+	{
 		"created_at": "Fri Feb 03 08:19:05 +0000 2023",
 		"id": 1621423013064474600,
 		"id_str": "1621423013064474624",
