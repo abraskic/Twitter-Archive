cc36b9fc036c74d0f35a5764c8cb7f6333d6beb3
abraskic <andrew.braskic@gmail.com>
2023-02-20 11:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78182,
-	"created_at": "Sun Feb 19 01:14:45 +0000 2023",
+	"tweets": 78183,
+	"created_at": "Mon Feb 20 03:14:46 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Mon Feb 20 02:46:25 +0000 2023",
+		"id": 1627409766367776800,
+		"id_str": "1627499889549807616",
+		"full_text": "Yes, your email finds me well https://t.co/O0c4DAjYMN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1627409761561088000,
+					"id_str": "1627409761561088000",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/FpW4jkXaEAAnqVZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FpW4jkXaEAAnqVZ.jpg",
+					"url": "https://t.co/O0c4DAjYMN",
+					"display_url": "pic.twitter.com/O0c4DAjYMN",
+					"expanded_url": "https://twitter.com/_wkelly_/status/1627409766367776769/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1627409761561088000,
+					"id_str": "1627409761561088000",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/FpW4jkXaEAAnqVZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FpW4jkXaEAAnqVZ.jpg",
+					"url": "https://t.co/O0c4DAjYMN",
+					"display_url": "pic.twitter.com/O0c4DAjYMN",
+					"expanded_url": "https://twitter.com/_wkelly_/status/1627409766367776769/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 301,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 247582999,
+			"id_str": "247582999",
+			"name": "will kelly",
+			"screen_name": "_wkelly_",
+			"location": "he/him in Naarm",
+			"description": "media & communications for @adambandt. failed archaeologist. join your union.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1969,
+			"friends_count": 2049,
+			"listed_count": 21,
+			"created_at": "Sat Feb 05 03:19:29 +0000 2011",
+			"favourites_count": 19876,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 655,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1372752309474619392/dS8zJX-S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1372752309474619392/dS8zJX-S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/247582999/1634167709",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yes, your email finds me well https://t.co/O0c4DAjYMN"
+	},
+	{
 		"created_at": "Sun Feb 19 00:47:10 +0000 2023",
 		"id": 1627107488255590400,
 		"id_str": "1627107488255590400",
