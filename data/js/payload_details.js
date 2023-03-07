421d8313e569833beb3f0c84f5e213aa6902df83
abraskic <andrew.braskic@gmail.com>
2023-03-07 10:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78189,
-	"created_at": "Sat Mar 04 02:14:45 +0000 2023",
+	"tweets": 78190,
+	"created_at": "Tue Mar 07 02:14:45 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 07 01:55:27 +0000 2023",
+		"id": 1632922881113092000,
+		"id_str": "1632922881113092096",
+		"full_text": "@AdenGlaven1994 https://t.co/Osy2k41QLB",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AdenGlaven1994",
+					"name": "Pineapple Progressivism",
+					"id": 969828390306902000,
+					"id_str": "969828390306902022",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1632922825312051200,
+					"id_str": "1632922825312051200",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1632922825312051200/pu/img/q5WbT_jlcuMVOAnw.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1632922825312051200/pu/img/q5WbT_jlcuMVOAnw.jpg",
+					"url": "https://t.co/Osy2k41QLB",
+					"display_url": "pic.twitter.com/Osy2k41QLB",
+					"expanded_url": "https://twitter.com/braskic/status/1632922881113092096/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 576,
+							"h": 834,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 470,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 576,
+							"h": 834,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1632922825312051200,
+					"id_str": "1632922825312051200",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1632922825312051200/pu/img/q5WbT_jlcuMVOAnw.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1632922825312051200/pu/img/q5WbT_jlcuMVOAnw.jpg",
+					"url": "https://t.co/Osy2k41QLB",
+					"display_url": "pic.twitter.com/Osy2k41QLB",
+					"expanded_url": "https://twitter.com/braskic/status/1632922881113092096/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 576,
+							"h": 834,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 470,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 576,
+							"h": 834,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							96,
+							139
+						],
+						"duration_millis": 21316,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1632922825312051200/pu/vid/320x462/jFNmTaF34UJATnog.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1632922825312051200/pu/vid/576x834/VYa7eFkpYMzudueC.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1632922825312051200/pu/pl/2ZZM47PbSP4yZ2TK.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1632922825312051200/pu/vid/480x694/c6Iw-nJgit069pS_.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1632697421674459100,
+		"in_reply_to_status_id_str": "1632697421674459136",
+		"in_reply_to_user_id": 969828390306902000,
+		"in_reply_to_user_id_str": "969828390306902022",
+		"in_reply_to_screen_name": "AdenGlaven1994",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 228,
+			"friends_count": 71,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77398,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "qme",
+		"text": "@AdenGlaven1994 https://t.co/Osy2k41QLB"
+	},
+	{
 		"created_at": "Sat Mar 04 01:58:42 +0000 2023",
 		"id": 1631836535023890400,
 		"id_str": "1631836535023890437",
