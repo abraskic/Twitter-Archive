15e732fabf713dcd8ab804bb6105e2728510759c
abraskic <andrew.braskic@gmail.com>
2023-03-14 10:14:46 +0800

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 14 02:11:34 +0000 2023",
+		"id": 1635443228106453000,
+		"id_str": "1635463651175043073",
+		"full_text": "https://t.co/ROhIrUFy7K",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1635443221206794200,
+					"id_str": "1635443221206794242",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FrJC8dpaAAIJvF1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrJC8dpaAAIJvF1.jpg",
+					"url": "https://t.co/ROhIrUFy7K",
+					"display_url": "pic.twitter.com/ROhIrUFy7K",
+					"expanded_url": "https://twitter.com/kels_316/status/1635443228106452992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 782,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1862,
+							"h": 1214,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 443,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1635443221206794200,
+					"id_str": "1635443221206794242",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FrJC8dpaAAIJvF1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrJC8dpaAAIJvF1.jpg",
+					"url": "https://t.co/ROhIrUFy7K",
+					"display_url": "pic.twitter.com/ROhIrUFy7K",
+					"expanded_url": "https://twitter.com/kels_316/status/1635443228106452992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 782,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1862,
+							"h": 1214,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 443,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 97283842,
+			"id_str": "97283842",
+			"name": "Adam",
+			"screen_name": "kels_316",
+			"location": "Nunya ",
+			"description": "Rescue, drones, land rovers, pretengineering, nonsense. I'm also at: Kels_316@mastodon.social and Kels_316@aus.social",
+			"url": "https://t.co/ndDFwFf4qo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ndDFwFf4qo",
+							"expanded_url": "http://thescornfulgourmand.wordpress.com/",
+							"display_url": "thescornfulgourmand.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2639,
+			"friends_count": 1475,
+			"listed_count": 47,
+			"created_at": "Wed Dec 16 20:35:27 +0000 2009",
+			"favourites_count": 81809,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28578,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1308253124969164801/6fvDLb8g_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1308253124969164801/6fvDLb8g_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/97283842/1465030886",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/ROhIrUFy7K"
+	},
+	{
 		"created_at": "Mon Mar 13 17:09:24 +0000 2023",
 		"id": 1635245104347893800,
 		"id_str": "1635327212223340545",
