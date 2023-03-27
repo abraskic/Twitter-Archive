904bc04240f18689316315ad6513a8beb2ece633
abraskic <andrew.braskic@gmail.com>
2023-03-27 12:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78210,
-	"created_at": "Sat Mar 25 11:26:29 +0000 2023",
+	"tweets": 78211,
+	"created_at": "Mon Mar 27 04:14:46 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,312 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Mon Mar 27 04:07:17 +0000 2023",
+		"id": 1640203813817815000,
+		"id_str": "1640203813817815041",
+		"full_text": "Driver did well. Caravan just thought he was another methed out truckie. Also the lane closed for “roadworks”. https://t.co/wFuMVKDO4W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wFuMVKDO4W",
+					"expanded_url": "https://twitter.com/dashcamownersau/status/1640152859160502272",
+					"display_url": "twitter.com/dashcamownersa…",
+					"indices": [
+						111,
+						134
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 226,
+			"friends_count": 71,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3354,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77423,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1640152859160502300,
+		"quoted_status_id_str": "1640152859160502272",
+		"quoted_status_permalink": {
+			"url": "https://t.co/wFuMVKDO4W",
+			"expanded": "https://twitter.com/dashcamownersau/status/1640152859160502272",
+			"display": "twitter.com/dashcamownersa…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Mar 27 00:44:48 +0000 2023",
+			"id": 1640152859160502300,
+			"id_str": "1640152859160502272",
+			"full_text": "\"Brake fail going down Mt Victoria. Loaded semi 40ish tonne \" https://t.co/MyqHvG9zTV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				61
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1640151665121820700,
+						"id_str": "1640151665121820672",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1640151665121820672/pu/img/kXJyt79H_l1Da84c.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1640151665121820672/pu/img/kXJyt79H_l1Da84c.jpg",
+						"url": "https://t.co/MyqHvG9zTV",
+						"display_url": "pic.twitter.com/MyqHvG9zTV",
+						"expanded_url": "https://twitter.com/DashCamOwnersAu/status/1640152859160502272/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1640151665121820700,
+						"id_str": "1640151665121820672",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1640151665121820672/pu/img/kXJyt79H_l1Da84c.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1640151665121820672/pu/img/kXJyt79H_l1Da84c.jpg",
+						"url": "https://t.co/MyqHvG9zTV",
+						"display_url": "pic.twitter.com/MyqHvG9zTV",
+						"expanded_url": "https://twitter.com/DashCamOwnersAu/status/1640152859160502272/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 124842,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1640151665121820672/pu/pl/PlOg3QPTeiUvJ1wN.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1640151665121820672/pu/vid/640x360/eql_xcSwPN6qyQmY.mp4?tag=12"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1640151665121820672/pu/vid/480x270/z5tXgMnmyAN8oKoR.mp4?tag=12"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1640151665121820672/pu/vid/1280x720/46sXf7L3yI8w_8At.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3078452209,
+				"id_str": "3078452209",
+				"name": "Dash Cam Owners Aus",
+				"screen_name": "DashCamOwnersAu",
+				"location": "Australia",
+				"description": "Australian Dash Cam Sales & Media - YouTube, Facebook and Twitch.",
+				"url": "https://t.co/t8zfn1GS6p",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/t8zfn1GS6p",
+								"expanded_url": "http://www.dashcamownersaus.com.au/",
+								"display_url": "dashcamownersaus.com.au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7567,
+				"friends_count": 177,
+				"listed_count": 12,
+				"created_at": "Fri Mar 13 22:43:53 +0000 2015",
+				"favourites_count": 1611,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 2153,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/576514537337163777/i8lDi-lu_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/576514537337163777/i8lDi-lu_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3078452209/1592860218",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8,
+			"favorite_count": 123,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "Driver did well. Caravan just thought he was another methed out truckie. Also the lane closed for “roadworks”. https://t.co/wFuMVKDO4W"
+	},
+	{
 		"created_at": "Sat Mar 25 11:21:22 +0000 2023",
 		"id": 1639588279585771500,
 		"id_str": "1639588279585771524",
