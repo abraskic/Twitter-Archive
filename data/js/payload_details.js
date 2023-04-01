689cb725b07ca0bba2bc9787b315f425df39ea73
abraskic <andrew.braskic@gmail.com>
2023-04-01 14:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78213,
-	"created_at": "Fri Mar 31 09:14:46 +0000 2023",
+	"tweets": 78214,
+	"created_at": "Sat Apr 01 06:14:46 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,220 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Sat Apr 01 05:55:39 +0000 2023",
+		"id": 1641456589457506300,
+		"id_str": "1642043026485964804",
+		"full_text": "The wildest, most unhinged reality TV show yet, hosted by yours truly. 😘\n\nGet ready for Love Language, a Duolingo and @peacock original series coming soon…or is it? https://t.co/RCYj6PJGqZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "peacock",
+					"name": "Peacock",
+					"id": 1171155332917321700,
+					"id_str": "1171155332917321730",
+					"indices": [
+						118,
+						126
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1641456406007087000,
+					"id_str": "1641456406007087105",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1641456406007087105/pu/img/ODYZbo5Xf6hp9mQv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1641456406007087105/pu/img/ODYZbo5Xf6hp9mQv.jpg",
+					"url": "https://t.co/RCYj6PJGqZ",
+					"display_url": "pic.twitter.com/RCYj6PJGqZ",
+					"expanded_url": "https://twitter.com/duolingo/status/1641456589457506306/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641456406007087000,
+					"id_str": "1641456406007087105",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1641456406007087105/pu/img/ODYZbo5Xf6hp9mQv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1641456406007087105/pu/img/ODYZbo5Xf6hp9mQv.jpg",
+					"url": "https://t.co/RCYj6PJGqZ",
+					"display_url": "pic.twitter.com/RCYj6PJGqZ",
+					"expanded_url": "https://twitter.com/duolingo/status/1641456589457506306/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 85500,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1641456406007087105/pu/vid/1280x720/1LMowk4gaGcTdoYy.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1641456406007087105/pu/pl/Vuh1nNAjpE5Edu63.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1641456406007087105/pu/vid/480x270/Hymr2PSdUK8UnB0y.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1641456406007087105/pu/vid/640x360/IgWlEPFgFzxmMl9g.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 107238136,
+			"id_str": "107238136",
+			"name": "Duolingo",
+			"screen_name": "duolingo",
+			"location": "",
+			"description": "Free language education for the world. Available on Android, iOS, and the web.",
+			"url": "https://t.co/ecOr0JyqFO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ecOr0JyqFO",
+							"expanded_url": "https://www.duolingo.com/",
+							"display_url": "duolingo.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 675953,
+			"friends_count": 523,
+			"listed_count": 2600,
+			"created_at": "Thu Jan 21 23:15:30 +0000 2010",
+			"favourites_count": 26745,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 39473,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F0F0F0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1544787672517967879/yEWm1BEl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1544787672517967879/yEWm1BEl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/107238136/1639491044",
+			"profile_link_color": "7AB41D",
+			"profile_sidebar_border_color": "F0F0F0",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 638,
+		"favorite_count": 5678,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The wildest, most unhinged reality TV show yet, hosted by yours truly. 😘\n\nGet ready for Love Language, a Duolingo and @peacock original series coming soon…or is it? https://t.co/RCYj6PJGqZ"
+	}
+]
