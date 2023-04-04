82bef45a47f90160fc8314239d8499f0f44e8ec0
abraskic <andrew.braskic@gmail.com>
2023-04-04 12:14:47 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78214,
-	"created_at": "Sat Apr 01 06:14:46 +0000 2023",
+	"tweets": 78215,
+	"created_at": "Tue Apr 04 04:14:46 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 04 04:12:55 +0000 2023",
+		"id": 1643104333503029200,
+		"id_str": "1643104333503029248",
+		"full_text": "On the mines, over east or overseas. Also probably doesn’t help when they shut the line for a month. https://t.co/dGdHDkF1jv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dGdHDkF1jv",
+					"expanded_url": "https://twitter.com/abcperth/status/1643007631882338304",
+					"display_url": "twitter.com/abcperth/statu…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 226,
+			"friends_count": 71,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3352,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1643007631882338300,
+		"quoted_status_id_str": "1643007631882338304",
+		"quoted_status_permalink": {
+			"url": "https://t.co/dGdHDkF1jv",
+			"expanded": "https://twitter.com/abcperth/status/1643007631882338304",
+			"display": "twitter.com/abcperth/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 03 21:48:39 +0000 2023",
+			"id": 1643007631882338300,
+			"id_str": "1643007631882338304",
+			"full_text": "The WA government said 20,000 people a day would be heading to Perth Airport by rail. So where are they all? https://t.co/q9QIxWiJ1U",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				132
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/q9QIxWiJ1U",
+						"expanded_url": "https://www.abc.net.au/news/2023-04-04/perth-airport-rail-line-passenger-numbers/102102012",
+						"display_url": "abc.net.au/news/2023-04-0…",
+						"indices": [
+							109,
+							132
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16213339,
+				"id_str": "16213339",
+				"name": "ABC Perth",
+				"screen_name": "abcperth",
+				"location": "Perth",
+				"description": "ABC Local Radio Perth & ABC News I Listen online: https://t.co/TvZnTob8Ol This is an official @abcaustralia account.",
+				"url": "https://t.co/ScZygUv99k",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ScZygUv99k",
+								"expanded_url": "http://www.abc.net.au/perth",
+								"display_url": "abc.net.au/perth",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/TvZnTob8Ol",
+								"expanded_url": "http://bit.ly/ABCRadioPerth",
+								"display_url": "bit.ly/ABCRadioPerth",
+								"indices": [
+									50,
+									73
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 86352,
+				"friends_count": 1801,
+				"listed_count": 771,
+				"created_at": "Tue Sep 09 23:37:34 +0000 2008",
+				"favourites_count": 4682,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 64310,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FAFAFA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/875608978729730049/1OWY8_Dq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/875608978729730049/1OWY8_Dq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16213339/1679353831",
+				"profile_link_color": "272FC2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "E3E3E3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "On the mines, over east or overseas. Also probably doesn’t help when they shut the line for a month. https://t.co/dGdHDkF1jv"
+	},
+	{
 		"created_at": "Sat Apr 01 05:55:39 +0000 2023",
 		"id": 1641456589457506300,
 		"id_str": "1642043026485964804",
