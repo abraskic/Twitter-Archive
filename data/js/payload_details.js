d78ddef31b1caf59bba4986f4560ae11e1a98969
abraskic <andrew.braskic@gmail.com>
2023-05-04 13:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78238,
-	"created_at": "Thu May 04 00:14:46 +0000 2023",
+	"tweets": 78239,
+	"created_at": "Thu May 04 05:14:45 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Thu May 04 04:41:45 +0000 2023",
+		"id": 1653983227521163300,
+		"id_str": "1653983227521163264",
+		"full_text": "@ProfRetail @DavidJonesStore At the very start of Sydney’s Delta wave in 2021 there was a man walking around David Jones Bondi Junction infectious. As far as we can tell there was no onward transmission from that store.",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ProfRetail",
+					"name": "Professor Gary Mortimer 🛒🛒🛒",
+					"id": 388622530,
+					"id_str": "388622530",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "DavidJonesStore",
+					"name": "David Jones",
+					"id": 297730235,
+					"id_str": "297730235",
+					"indices": [
+						12,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1653619921501442000,
+		"in_reply_to_status_id_str": "1653619921501442049",
+		"in_reply_to_user_id": 388622530,
+		"in_reply_to_user_id_str": "388622530",
+		"in_reply_to_screen_name": "ProfRetail",
+		"user": {
+			"id": 14866613,
+			"id_str": "14866613",
+			"name": "Andrew Braskic",
+			"screen_name": "braskic",
+			"location": "Australia",
+			"description": "Too good for the cheap shit, too cheap for the good shit.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 225,
+			"friends_count": 69,
+			"listed_count": 23,
+			"created_at": "Thu May 22 09:52:24 +0000 2008",
+			"favourites_count": 3361,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77452,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1491938407551238147/VlSxpeRC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14866613/1379820914",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0118c71c0ed41109",
+			"url": "https://api.twitter.com/1.1/geo/id/0118c71c0ed41109.json",
+			"place_type": "city",
+			"name": "Perth (WA)",
+			"full_name": "Perth, Western Australia",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.617614368,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-32.675715325
+						],
+						[
+							116.239023008,
+							-31.6244855145
+						],
+						[
+							115.617614368,
+							-31.6244855145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ProfRetail @DavidJonesStore At the very start of Sydney’s Delta wave in 2021 there was a man walking around David Jones Bondi Junction infectious. As far as we can tell there was no onward transmission from that store."
+	},
+	{
 		"created_at": "Thu May 04 00:06:15 +0000 2023",
 		"id": 1653643765394067500,
 		"id_str": "1653913893864091648",
@@ -106,7 +243,7 @@ Grailbird.data.tweets_2023_05 =
 			"name": "ham 🪴",
 			"screen_name": "hamhammer27",
 			"location": "Melbourne | Wurundjeri Land",
-			"description": "🗺️ 🗳️ 🚞 🏗️ ✈️ 🏳️‍🌈 #springst #auspol",
+			"description": "🗺️ 🗳️ 🚞 🏗️ ✈️ 🏳️🌈 #springst #auspol",
 			"url": null,
 			"entities": {
 				"description": {
