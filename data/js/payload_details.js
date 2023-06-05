5913c25ff992cadf7bf2a4a576a8845771639759
abraskic <andrew.braskic@gmail.com>
2023-06-06 02:14:46 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78279,
-	"created_at": "Thu Jun 01 12:14:45 +0000 2023",
+	"tweets": 78280,
+	"created_at": "Mon Jun 05 18:14:45 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_06.js
+++ data/js/tweets/2023_06.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Mon Jun 05 17:57:20 +0000 2023",
+		"id": 1665733421933514800,
+		"id_str": "1665779854351147008",
+		"full_text": "these lane changes in the latest Tesla Full Self-Driving Beta are something else https://t.co/80ddz5uIOv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1665733004357148700,
+					"id_str": "1665733004357148673",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1665733004357148673/pu/img/KrSgChtcTEmumzyH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1665733004357148673/pu/img/KrSgChtcTEmumzyH.jpg",
+					"url": "https://t.co/80ddz5uIOv",
+					"display_url": "pic.twitter.com/80ddz5uIOv",
+					"expanded_url": "https://twitter.com/WholeMarsBlog/status/1665733421933514758/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1665733004357148700,
+					"id_str": "1665733004357148673",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1665733004357148673/pu/img/KrSgChtcTEmumzyH.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1665733004357148673/pu/img/KrSgChtcTEmumzyH.jpg",
+					"url": "https://t.co/80ddz5uIOv",
+					"display_url": "pic.twitter.com/80ddz5uIOv",
+					"expanded_url": "https://twitter.com/WholeMarsBlog/status/1665733421933514758/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 50717,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1665733004357148673/pu/pl/cxhFUyfDXckTsXz0.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 10368000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1665733004357148673/pu/vid/1920x1080/1A5Tm1N4jCo8HtKs.mp4?tag=14"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1665733004357148673/pu/vid/480x270/bdTBkMi7CrZ2oHbm.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1665733004357148673/pu/vid/1280x720/lko6pRe__unnzDe_.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1665733004357148673/pu/vid/640x360/MJby32jsWe1sR_-5.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1263491240336769000,
+			"id_str": "1263491240336769026",
+			"name": "Whole Mars Catalog",
+			"screen_name": "WholeMarsBlog",
+			"location": "San Francisco, CA",
+			"description": "Covering tech, markets, AI, new energy, and autonomous electric vehicles 24/7",
+			"url": "https://t.co/QmsKvMsFxC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QmsKvMsFxC",
+							"expanded_url": "http://sponsor.wholemars.com",
+							"display_url": "sponsor.wholemars.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312933,
+			"friends_count": 19660,
+			"listed_count": 2281,
+			"created_at": "Thu May 21 15:26:16 +0000 2020",
+			"favourites_count": 468086,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 266666,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1593688242771017728/1Ocz0cvu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1593688242771017728/1Ocz0cvu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1263491240336769026/1680511387",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 155,
+		"favorite_count": 1286,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "these lane changes in the latest Tesla Full Self-Driving Beta are something else https://t.co/80ddz5uIOv"
+	},
+	{
 		"created_at": "Thu Jun 01 11:46:48 +0000 2023",
 		"id": 1664237055529078800,
 		"id_str": "1664237055529078787",
